31e92371b5f067ff0ae4d733a39383d7f3b3a4bb
Ola Okelola <ola@okelola.com>
2019-01-15 01:40:02 -0800

    Add User.GetContacts()

--- models/models.go
+++ models/models.go
@@ -1,12 +1,14 @@
 package models
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"strings"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx/reflectx"
 	"github.com/lolopinto/jarvis/data"
 )
 
@@ -18,7 +20,12 @@ type insertdata struct {
 }
 
 func getFieldsAndValuesOfStruct(value reflect.Value, setIDField bool) insertdata {
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
 	valueType := value.Type()
+
+	//fmt.Println(value, valueType)
 
 	fieldCount := value.NumField()
 	// fields -1 for node + 3 for uuid, created_at, updated_at
@@ -39,6 +46,7 @@ func getFieldsAndValuesOfStruct(value reflect.Value, setIDField bool) insertdata
 		}
 	}
 
+	// use sqlx here?
 	for i := 0; i < fieldCount; i++ {
 		field := value.Field(i)
 		typeOfField := valueType.Field(i)
@@ -50,6 +58,7 @@ func getFieldsAndValuesOfStruct(value reflect.Value, setIDField bool) insertdata
 			// or just migrate to use pop
 			//getFieldsAndValuesOfStruct(field)
 		}
+		//fmt.Println(field.Kind(), field.Type())
 
 		var column string
 		tag := typeOfField.Tag.Get("db")
@@ -70,18 +79,19 @@ func getFieldsAndValuesOfStruct(value reflect.Value, setIDField bool) insertdata
 
 func getFieldsAndValues(obj interface{}, setIDField bool) insertdata {
 	value := reflect.ValueOf(obj)
-	if value.Kind() == reflect.Ptr {
-		value = value.Elem()
-	}
-
 	return getFieldsAndValuesOfStruct(value, setIDField)
+}
+
+func getColumnsString(insertData insertdata) string {
+	// remove the time pieces. can't scan into embedded objects
+	columns := insertData.columns[:len(insertData.columns)-2]
+
+	return strings.Join(columns, ",")
 }
 
 func loadNode(id string, entity interface{}, tableName string) error {
 	if entity == nil {
-		// TODO handle this better later. maybe have custom error
-		// return nil, err
-		panic("nil entity passed to loadNode")
+		return errors.New("nil pointer passed to loadNode")
 	}
 	// ok, so now we need a way to map from struct to fields
 	db, err := data.DBConn()
@@ -92,11 +102,8 @@ func loadNode(id string, entity interface{}, tableName string) error {
 
 	defer db.Close()
 
-	insertdata := getFieldsAndValues(entity, false)
-	// remove the time pieces. can't scan into embedded objects
-	columns := insertdata.columns[:len(insertdata.columns)-2]
-
-	colsString := strings.Join(columns, ",")
+	insertData := getFieldsAndValues(entity, false)
+	colsString := getColumnsString(insertData)
 
 	computedQuery := fmt.Sprintf("SELECT %s FROM %s WHERE id = $1", colsString, tableName)
 	fmt.Println(computedQuery)
@@ -113,6 +120,76 @@ func loadNode(id string, entity interface{}, tableName string) error {
 		fmt.Println(err)
 	}
 	return err
+}
+
+// this borrows from/learns from scanAll in sqlx library
+func loadNodes(id string, nodes interface{}, colName string, tableName string) error {
+	db, err := data.DBConn()
+	if err != nil {
+		fmt.Println("error connecting to db ", err)
+		return err
+	}
+
+	value := reflect.ValueOf(nodes)
+	direct := reflect.Indirect(value)
+
+	if value.Kind() != reflect.Ptr {
+		return errors.New("must pass a pointer to loadNodes")
+	}
+	if value.IsNil() {
+		return errors.New("nil pointer passed to loadNodes")
+	}
+
+	// get the slice from the pointer
+	slice := reflectx.Deref(value.Type())
+	if slice.Kind() != reflect.Slice {
+		fmt.Println("sadness")
+		return errors.New("sadness with error in loadNodes")
+	}
+
+	// get the base type from the slice
+	base := reflectx.Deref(slice.Elem())
+	// todo: confirm this is what I think it is
+	fmt.Println(base)
+
+	// get a zero value of this
+	value = reflect.New(base)
+	// really need to rename this haha
+	insertData := getFieldsAndValuesOfStruct(value, false)
+	colsString := getColumnsString(insertData)
+
+	computedQuery := fmt.Sprintf("SELECT %s FROM %s WHERE %s = $1", colsString, tableName, colName)
+	fmt.Println(computedQuery)
+
+	defer db.Close()
+
+	stmt, err := db.Preparex(computedQuery)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Queryx(id)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		entity := reflect.New(base)
+		err = rows.StructScan(entity.Interface())
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		// append each entity into "nodes" destination
+		direct.Set(reflect.Append(direct, reflect.Indirect(entity)))
+	}
+
+	return nil
 }
 
 func createNode(entity interface{}, tableName string) error {
