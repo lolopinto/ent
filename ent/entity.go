44018ba086c430f1a98f0718b38487af0832f1f9
Ola Okelola <ola@okelola.com>
2019-06-06 22:32:29 -0700

    Kill EntWithPrivacy and make it Entity.
    Every ent now has privacy and no need to break it up anymore

--- ent/entity.go
+++ ent/entity.go
@@ -6,10 +6,5 @@ type Entity interface {
 
 	// GetType returns the NodeType of this entity
 	GetType() NodeType
-}
-
-type EntWithPrivacy interface {
-	Entity
-	// TODO eventually make this part of Entity
 	GetPrivacyPolicy() privacy.Policy
 }
