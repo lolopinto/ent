be852aaaa04b7b3cfb8a622281ec431f694de85c
Ola Okelola <ola@okelola.com>
2019-06-08 01:25:36 -0700

    make it so that we can respect manual code put in functions starting with Rules method in privacy policy

--- cmd/gent/ent_codegen.go
+++ cmd/gent/ent_codegen.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/format"
@@ -16,6 +17,14 @@ import (
 	"text/template"
 
 	"github.com/iancoleman/strcase"
+
+	// need to use dst because of this issue:
+	// https://github.com/golang/go/issues/20744
+	// goast doesn't do a keep job of keeping track of comments and it becomes
+	// annoying to keep track of everything that's going on without this library
+	// As of right now, AST is used for everything else but modifying the AST in place
+	"github.com/dave/dst"
+	"github.com/dave/dst/decorator"
 )
 
 type EdgeConfigType string
@@ -82,8 +91,7 @@ func shouldCodegenPackage(file *ast.File, specificConfig string) bool {
 
 func codegenPackage(packageName string, filePath string, specificConfig string) {
 	fset := token.NewFileSet()
-	var src interface{}
-	file, err := parser.ParseFile(fset, filePath, src, parser.AllErrors)
+	file, err := parser.ParseFile(fset, filePath, nil, parser.AllErrors)
 	//spew.Dump(file)
 	die(err)
 	//fmt.Println(f)
@@ -133,20 +141,7 @@ func codegenPackage(packageName string, filePath string, specificConfig string) 
 	}
 }
 
-func getLastStatement(stmts []ast.Stmt) ast.Stmt {
-	length := len(stmts)
-	lastStmt := stmts[length-1]
-	return lastStmt
-}
-
-func getLastExpr(exprs []ast.Expr) ast.Expr {
-	length := len(exprs)
-	lastExpr := exprs[length-1]
-	return lastExpr
-}
-
-// http://goast.yuroyoro.net/ is really helpful to see the tree
-func parseEdgesFunc(packageName string, fn *ast.FuncDecl) []edgeInfo {
+func getCompositeStmtsInFunc(fn *ast.FuncDecl) *ast.CompositeLit {
 	// fn.Body is an instance of *ast.BlockStmt
 	lastStmt := getLastStatement(fn.Body.List)
 
@@ -165,10 +160,263 @@ func parseEdgesFunc(packageName string, fn *ast.FuncDecl) []edgeInfo {
 	if len(returnStmt.Results) != 1 {
 		panic("invalid number or format of return statement")
 	}
+	return compositeListStmt
+}
 
-	// get the
-	edges := make([]edgeInfo, len(compositeListStmt.Elts))
-	for idx, expr := range compositeListStmt.Elts {
+// given a function node, it gets the list of elts in the func
+// works for both edges in GetEdges() func and list of privacy rules in Rules
+func getEltsInFunc(fn *ast.FuncDecl) []ast.Expr {
+	return getCompositeStmtsInFunc(fn).Elts
+}
+
+// astConfig returns the config of a file before it was re-generated to keep
+// useful information that'll be needed when we need to regenerate the manual sections
+// later
+type astConfig struct {
+	fset *token.FileSet //stores the fset
+
+	commentMap map[string]*commentGroupPair
+	// stores the expressions in the method we care about...
+	exprMap map[string][]ast.Expr
+	file    *ast.File
+}
+
+// get the manual expression in the function
+// this assumes the first and last are automated
+// needs to eventually handle the cases where that's not true
+// other places also assume this e.g. in rewriteAstWithConfig
+func (config *astConfig) getManualExprs(fnName string) []ast.Expr {
+	allExprs := config.exprMap[fnName]
+	// returns a slice of the exprs excluding the first and last elements
+	// when we make this more complicated, need to compare against position of MANUAL comments as needed
+	return allExprs[1 : len(allExprs)-1]
+}
+
+// getLastExpr returns the last expression in the function for the given function name
+func (config *astConfig) getLastExpr(fnName string) ast.Expr {
+	allExprs := config.exprMap[fnName]
+	return getLastExpr(allExprs)
+}
+
+// this is for keeping track of commentgroup pairs that have a BEGIN of...
+// and END of... where manual declarations would be put in a function or something
+type commentGroupPair struct {
+	BeginCommentGroup *ast.CommentGroup
+	EndCommentGroup   *ast.CommentGroup
+}
+
+// parseFileForManualCode checks the path of the file we're about to generate,
+// checks to see if it exists and then annotates it with the information
+// needed to regenerate the MANUAL sections later
+func parseFileForManualCode(path string) *astConfig {
+	_, err := os.Stat(path)
+	fmt.Println("sss", path)
+	// file doesn't exist or can't read file, nothing to do here...
+	if err != nil {
+		return nil
+	}
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
+	die(err)
+
+	comments := []*commentGroupPair{}
+
+	var begin *ast.CommentGroup
+
+	for idx := 0; idx < len(file.Comments); idx++ {
+		cg := file.Comments[idx]
+		fmt.Println(cg.Text())
+
+		if begin == nil {
+			if strings.HasPrefix(cg.Text(), "BEGIN MANUAL SECTION") {
+				begin = cg
+			}
+		} else {
+			// this doesn't work when this has something in front of it...
+			splits := strings.Split(cg.Text(), "\n")
+			for _, s := range splits {
+				if strings.HasPrefix(s, "END MANUAL SECTION") {
+					comments = append(comments, &commentGroupPair{
+						BeginCommentGroup: begin,
+						EndCommentGroup:   cg,
+					})
+					begin = nil
+				}
+			}
+		}
+	}
+
+	// nothing to do here since no manual thing in between that we care about
+	if len(comments) == 0 {
+		return nil
+	}
+
+	commentMap := make(map[string]*commentGroupPair)
+	exprMap := make(map[string][]ast.Expr)
+
+	//comments := []*ast.Comment{}
+	ast.Inspect(file, func(node ast.Node) bool {
+		//spew.Dump(node)
+		// collect comments
+		if fn, ok := node.(*ast.FuncDecl); ok {
+			// we only care about the Rules method for now but we can make this work for other methods in the future...
+
+			fnName := fn.Name.Name
+
+			if fnName == "Rules" {
+
+				for _, cgPair := range comments {
+					begin := cgPair.BeginCommentGroup.List[0]
+					end := cgPair.EndCommentGroup.List[0]
+
+					// the comments map to what we're looking for
+					if !(fn.Pos() < begin.Pos() && end.Pos() < fn.End()) {
+						continue
+					}
+					fmt.Println("yay!...")
+
+					commentMap[fnName] = cgPair
+					//exprMap[fnName] = manualStmts
+					// store the entire elts in func...
+					exprMap[fnName] = getEltsInFunc(fn)
+				}
+			}
+		}
+
+		return true
+	})
+
+	return &astConfig{
+		fset:       fset,
+		commentMap: commentMap,
+		exprMap:    exprMap,
+		file:       file,
+	}
+}
+
+// rewriteAstWithConfig takes the ast that was generated and rewrites it
+// so that we respect the user-generated MANUAL code
+func rewriteAstWithConfig(config *astConfig, b []byte) []byte {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", b, parser.ParseComments)
+	die(err)
+
+	comments := []*ast.CommentGroup{}
+
+	// get the commentgroups in this file that map to what we care about...
+	for _, cg := range file.Comments {
+		// in the new AST we need a comment group with exactly 2 items
+		// the BEGIN and END
+		if len(cg.List) != 2 {
+			continue
+		}
+		// not the right cg so bounce
+		if !strings.HasPrefix(cg.Text(), "BEGIN MANUAL SECTION") {
+			continue
+		}
+		comments = append(comments, cg)
+	}
+
+	// something weird happened, bad...
+	if len(comments) == 0 {
+		die(errors.New("did not find comments to be rewritten in generated file"))
+	}
+
+	// create decorator for file before it was changed
+	oldDec := decorator.NewDecorator(config.fset)
+	_, err = oldDec.DecorateFile(config.file)
+	die(err)
+
+	// Create a new decorator, which will track the mapping between ast and dst nodes
+	dec := decorator.NewDecorator(fset)
+	dstFile, err := dec.DecorateFile(file)
+	die(err)
+
+	// inspect the methods we care about
+	dst.Inspect(dstFile, func(node dst.Node) bool {
+		fn, ok := node.(*dst.FuncDecl)
+		if !ok {
+			return true
+		}
+
+		fnName := fn.Name.Name
+		// same as in parseFileForManualCode. only care about Rules method for now...
+		// should eventually make sure this works for everything.
+		if fnName != "Rules" {
+			return true
+		}
+
+		manualExprs := config.getManualExprs(fnName)
+
+		// take the DST func, convert to DST so we can use helper methods we have here
+		// and then convert back to DST so that we can
+		astFunc := dec.Ast.Nodes[fn].(*ast.FuncDecl)
+		compositeLit := getCompositeStmtsInFunc(astFunc)
+
+		compositeLitDst := dec.Dst.Nodes[compositeLit].(*dst.CompositeLit)
+		elts := compositeLitDst.Elts
+
+		manualDstExprs := make([]dst.Expr, len(manualExprs))
+		for idx, stmt := range manualExprs {
+			expr := oldDec.Dst.Nodes[stmt].(dst.Expr)
+			//spew.Dump(expr, ok)
+			// take each of the manual stmts from the old file and add them to the new elts
+			clonedExpr := dst.Clone(expr).(dst.Expr)
+			manualDstExprs[idx] = clonedExpr
+		}
+
+		// append list at position
+		// TODO create a library for helpers like these...
+		elts = append(elts[:1], append(manualDstExprs, elts[1:]...)...)
+		compositeLitDst.Elts = elts
+
+		// ensure that whatever comments were in the last node of the manual
+		// section are copied over to the new last node.
+		// Does a few things of note:
+		// * removes duplicate BEGIN MANUAL SECTION which was	associated with last node
+		// when there were only 2 statements before the manual sections were considered
+		// * ensures that any comments at the end of the manual section that the developer
+		// had written were remembered.
+		lastExpr := config.getLastExpr(fnName)
+		lastDstExpr := oldDec.Dst.Nodes[lastExpr].(dst.Expr)
+
+		lastElt := elts[len(elts)-1]
+		//spew.Dump("lastElt", lastElt.Decorations())
+		// replace the string with what was in the manual version...
+		lastElt.Decorations().Start.Replace(lastDstExpr.Decorations().Start.All()...)
+
+		return true
+	})
+
+	decorator.Print(dstFile)
+	restoredFset, restoredFile, err := decorator.RestoreFile(dstFile)
+
+	var buf bytes.Buffer
+	format.Node(&buf, restoredFset, restoredFile)
+	die(err)
+
+	return buf.Bytes()
+}
+
+func getLastStatement(stmts []ast.Stmt) ast.Stmt {
+	length := len(stmts)
+	lastStmt := stmts[length-1]
+	return lastStmt
+}
+
+func getLastExpr(exprs []ast.Expr) ast.Expr {
+	length := len(exprs)
+	lastExpr := exprs[length-1]
+	return lastExpr
+}
+
+// http://goast.yuroyoro.net/ is really helpful to see the tree
+func parseEdgesFunc(packageName string, fn *ast.FuncDecl) []edgeInfo {
+	elts := getEltsInFunc(fn)
+
+	// get the edges in teh function
+	edges := make([]edgeInfo, len(elts))
+	for idx, expr := range elts {
 		keyValueExpr := getExprToKeyValueExpr(expr)
 		fmt.Println(keyValueExpr)
 		// get the edge as needed
@@ -515,11 +763,12 @@ func getNodeTemplate(packageName string, fields []field) nodeTemplate {
 }
 
 type fileToWriteInfo struct {
-	nodeData          nodeTemplate
-	pathToTemplate    string
-	templateName      string
-	pathToFile        string
-	createDirIfNeeded bool
+	nodeData           nodeTemplate
+	pathToTemplate     string
+	templateName       string
+	pathToFile         string
+	createDirIfNeeded  bool
+	checkForManualCode bool
 }
 
 func writeModelFile(nodeData nodeTemplate) {
@@ -548,18 +797,21 @@ func writeMutatorFile(nodeData nodeTemplate) {
 }
 
 func writePrivacyFile(nodeData nodeTemplate) {
-	// todo figure out what package should be here
+	pathToFile := fmt.Sprintf("models/%s_privacy.go", nodeData.PackageName)
+
 	writeFile(
 		fileToWriteInfo{
-			nodeData:       nodeData,
-			pathToTemplate: "cmd/gent/privacy.tmpl",
-			templateName:   "privacy.tmpl",
-			pathToFile:     fmt.Sprintf("models/%s_privacy.go", nodeData.PackageName),
+			nodeData:           nodeData,
+			pathToTemplate:     "cmd/gent/privacy.tmpl",
+			templateName:       "privacy.tmpl",
+			pathToFile:         pathToFile,
+			checkForManualCode: true,
 		},
 	)
 }
 
-func writeFile(file fileToWriteInfo) {
+// generate new AST for the given file from the template
+func generateNewAst(file fileToWriteInfo) []byte {
 	path := []string{file.pathToTemplate}
 	t, err := template.New(file.templateName).ParseFiles(path...)
 	die(err)
@@ -577,6 +829,26 @@ func writeFile(file fileToWriteInfo) {
 	// gofmt the buffer
 	bytes, err := format.Source(buffer.Bytes())
 	die(err)
+	return bytes
+}
+
+func writeFile(file fileToWriteInfo) {
+	// parse existing file to see what we need to keep in the rewrite
+	var config *astConfig
+
+	// no real reason this only applies for just privacy.tmpl
+	// but it's the only one where we care about this for now so limiting to just that
+	// in the future, this should apply for all
+	if file.checkForManualCode {
+		config = parseFileForManualCode(file.pathToFile)
+	}
+
+	// generate the new AST we want for the file
+	bytes := generateNewAst(file)
+
+	if config != nil {
+		bytes = rewriteAstWithConfig(config, bytes)
+	}
 
 	//codeForFile := string(bytes)
 
@@ -628,7 +900,7 @@ func writeFile(file fileToWriteInfo) {
 		}
 	}
 
-	err = ioutil.WriteFile(file.pathToFile, bytes, 0666)
+	err := ioutil.WriteFile(file.pathToFile, bytes, 0666)
 	//	_, err = file.Write(bytes)
 	die(err)
 	//err = file.Close()
