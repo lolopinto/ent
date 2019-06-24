b75aa3815ac688bcd4262f6f6c7715f3531338d6
Ola Okelola <ola@okelola.com>
2019-06-24 13:42:28 -0700

    make auto_schema arguments required and clearer what to do

--- python/auto_schema/gen_db_schema.py
+++ python/auto_schema/gen_db_schema.py
@@ -7,8 +7,9 @@ from auto_schema import runner
 from importlib import import_module
 
 parser = argparse.ArgumentParser(description="generate the db schema for an ent")
-parser.add_argument('--schema', metavar='S', help='path to the folder the generated schema file is in')
-parser.add_argument('--engine', metavar='E', help='URL to connect to the database')
+required = parser.add_argument_group('required arguments')
+required.add_argument('-s', '--schema', help='path to the folder the generated schema file is in', required=True)
+required.add_argument('-e', '--engine', help='URL to connect to the database', required=True)
 
 if __name__ == "__main__" :
   # TODO we need to support running each of the alembic commands directly e.g. upgrade head, upgrade +1, downgrade -1, current, history, etc
