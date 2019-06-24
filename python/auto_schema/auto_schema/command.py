2c7437484bde7ad8a8a017ffcb6efe10a4d6848f
Ola Okelola <ola@okelola.com>
2019-06-24 15:21:12 -0700

    first step: automatically generate the alembic revision based on the changes
    only supports add_table for now but we can detect these changes and do things with them going forward

--- python/auto_schema/auto_schema/command.py
+++ python/auto_schema/auto_schema/command.py
@@ -14,6 +14,11 @@ class Command(object):
 
     alembic_cfg.set_main_option("version_locations", os.path.join(schema_path, "versions"))
 
+    # should probably make some of these configurable eventually
+    alembic_cfg.set_main_option("file_template", "%%(rev)s_%%(year)d%%(month)d%%(day)d%%(hour)d%%(minute)d%%(second)d_%%(slug)s")
+    alembic_cfg.set_main_option("truncate_slug_length", "40")
+    alembic_cfg.set_main_option("timezone", "utc") # use utc timezone.
+
     self.alembic_cfg = alembic_cfg
 
     # pass connection instead of re-creating it and using a sqlalchemy_url file
@@ -25,7 +30,6 @@ class Command(object):
     command.current(self.alembic_cfg, verbose=True)
 
   # Simulates running the `alembic revision -m` command
-  # TODO autogenerate the message based on reading the schema
   def revision(self, message):
     command.revision(self.alembic_cfg, message, autogenerate=True)
 
