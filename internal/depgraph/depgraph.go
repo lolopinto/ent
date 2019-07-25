9b7828048b05f5a2bacb132f0893da0d63652529
Ola Okelola <ola@okelola.com>
2019-07-25 00:17:37 -0700

    add simple depgraph to make it so that parsing files and managing dependencies is easier

---
+++ internal/depgraph/depgraph.go
@@ -0,0 +1,137 @@
+package depgraph
+
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+type data struct {
+	value     interface{}
+	completed bool
+	deps      []string // todo this can eventually be a graph but we're keeping this simple for now
+}
+
+type Depgraph struct {
+	items map[string]*data
+	queue []*queueItem
+}
+
+func (g *Depgraph) AddItem(key string, value interface{}, deps ...string) {
+	if g.items == nil {
+		g.items = make(map[string]*data)
+	}
+	g.items[key] = &data{
+		value:     value,
+		completed: false,
+		deps:      deps,
+	}
+}
+
+// Run runs the dependency graph. takes a func that takes an interface{}, calls it exactly once for everything that has been added
+// and makes sure it's called exactly once for each
+func (g *Depgraph) Run(exec func(interface{})) {
+	itemsToCheck := []*data{}
+	for _, item := range g.items {
+		if !g.checkDependenciesCompleted(item) {
+			itemsToCheck = append(itemsToCheck, item)
+			continue
+		}
+		exec(item.value)
+		item.completed = true
+	}
+
+	i := 1
+	for len(itemsToCheck) > 0 {
+		i++
+		if i == 5 {
+			panic("dependency graph not resolving. halp!")
+		}
+		newItems := g.runDepgraph(itemsToCheck, exec)
+		itemsToCheck = newItems
+	}
+}
+
+func (g *Depgraph) runDepgraph(itemsToCheck []*data, exec func(interface{})) []*data {
+	newItems := []*data{}
+	for _, item := range itemsToCheck {
+		if !g.checkDependenciesCompleted(item) {
+			newItems = append(newItems, item)
+			continue
+		}
+		exec(item.value)
+		item.completed = true
+	}
+	return newItems
+}
+
+type queueItem struct {
+	item *data
+	exec func(interface{})
+}
+
+func (g *Depgraph) CheckAndQueue(key string, exec func(interface{})) {
+	item, ok := g.items[key]
+	if !ok {
+		panic(fmt.Sprintf("no function exists for key item %s", key))
+	}
+	if g.checkDependenciesCompleted(item) {
+		exec(item.value)
+		item.completed = true
+	} else {
+		g.queue = append(g.queue, &queueItem{
+			item: item,
+			exec: exec,
+		})
+	}
+}
+
+func (g *Depgraph) RunQueuedUpItems() {
+	i := 1
+	queue := g.queue
+	for len(queue) > 0 {
+		if i == 5 {
+			panic("dependency graph not resolving. halp!")
+		}
+		newQueue := g.runQueue(queue)
+		spew.Dump(newQueue, i)
+		queue = newQueue
+		i = i + 1
+	}
+	g.queue = nil
+}
+
+func (g *Depgraph) runQueue(queue []*queueItem) []*queueItem {
+	newQueue := []*queueItem{}
+	for _, q := range queue {
+		if !g.checkDependenciesCompleted(q.item) {
+			newQueue = append(newQueue, q)
+			continue
+		}
+		q.exec(q.item.value)
+		q.item.completed = true
+	}
+	return newQueue
+}
+
+// RunLoop is used in scenarios where the client needs to control what's run and what's passed. they should
+// "override" RunLoop(), call CheckAndQueue() for each item and then call RunQueuedUpItems once afterwards to run the rest
+func (g *Depgraph) RunLoop() {
+	panic("need client to implement")
+}
+
+func (g *Depgraph) checkDependenciesCompleted(item *data) bool {
+	if len(item.deps) == 0 {
+		return true
+	}
+	for _, dep := range item.deps {
+		depItem, ok := g.items[dep]
+		if !ok {
+			panic(fmt.Errorf("a dependency was added on key %s but no function exists for it", dep))
+		}
+		if !depItem.completed {
+			return false
+		}
+	}
+	return true
+}
---
+++ internal/depgraph/depgraph_test.go
@@ -0,0 +1,275 @@
+package depgraph
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func getKey(i int) string {
+	return fmt.Sprintf("item%d", i)
+}
+
+type depgraphTest struct {
+	Depgraph
+	sum int
+}
+
+func (g *depgraphTest) sumFunc(i int) {
+	g.sum = g.sum + i
+}
+
+func (g *depgraphTest) RunLoop() {
+	for i := 0; i < 10; i++ {
+		j := i // to capture the loop variable correctly
+		g.CheckAndQueue(getKey(i), func(item interface{}) {
+			passedFunc, ok := item.(func(int))
+			if !ok {
+				panic("invalid func passed")
+			}
+			passedFunc(j)
+		})
+	}
+}
+
+type depgraphTestSimple struct {
+	Depgraph
+	//	numberOfTimesCalled int
+	//	numbers []int
+	//	map[]
+}
+
+type object struct {
+	field1 int
+	field2 string
+	field3 int
+	field4 string
+}
+
+// func (g *depgraphTestSimple) execFn(exec func(interface{})) {
+// 	g.numberOfTimesCalled++
+// }
+
+// func (g *depgraphTestSimple) sumFunc(i int) {
+// 	g.numbers = append(g.numbers, i)
+// }
+
+func TestRunLoopNoDeps(t *testing.T) {
+	g := &depgraphTest{}
+
+	for i := 0; i < 10; i++ {
+		g.AddItem(getKey(i), g.sumFunc)
+	}
+
+	if len(g.items) != 10 {
+		t.Errorf("expected 10 items to be added. got %d instead", len(g.items))
+	}
+
+	g.RunLoop()
+
+	if len(g.queue) != 0 {
+		t.Errorf("expected no items queued up. %d items were queued up instead", len(g.queue))
+	}
+
+	expectedSum := (10 * 9) / 2
+	if g.sum != expectedSum {
+		t.Errorf(
+			"expected the sum to be %d, it was %d instead implying every function wasn't called exactly once",
+			expectedSum,
+			g.sum,
+		)
+	}
+}
+
+func TestRunLoopWithSimpleDeps(t *testing.T) {
+	g := &depgraphTest{}
+
+	// for half of the items, add a dependency, for the other half run once
+	// even numbered items have a dependency on the one after them
+	for i := 0; i < 10; i++ {
+		key := getKey(i)
+		if i%2 == 0 {
+			g.AddItem(key, g.sumFunc, getKey(i+1))
+		} else {
+			g.AddItem(key, g.sumFunc)
+		}
+	}
+
+	if len(g.items) != 10 {
+		t.Errorf("expected 10 items to be added. got %d instead", len(g.items))
+	}
+
+	g.RunLoop()
+
+	if len(g.queue) != 5 {
+		t.Errorf("expected 5 items queued up. %d items were queued up instead", len(g.queue))
+	}
+
+	expectedSum := 1 + 3 + 5 + 7 + 9
+	if g.sum != expectedSum {
+		t.Errorf("expected sum for the items run so far is not as expected")
+	}
+
+	g.RunQueuedUpItems()
+
+	if len(g.queue) != 0 {
+		t.Errorf("expected no items queued up. %d items were queued up instead", len(g.queue))
+	}
+
+	expectedSum = (10 * 9) / 2
+	if g.sum != expectedSum {
+		t.Errorf(
+			"expected the sum to be %d, it was %d instead implying every function wasn't called exactly once",
+			expectedSum,
+			g.sum,
+		)
+	}
+}
+
+func TestRunLoopTooManyDeps(t *testing.T) {
+	g := &depgraphTest{}
+
+	// we have 1 depends on 2, 3; 2 depends on 3, 4- and so on and since it's simple and not a
+	// real graph, it breaks
+	for i := 0; i < 10; i++ {
+		key := getKey(i)
+		if i == 9 {
+			g.AddItem(key, g.sumFunc)
+		} else {
+			g.AddItem(key, g.sumFunc, getKey(i+1), getKey((i+2)%10))
+		}
+	}
+
+	if len(g.items) != 10 {
+		t.Errorf("expected 10 items to be added. got %d instead", len(g.items))
+	}
+
+	g.RunLoop()
+
+	if len(g.queue) != 9 {
+		t.Errorf("expected 9 items queued up. %d items were queued up instead", len(g.queue))
+	}
+
+	assert.Panics(t, g.RunQueuedUpItems)
+}
+
+func TestInvalidDep(t *testing.T) {
+	g := &depgraphTest{}
+
+	for i := 0; i < 10; i++ {
+		key := getKey(i)
+		if i == 0 {
+			g.AddItem(key, g.sumFunc, getKey(10))
+		} else {
+			g.AddItem(key, g.sumFunc)
+		}
+	}
+
+	// panics here
+	assert.Panics(t, g.RunLoop)
+}
+
+func TestRunNoDeps(t *testing.T) {
+	g := &depgraphTestSimple{}
+
+	g.AddItem("field1", func(obj *object) {
+		obj.field1 = 1
+	})
+
+	g.AddItem("field2", func(obj *object) {
+		obj.field2 = "field2"
+	})
+
+	g.AddItem("field3", func(obj *object) {
+		obj.field3 = 3
+	})
+
+	g.AddItem("field4", func(obj *object) {
+		obj.field4 = "field4"
+	})
+
+	if len(g.items) != 4 {
+		t.Errorf("expected 4 items to be added. got %d instead", len(g.items))
+	}
+
+	var hardToCalObj object
+
+	g.Run(func(item interface{}) {
+		execFn, ok := item.(func(*object))
+		if !ok {
+			panic("invalid object passed")
+		}
+		execFn(&hardToCalObj)
+	})
+
+	if len(g.queue) != 0 {
+		t.Errorf("expected no items queued up. %d items were queued up instead", len(g.queue))
+	}
+
+	if hardToCalObj.field1 != 1 {
+		t.Errorf("field1 was not set when Run() was called")
+	}
+	if hardToCalObj.field2 != "field2" {
+		t.Errorf("field2 was not set when Run() was called")
+	}
+	if hardToCalObj.field3 != 3 {
+		t.Errorf("field3 was not set when Run() was called")
+	}
+	if hardToCalObj.field4 != "field4" {
+		t.Errorf("field4 was not set when Run() was called")
+	}
+}
+
+func TestRunWithDeps(t *testing.T) {
+	g := &depgraphTestSimple{}
+
+	g.AddItem("field1", func(obj *object) {
+		obj.field1 = uuid.MustParse(obj.field2).ClockSequence()
+	}, "field2")
+
+	g.AddItem("field2", func(obj *object) {
+		obj.field2 = uuid.New().String()
+	})
+
+	g.AddItem("field3", func(obj *object) {
+		obj.field3 = 3
+	}, "field4")
+
+	g.AddItem("field4", func(obj *object) {
+		obj.field4 = "field4"
+	})
+
+	if len(g.items) != 4 {
+		t.Errorf("expected 4 items to be added. got %d instead", len(g.items))
+	}
+
+	var hardToCalObj object
+
+	g.Run(func(item interface{}) {
+		execFn, ok := item.(func(*object))
+		if !ok {
+			panic("invalid object passed")
+		}
+		execFn(&hardToCalObj)
+	})
+
+	if len(g.queue) != 0 {
+		t.Errorf("expected no items queued up. %d items were queued up instead", len(g.queue))
+	}
+
+	// shows the dependency btw 1 and 2 since 2 has to be set before 1 can be
+	// 4 and 3 is random right now and doesn't necessarily prove anything but that's fine
+	field2Uuid := uuid.MustParse(hardToCalObj.field2)
+
+	if hardToCalObj.field1 != field2Uuid.ClockSequence() {
+		t.Errorf("field1 was not set when Run() was called")
+	}
+	if hardToCalObj.field3 != 3 {
+		t.Errorf("field3 was not set when Run() was called")
+	}
+	if hardToCalObj.field4 != "field4" {
+		t.Errorf("field4 was not set when Run() was called")
+	}
+}
