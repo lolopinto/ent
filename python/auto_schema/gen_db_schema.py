9055b51148b832259b57500e9e8a3707fbb65a40
Ola Okelola <ola@okelola.com>
2019-10-11 21:52:03 -0700

    provide a way to re-insert any edges that may be (accidentally or not) deleted
    command to run is: `python3 ../../python/auto_schema/gen_db_schema.py -s=models/configs -e="postgres://ola:@localhost/ent_test?sslmode=disable" -f=True`

--- python/auto_schema/gen_db_schema.py
+++ python/auto_schema/gen_db_schema.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="generate the db schema for an ent"
 required = parser.add_argument_group('required arguments')
 required.add_argument('-s', '--schema', help='path to the folder the generated schema file is in', required=True)
 required.add_argument('-e', '--engine', help='URL to connect to the database', required=True)
+parser.add_argument('-f', '--fix_edges', help='fix edges in schema into db')
 
 if __name__ == "__main__" :
   # TODO we need to support running each of the alembic commands directly e.g. upgrade head, upgrade +1, downgrade -1, current, history, etc
@@ -21,6 +22,9 @@ if __name__ == "__main__" :
   schema = import_module('schema')
   metadata = schema.get_metadata()
 
-  r = runner.Runner.from_command_line(metadata, args)
-  r.run()
+  if args.fix_edges:
+    runner.Runner.fix_edges(metadata, args)
+  else:
+    r = runner.Runner.from_command_line(metadata, args)
+    r.run()
   
