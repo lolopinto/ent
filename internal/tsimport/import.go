54b3be9b6bdcee02ca76455a768d0c3da9c019b5
lolopinto <ola@okelola.com>
2020-03-19 00:11:19 -0700

    fix imports of files in generated code (#40)
    so we only import paths we use instead of all the possible options across all the files
    
    This is similar to what we do in go (taken from/inspired by gqlgen).
    
    This makes it so that the generated files pass vscode lint and do the right thing
    
    It's slightly more annoying to edit the tmpl files but I think that's fine

---
+++ internal/tsimport/import.go
@@ -0,0 +1,158 @@
+package tsimport
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+// Imports keeps track of imports in a generated typescript file
+type Imports struct {
+	exportMap   map[string]*importInfo
+	usedExports map[string]bool
+	imports     []*importInfo
+}
+
+// NewImports is the constructor for Imports
+func NewImports() *Imports {
+	return &Imports{
+		exportMap:   make(map[string]*importInfo),
+		usedExports: make(map[string]bool),
+	}
+}
+
+// Reserve reserves a path and list of exports
+func (imps *Imports) Reserve(path string, exports ...string) (string, error) {
+	return imps.reserve(path, "", false, exports)
+}
+
+// ReserveDefault reserves the default export from a path and a list of exports
+func (imps *Imports) ReserveDefault(path, defaultExport string, exports ...string) (string, error) {
+	return imps.reserve(path, defaultExport, false, exports)
+}
+
+// ReserveAll reserves importing all from a path as the alias
+func (imps *Imports) ReserveAll(path, as string) (string, error) {
+	return imps.reserve(path, "", true, []string{as})
+}
+
+func (imps *Imports) reserve(path string, defaultExport string, importAll bool, exports []string) (string, error) {
+	imp := &importInfo{
+		path:          path,
+		exports:       exports,
+		importAll:     importAll,
+		defaultExport: defaultExport,
+	}
+
+	if defaultExport != "" {
+		exports = append(exports, defaultExport)
+		imp.exports = exports
+	}
+
+	for _, export := range exports {
+		existingImport := imps.exportMap[export]
+		if existingImport != nil {
+			return "", fmt.Errorf("%s is already exported from path %s", export, existingImport.path)
+		}
+		imps.exportMap[export] = imp
+	}
+	imps.imports = append(imps.imports, imp)
+	return "", nil
+}
+
+// FuncMap returns the FuncMap to be passed to a template
+func (imps *Imports) FuncMap() template.FuncMap {
+	return template.FuncMap{
+		"reserveImport":        imps.Reserve,
+		"reserveDefaultImport": imps.ReserveDefault,
+		"reserveAllImport":     imps.ReserveAll,
+		"useImport":            imps.Use,
+	}
+}
+
+// Use makes use of an export and ensures that's imported
+func (imps *Imports) Use(export string) (string, error) {
+	if imps.exportMap[export] == nil {
+		return "", fmt.Errorf("tried to use export %s even though it was never reserved", export)
+	}
+
+	imps.usedExports[export] = true
+	return export, nil
+}
+
+// Returns a list of imported lines
+func (imps *Imports) String() (string, error) {
+	var sb strings.Builder
+	for _, imp := range imps.imports {
+
+		str, err := imp.String(imps.usedExports)
+
+		if err != nil {
+			return "", err
+		}
+		if str == "" {
+			continue
+		}
+		_, err = sb.WriteString(str)
+		if err != nil {
+			return "", err
+		}
+		sb.WriteString("\n")
+	}
+
+	return sb.String(), nil
+}
+
+type importInfo struct {
+	path          string
+	exports       []string
+	defaultExport string
+	importAll     bool
+}
+
+func (imp *importInfo) String(usedExportsMap map[string]bool) (string, error) {
+	var usedExports []string
+	var defaultExport string
+	for _, export := range imp.exports {
+		if !usedExportsMap[export] {
+			continue
+		}
+
+		if imp.defaultExport == export && imp.defaultExport != "" {
+			// default export
+			defaultExport = imp.defaultExport
+		} else {
+			// no default export nothing to do here
+			usedExports = append(usedExports, export)
+		}
+	}
+
+	// not using anything here, we're done with this one
+	if len(usedExports) == 0 && defaultExport == "" {
+		return "", nil
+	}
+
+	var imports []string
+
+	if imp.importAll {
+		if defaultExport != "" || len(usedExports) != 1 {
+			return "", fmt.Errorf("when importing all, expect 1 export. something broken for %s", imp.path)
+		}
+
+		imports = append(imports, fmt.Sprintf("* as %s", usedExports[0]))
+
+	} else {
+		if defaultExport != "" {
+			imports = append(imports, defaultExport)
+		}
+		if len(usedExports) != 0 {
+			imports = append(imports, fmt.Sprintf("{%s}", strings.Join(usedExports, ", ")))
+		}
+	}
+
+	return fmt.Sprintf(
+		"import %s from %s;",
+		strings.Join(imports, ", "), strconv.Quote(imp.path),
+	), nil
+}
---
+++ internal/tsimport/import_test.go
@@ -0,0 +1,189 @@
+package tsimport_test
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/lolopinto/ent/internal/tsimport"
+	"github.com/lolopinto/ent/internal/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type testCase struct {
+	fn            func(*tsimport.Imports)
+	expectedLines []string
+	panicInFn     bool
+}
+
+func getImportLine(line, path string) string {
+	r := strings.NewReplacer("{path}", strconv.Quote(path))
+	return r.Replace(line)
+}
+
+func reserveImport(imps *tsimport.Imports, path string, exports ...string) {
+	_, err := imps.Reserve(path, exports...)
+	util.Die(err)
+}
+
+func useImport(imps *tsimport.Imports, export string) {
+	_, err := imps.Use(export)
+	util.Die(err)
+}
+
+func reserveDefaultImport(imps *tsimport.Imports, path, defaultExport string, exports ...string) {
+	_, err := imps.ReserveDefault(path, defaultExport, exports...)
+	util.Die(err)
+}
+
+func reserveAllImport(imps *tsimport.Imports, path, as string) {
+	_, err := imps.ReserveAll(path, as)
+	util.Die(err)
+}
+
+func TestImports(t *testing.T) {
+	testCases := map[string]testCase{
+		"reserve some": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveImport(imps, "ent/ent", "loadEnt", "loadEntX", "Viewer")
+
+				useImport(imps, "loadEnt")
+				useImport(imps, "loadEntX")
+			},
+			expectedLines: []string{
+				getImportLine("import {loadEnt, loadEntX} from {path};", "ent/ent"),
+			},
+		},
+		"nothing used": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveImport(imps, "ent/ent", "loadEnt", "loadEntX", "Viewer")
+			},
+			expectedLines: []string{},
+		},
+		"reserve default. only default used": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveDefaultImport(imps, "src/ent/user", "User", "createUser", "editUser")
+
+				useImport(imps, "User")
+			},
+			expectedLines: []string{
+				getImportLine("import User from {path};", "src/ent/user"),
+			},
+		},
+		"reserve default. non-default used": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveDefaultImport(imps, "src/ent/user", "User", "createUser", "editUser")
+
+				useImport(imps, "createUser")
+				useImport(imps, "editUser")
+			},
+			expectedLines: []string{
+				getImportLine("import {createUser, editUser} from {path};", "src/ent/user"),
+			},
+		},
+		"reserve default. mix used": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveDefaultImport(imps, "src/ent/user", "User", "createUser", "editUser")
+
+				useImport(imps, "createUser")
+				useImport(imps, "editUser")
+				useImport(imps, "User")
+			},
+			expectedLines: []string{
+				getImportLine("import User, {createUser, editUser} from {path};", "src/ent/user"),
+			},
+		},
+		"reserve all. used": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveAllImport(imps, "ent/query", "query")
+
+				useImport(imps, "query")
+			},
+			expectedLines: []string{
+				getImportLine("import * as query from {path};", "ent/query"),
+			},
+		},
+		"reserve all. not used": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveAllImport(imps, "ent/query", "query")
+			},
+			expectedLines: []string{},
+		},
+		// TODO name collisions foo as bar
+		// simpler version of * as query?
+		"reserve same thing multiple times": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveAllImport(imps, "ent/query", "query")
+				reserveAllImport(imps, "bar/foo", "query")
+			},
+			panicInFn: true,
+		},
+		"use not reserved": testCase{
+			fn: func(imps *tsimport.Imports) {
+				useImport(imps, "query")
+			},
+			panicInFn: true,
+		},
+		"use multiple times": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveAllImport(imps, "ent/query", "query")
+
+				useImport(imps, "query")
+				useImport(imps, "query")
+			},
+			expectedLines: []string{
+				getImportLine("import * as query from {path};", "ent/query"),
+			},
+		},
+		"combo": testCase{
+			fn: func(imps *tsimport.Imports) {
+				reserveImport(imps, "ent/ent", "loadEnt", "loadEntX", "Viewer", "ID")
+				reserveDefaultImport(imps, "src/ent/user", "User", "createUser", "editUser")
+				reserveDefaultImport(imps, "src/ent/contact", "Contact", "createContact", "editContact")
+
+				reserveAllImport(imps, "ent/query", "query")
+
+				useImport(imps, "ID")
+				useImport(imps, "loadEnt")
+				useImport(imps, "loadEntX")
+
+				useImport(imps, "User")
+				useImport(imps, "editUser")
+				useImport(imps, "query")
+			},
+			expectedLines: []string{
+				getImportLine("import {loadEnt, loadEntX, ID} from {path};", "ent/ent"),
+				getImportLine("import User, {editUser} from {path};", "src/ent/user"),
+				getImportLine("import * as query from {path};", "ent/query"),
+			},
+		},
+	}
+
+	for key, tt := range testCases {
+		t.Run(key, func(t *testing.T) {
+			imps := tsimport.NewImports()
+
+			if tt.panicInFn {
+				assert.Len(t, tt.expectedLines, 0)
+				assert.Panics(t, func() {
+					tt.fn(imps)
+				})
+			} else {
+				tt.fn(imps)
+
+				validateExpectedLines(t, imps, tt.expectedLines)
+			}
+		})
+	}
+}
+
+func validateExpectedLines(t *testing.T, imps *tsimport.Imports, lines []string) {
+	str, err := imps.String()
+	require.Nil(t, err)
+	expLines := strings.Split(str, "\n")
+	// remove last element since it's always nonsense
+	expLines = expLines[:len(expLines)-1]
+
+	assert.Equal(t, expLines, lines)
+}
