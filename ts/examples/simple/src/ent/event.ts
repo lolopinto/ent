c90f180076b86d7e0bc56d025f6962172f66ff12
Ola Okelola <ola@okelola.com>
2020-04-05 01:12:46 -0700

    fix ents
    forgot to remove the codes in contact, event, user now that the generated mutations have been moved to builders
    
    see 55d1b646290b4d190541a07e223a9c8355b07ec5 (#45)

--- ts/examples/simple/src/ent/event.ts
+++ ts/examples/simple/src/ent/event.ts
@@ -1,12 +1,4 @@
-import {
-  EventBase,
-  EventCreateInput,
-  EventEditInput,
-  createEventFrom,
-  editEventFrom,
-  deleteEvent,
-} from "./generated/event_base";
-import { ID, Viewer } from "ent/ent";
+import { EventBase } from "./generated/event_base";
 import { PrivacyPolicy, AlwaysAllowRule } from "ent/privacy";
 
 // we're only writing this once except with --force and packageName provided
@@ -14,22 +6,4 @@ export default class Event extends EventBase {
   privacyPolicy: PrivacyPolicy = {
     rules: [AlwaysAllowRule],
   };
-}
-
-// no actions yet so we support full create, edit, delete for now
-export { EventCreateInput, EventEditInput, deleteEvent };
-
-export async function createEvent(
-  viewer: Viewer,
-  input: EventCreateInput,
-): Promise<Event | null> {
-  return createEventFrom(viewer, input, Event);
-}
-
-export async function editEvent(
-  viewer: Viewer,
-  id: ID,
-  input: EventEditInput,
-): Promise<Event | null> {
-  return editEventFrom(viewer, id, input, Event);
 }
