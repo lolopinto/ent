468613a9bb727b3fab39b95c73671410007e9005
Ola Okelola <ola@okelola.com>
2020-04-27 15:07:04 -0700

    use webpack to build
    steps are `npm run build`
    
    and then `node lib/main.js` to get an outcome going

---
+++ ts/examples/simple/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+//const glob = require("glob");
+
+module.exports = {
+  // entry: glob.sync("./**/*.ts", {
+  //   ignore: [".*/node_modules/**", "./tests/**"],
+  // }),
+  entry: "./src/index.ts",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      ent: path.resolve(__dirname, "../../src/"),
+      src: path.resolve(__dirname, "./src/"),
+    },
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "[name].js",
+  },
+  plugins: [
+    new CopyPlugin([{ from: "config", to: path.resolve(__dirname, "lib") }]),
+  ],
+  mode: process.node_env || "development",
+};
