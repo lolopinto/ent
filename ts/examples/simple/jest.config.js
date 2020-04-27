6830ee9a662d3d788e4393557ad9906f6725417d
Ola Okelola <ola@okelola.com>
2020-04-27 16:21:58 -0700

    have jest.config.js and webpack.config.js both have their maps be taken from code

--- ts/examples/simple/jest.config.js
+++ ts/examples/simple/jest.config.js
@@ -1,14 +1,10 @@
+let moduleMapper = require("./start").getJestModuleMapper();
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(tsx?)$",
   rootDir: ".",
-  moduleNameMapper: {
-    // corresponding change from paths in tsconfig.json
-    // only map ent/ at beginning instead of relative ents which are the generated ents
-    "^ent/(.*)": "<rootDir>/../../src/$1",
-    // map to src/ so that we don't have relative paths like "./../"
-    "^src/(.*)": "<rootDir>/src/$1",
-  },
+  moduleNameMapper: moduleMapper,
   setupFilesAfterEnv: ["jest-expect-message"],
 };
--- ts/examples/simple/start.js
+++ ts/examples/simple/start.js
@@ -2,69 +2,120 @@ const fs = require("fs");
 const JSON5 = require("json5");
 const path = require("path");
 
-function getWebpackAlias() {
-  let alias = {};
-  // grab the paths from tsconfig.json instead of copying this code multiple times
+let json = null;
+let paths = undefined;
+function readJSONFile() {
+  if (json !== null) {
+    return json;
+  }
 
   try {
-    //https://www.typescriptlang.org/docs/handbook/module-resolution.html#path-mapping
-    // TODO: don't really use the * case well so need to write tests for it
-    function convertPath(path) {
-      let idx = path.lastIndexOf("/");
-      if (idx !== -1) {
-        path = path.substr(0, idx);
-      }
-      return path;
-    }
-
-    let options = JSON5.parse(
+    json = JSON5.parse(
       fs.readFileSync("./tsconfig.json", {
         encoding: "utf8",
       }),
     );
-    let paths;
-    if (options.compilerOptions && options.compilerOptions.paths) {
-      paths = options.compilerOptions.paths;
-    }
-    if (paths) {
-      for (let key in paths) {
-        let values = paths[key];
-        if (!Array.isArray(values)) {
-          console.error("format of value is not as expected");
-          break;
-        }
-
-        // special case. come back
-        if (key === "*") {
-          values.forEach((value) => {
-            if (value === "*") {
-              return;
-            }
-            let aliasKey = value;
-            let idx = value.indexOf("/");
-            if (idx !== -1) {
-              alias = value.substr(0, idx);
-            }
-            alias[aliasKey] = convertPath(value);
-          });
-        } else {
-          key = convertPath(key);
-
-          values.forEach((value) => {
-            value = convertPath(value);
-            console.log(value);
-
-            alias[key] = path.resolve(__dirname, value);
-          });
-        }
-      }
-    }
   } catch (e) {
     console.error("couldn't read tsconfig.json file");
+    json = {};
   }
-  return alias;
+  return json;
+}
+
+function getPaths() {
+  if (paths !== undefined) {
+    return paths;
+  }
+  let options = readJSONFile();
+  if (options.compilerOptions && options.compilerOptions.paths) {
+    paths = options.compilerOptions.paths;
+  } else {
+    paths = null;
+  }
+  return paths;
+}
+
+function convertStarValue(value) {
+  let aliasKey = starValue;
+  let idx = starValue.indexOf("/");
+  if (idx !== -1) {
+    aliasKey = starValue.substr(0, idx);
+  }
+  return aliasKey;
+}
+
+function getWebpackAlias() {
+  function convertPath(path) {
+    let idx = path.lastIndexOf("/");
+    if (idx !== -1) {
+      path = path.substr(0, idx);
+    }
+    return path;
+  }
+
+  return processPaths(
+    convertPath,
+    (value) => {
+      value = convertPath(value);
+      return path.resolve(__dirname, value);
+    },
+    convertStarValue,
+  );
+}
+
+function getJestModuleMapper() {
+  return processPaths(
+    (key) => {
+      return `^${key.replace("*", "(.*)")}`;
+    },
+    (value) => {
+      // remove leading paths: ./
+      if (value.startsWith("./")) {
+        value = value.substr(2);
+      }
+      value = "<rootDir>/" + value;
+      return value.replace("*", "$1");
+    },
+    convertStarValue,
+  );
+}
+
+function processPaths(transformKey, transformValue, transformStarValue) {
+  let result = {};
+  let paths = getPaths();
+  if (!paths) {
+    return result;
+  }
+
+  for (let key in paths) {
+    let values = paths[key];
+    if (!Array.isArray(values)) {
+      console.error("format of value is not as expected");
+      break;
+    }
+
+    // special case. come back
+    if (key === "*") {
+      values.forEach((value) => {
+        if (value === "*") {
+          return;
+        }
+        let aliasKey = transformStarValue(value);
+        value = transformValue(value);
+        result[aliasKey] = value;
+      });
+    } else {
+      key = transformKey(key);
+
+      values.forEach((value) => {
+        result[key] = transformValue(value);
+      });
+    }
+  }
+  return result;
 }
 
 module.exports = {
   getWebpackAlias,
+  getJestModuleMapper,
 };
