b1f8782434980798e863bbf24d8aaa90dcb1901d
lolopinto <ola@okelola.com>
2020-05-19 01:16:05 -0700

    fix for when we're trying to import a file that's higher in the directory tree that looks like it's a parent of current (#62)

--- ts/examples/simple/src/index.ts
+++ ts/examples/simple/src/index.ts
@@ -1,12 +1,22 @@
-import User from "src/ent/user";
+import User from "src/ent/user.ts"; // default removes the suffix and transforms it...
 import { IDViewer } from "src/util/id_viewer";
-async function load() {
-  return await User.load(
+import CreateContactAction from "./ent/contact/actions/create_contact_action";
+import { randomEmail } from "./util/random";
+
+async function loadUserAndCreate() {
+  let user = await User.loadX(
     new IDViewer("6910e947-7bc1-4ed2-b8a8-5a07e6708830"),
     "6910e947-7bc1-4ed2-b8a8-5a07e6708830",
   );
+
+  return await CreateContactAction.create(user.viewer, {
+    firstName: "firstName",
+    lastName: "lastName",
+    emailAddress: randomEmail(),
+    userID: user.id,
+  }).saveX()
 }
 
-load().then((user) => {
+loadUserAndCreate().then((user) => {
   console.log(user);
 });
