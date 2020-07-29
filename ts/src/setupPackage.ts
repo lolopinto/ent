cb0f695b538a1e89ed8b65d98199a7cc9891a5fc
Ola Okelola <ola@okelola.com>
2020-07-29 15:23:15 -0700

    write little script to publish
    `npm run actual-publish` is what's needed for now

---
+++ ts/src/setupPackage.ts
@@ -0,0 +1,32 @@
+import * as fs from "fs";
+import path from "path";
+
+// root is for some reason "ts/"
+const root = path.resolve();
+
+// DO NOT DELETE THIS FILE
+// This file is used by build system to build a clean npm package with the compiled js files in the root of the package.
+// It will not be included in the npm package.
+// gotten from https://stackoverflow.com/questions/38935176/how-to-npm-publish-specific-folder-but-as-package-root
+function main() {
+  const source = fs
+    .readFileSync(path.join(root, "package.json"))
+    .toString("utf-8");
+  const sourceObj = JSON.parse(source);
+  sourceObj.scripts = {};
+  sourceObj.devDependencies = {};
+  if (sourceObj.main.startsWith("dist/")) {
+    sourceObj.main = sourceObj.main.slice(5);
+  }
+  fs.writeFileSync(
+    path.join(root, "dist", "/package.json"),
+    Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8"),
+  );
+
+  fs.copyFileSync(
+    path.join(root, ".npmignore"),
+    path.join(root, "dist", ".npmignore"),
+  );
+}
+
+main();
