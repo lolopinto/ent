lolopinto/ent

commit 11073c9301f338a1a1c232ffb1fe81c1b47cfb48

changes to make package importable from different repo (#80)
* show work

* use np

* wha

* changes

* write little script to publish

`npm run actual-publish` is what's needed for now

* don't run npm pack then npm publish as we end up with the tarball in the package

* time to attempt to publish v0.08

* move everything arround to get the format I want

this ends with the following top-level-imports:

"@lolopinto/ent"
"@lolopinto/ent/schema"
"@lolopinto/ent/graphql" (custom imports)
"@lolopinto/ent/auth" (TODO)
"@lolopinto/ent/action"

and will end up creating packages for phonenumber, email, password soon

* fix some paths

* update .npmignore

* fix some typos

* query exported

* add the privacy exports

* python changes

- add Pipfile
- call with pipenv from said directory

this needs a lot more work...

* everything working together

* remove comment

* remove TODO

* fix pending TODO in db_schema.go

* add `tsent upgrade`. run it and confirm no changes needed
* update go callsites. alas, didn't test those...

* bump to 0.0.9

* bump package-lock

* some changes:

1/ add GraphQLBoolean to object.tmpl
2/ reflect-metadata is not a dev dependency but a real dependency
3/ fix path of custom_graphql script

* bump to 0.10 because of the reflect-metadata change

internal/schema/input/parse_ts.go
@@ -8,7 +8,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
-	"strings"
 	"time"
 
 	"github.com/iancoleman/strcase"
@@ -91,24 +90,14 @@ type schemaData struct {
 	Path string
 }
 
-func GetAbsoluteSchemaPath() string {
-	schemaPath := util.GetAbsolutePath("../../../ts/src/schema.ts")
-	// trim the suffix for the import
-	schemaPath = strings.TrimSuffix(schemaPath, ".ts")
-	return schemaPath
-}
-
 func writeTsFile(fileToWrite string, schemas []schemaData) error {
-	schemaPath := GetAbsoluteSchemaPath()
 
 	return file.Write(
 		&file.TemplatedBasedFileWriter{
 			Data: struct {
-				Schemas    []schemaData
-				SchemaPath string
+				Schemas []schemaData
 			}{
 				schemas,
-				schemaPath,
 			},
 			AbsPathToTemplate: util.GetAbsolutePath("read_schema.tmpl"),
 			TemplateName:      "read_schema.tmpl",

internal/schema/input/read_schema.tmpl
@@ -1,5 +1,5 @@
 {{/* for now we are hardcoding to where this currently is. we should be better about this */ -}}
-import Schema, {Field, Edge, AssocEdge, AssocEdgeGroup} from "{{.SchemaPath}}"
+import {Schema, Field, Edge, AssocEdge, AssocEdgeGroup} from "@lolopinto/ent/schema";
 
 {{ range $schema := .Schemas -}}
   import {{$schema.Name}} from "./{{$schema.Path}}"

commit 3bc66091aa49ff72c9fa5a7423c9742f6415a89f

fix schema tests (in go)
broken by https://github.com/lolopinto/ent/pull/80/files

still need a real test suite that tests go and typescript

internal/schema/input/parse_ts.go
@@ -2,12 +2,14 @@ package input
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/iancoleman/strcase"
@@ -56,7 +58,7 @@ func ParseSchemaFromTSDir(dirPath string, fromTest bool) (*Schema, error) {
 
 	fileName := filepath.Join(schemaPath, fmt.Sprintf("%d_read_schema.ts", time.Now().Unix()))
 
-	err = writeTsFile(fileName, schemas)
+	err = writeTsFile(fileName, schemas, fromTest)
 	defer os.Remove(fileName)
 
 	if err != nil {
@@ -67,7 +69,11 @@ func ParseSchemaFromTSDir(dirPath string, fromTest bool) (*Schema, error) {
 	var execCmd exec.Cmd
 	if fromTest {
 		// no tsconfig-paths for tests...
-		execCmd = *exec.Command("ts-node", fileName)
+		opts, err := json.Marshal(map[string]interface{}{"lib": []string{"esnext", "dom"}})
+		if err != nil {
+			return nil, errors.Wrap(err, "error creating json compiler options")
+		}
+		execCmd = *exec.Command("ts-node", "--compiler-options", string(opts), fileName)
 	} else {
 		execCmd = *exec.Command("ts-node", "-r", "tsconfig-paths/register", fileName)
 	}
@@ -90,14 +96,31 @@ type schemaData struct {
 	Path string
 }
 
-func writeTsFile(fileToWrite string, schemas []schemaData) error {
+func GetAbsoluteSchemaPathForTest() string {
+	schemaPath := util.GetAbsolutePath("../../../ts/src/schema/index.ts")
+	// trim the suffix for the import
+	schemaPath = strings.TrimSuffix(schemaPath, ".ts")
+	return schemaPath
+}
+
+func writeTsFile(fileToWrite string, schemas []schemaData, fromTest bool) error {
+	var schemaPath string
+	if fromTest {
+		// in tests, we don't have @lolopinto/ent installed so for now
+		// since no node_modules (for now) so we use absolute path
+		schemaPath = GetAbsoluteSchemaPathForTest()
+	} else {
+		schemaPath = "@lolopinto/ent/schema"
+	}
 
 	return file.Write(
 		&file.TemplatedBasedFileWriter{
 			Data: struct {
-				Schemas []schemaData
+				Schemas    []schemaData
+				SchemaPath string
 			}{
 				schemas,
+				schemaPath,
 			},
 			AbsPathToTemplate: util.GetAbsolutePath("read_schema.tmpl"),
 			TemplateName:      "read_schema.tmpl",

internal/schema/input/read_schema.tmpl
@@ -1,5 +1,4 @@
-{{/* for now we are hardcoding to where this currently is. we should be better about this */ -}}
-import {Schema, Field, Edge, AssocEdge, AssocEdgeGroup} from "@lolopinto/ent/schema";
+import {Schema, Field, Edge, AssocEdge, AssocEdgeGroup} from "{{.SchemaPath}}";
 
 {{ range $schema := .Schemas -}}
   import {{$schema.Name}} from "./{{$schema.Path}}"

internal/schema/input/parse_ts_action_test.go
@@ -9,12 +9,11 @@ import (
 
 func TestParseActions(t *testing.T) {
 	testCases := map[string]testCase{
-		"mutations action": testCase{
+		"mutations action": {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import Schema, {Action, Field, ActionOperation} from "{schema}";
-				import {StringType, TimeType} from "{field}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
@@ -30,31 +29,30 @@ func TestParseActions(t *testing.T) {
 				};`),
 			},
 			expectedOutput: map[string]node{
-				"Event": node{
+				"Event": {
 					fields: []field{
-						field{
+						{
 							name:   "name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:   "start_time",
 							dbType: input.Time,
 						},
 					},
 					actions: []action{
-						action{
+						{
 							operation: ent.MutationsAction,
 						},
 					},
 				},
 			},
 		},
-		"delete action": testCase{
+		"delete action": {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import Schema, {Action, Field, ActionOperation} from "{schema}";
-				import {StringType, TimeType} from "{field}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
@@ -70,31 +68,30 @@ func TestParseActions(t *testing.T) {
 				};`),
 			},
 			expectedOutput: map[string]node{
-				"Event": node{
+				"Event": {
 					fields: []field{
-						field{
+						{
 							name:   "name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:   "start_time",
 							dbType: input.Time,
 						},
 					},
 					actions: []action{
-						action{
+						{
 							operation: ent.DeleteAction,
 						},
 					},
 				},
 			},
 		},
-		"edit action": testCase{
+		"edit action": {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import Schema, {Action, Field, ActionOperation} from "{schema}";
-				import {StringType, TimeType} from "{field}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
@@ -117,24 +114,24 @@ func TestParseActions(t *testing.T) {
 				};`),
 			},
 			expectedOutput: map[string]node{
-				"Event": node{
+				"Event": {
 					fields: []field{
-						field{
+						{
 							name:   "name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:   "start_time",
 							dbType: input.Time,
 						},
-						field{
+						{
 							name:     "end_time",
 							dbType:   input.Time,
 							nullable: true,
 						},
 					},
 					actions: []action{
-						action{
+						{
 							operation: ent.EditAction,
 							fields: []string{
 								"start_time",
@@ -147,12 +144,11 @@ func TestParseActions(t *testing.T) {
 				},
 			},
 		},
-		"create action": testCase{
+		"create action": {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import Schema, {Action, Field, ActionOperation} from "{schema}";
-				import {StringType, TimeType} from "{field}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
@@ -168,19 +164,19 @@ func TestParseActions(t *testing.T) {
 				};`),
 			},
 			expectedOutput: map[string]node{
-				"Event": node{
+				"Event": {
 					fields: []field{
-						field{
+						{
 							name:   "name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:   "start_time",
 							dbType: input.Time,
 						},
 					},
 					actions: []action{
-						action{
+						{
 							operation: ent.CreateAction,
 						},
 					},

internal/schema/input/parse_ts_edge_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestParseEdges(t *testing.T) {
 	testCases := map[string]testCase{
-		"unique with inverse edge": testCase{
+		"unique with inverse edge": {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import Schema, {Field, Edge} from "{schema}";
+				import {Schema, Field, Edge} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [];
@@ -30,10 +30,10 @@ func TestParseEdges(t *testing.T) {
 				};`),
 			},
 			expectedOutput: map[string]node{
-				"Event": node{
+				"Event": {
 					fields: []field{},
 					assocEdges: []assocEdge{
-						assocEdge{
+						{
 							name:       "creator",
 							schemaName: "User",
 							unique:     true,
@@ -46,12 +46,11 @@ func TestParseEdges(t *testing.T) {
 			},
 		},
 		// shown to contrast with unique above
-		"field edge with inverse edge": testCase{
+		"field edge with inverse edge": {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import Schema, {Field, Edge} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, Edge, StringType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
@@ -60,7 +59,7 @@ func TestParseEdges(t *testing.T) {
 				};`),
 				"user.ts": getCodeWithSchema(
 					`
-				import Schema, {Field, Edge} from "{schema}";
+				import {Schema, Field, Edge} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [];
@@ -73,18 +72,18 @@ func TestParseEdges(t *testing.T) {
 				};`),
 			},
 			expectedOutput: map[string]node{
-				"Event": node{
+				"Event": {
 					fields: []field{
-						field{
+						{
 							name:      "creatorID",
 							fieldEdge: &[2]string{"User", "createdEvents"},
 							dbType:    input.String,
 						},
 					},
 				},
-				"User": node{
+				"User": {
 					assocEdges: []assocEdge{
-						assocEdge{
+						{
 							name:       "createdEvents",
 							schemaName: "Event",
 						},
@@ -92,11 +91,11 @@ func TestParseEdges(t *testing.T) {
 				},
 			},
 		},
-		"symmetric edge": testCase{
+		"symmetric edge": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(
 					`
-					import Schema, {Field, Edge} from "{schema}";
+					import {Schema, Field, Edge} from "{schema}";
 	
 					export default class User implements Schema {
 						fields: Field[] = [];
@@ -111,10 +110,10 @@ func TestParseEdges(t *testing.T) {
 					};`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{},
 					assocEdges: []assocEdge{
-						assocEdge{
+						{
 							name:       "friends",
 							schemaName: "User",
 							symmetric:  true,
@@ -123,11 +122,11 @@ func TestParseEdges(t *testing.T) {
 				},
 			},
 		},
-		"one-way edge": testCase{
+		"one-way edge": {
 			code: map[string]string{
 				"post.ts": getCodeWithSchema(
 					`
-					import Schema, {Field, Edge} from "{schema}";
+					import {Schema, Field, Edge} from "{schema}";
 	
 					export default class Post implements Schema {
 						fields: Field[] = [];
@@ -141,10 +140,10 @@ func TestParseEdges(t *testing.T) {
 					};`),
 			},
 			expectedOutput: map[string]node{
-				"Post": node{
+				"Post": {
 					fields: []field{},
 					assocEdges: []assocEdge{
-						assocEdge{
+						{
 							name:       "likers",
 							schemaName: "User",
 						},
@@ -152,11 +151,11 @@ func TestParseEdges(t *testing.T) {
 				},
 			},
 		},
-		"edge group": testCase{
+		"edge group": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(
 					`
-					import Schema, {Field, Edge} from "{schema}";
+					import {Schema, Field, Edge} from "{schema}";
 	
 					export default class User implements Schema {
 						fields: Field[] = [];
@@ -184,21 +183,21 @@ func TestParseEdges(t *testing.T) {
 					};`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{},
 					assocEdgeGroups: []assocEdgeGroup{
-						assocEdgeGroup{
+						{
 							name:            "friendships",
 							groupStatusName: "friendshipStatus",
 							assocEdges: []assocEdge{
-								assocEdge{
+								{
 									name:       "friendRequests",
 									schemaName: "User",
 									inverseEdge: &inverseAssocEdge{
 										name: "friendRequestsReceived",
 									},
 								},
-								assocEdge{
+								{
 									name:       "friends",
 									schemaName: "User",
 									symmetric:  true,

internal/schema/input/parse_ts_field_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestParseFields(t *testing.T) {
 	testCases := map[string]testCase{
-		"node with implicit schema": testCase{
+		"node with implicit schema": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
 				import { DBType } from "{schema}";
@@ -27,9 +27,9 @@ func TestParseFields(t *testing.T) {
 			export default User`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:   "FirstName",
 							dbType: input.String,
 						},
@@ -37,11 +37,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"node with explicit schema": testCase{
+		"node with explicit schema": {
 			code: map[string]string{
 				"address.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class Address implements Schema {
 					tableName: string = "addresses";
@@ -53,14 +52,14 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"Address": node{
+				"Address": {
 					tableName: "addresses",
 					fields: []field{
-						field{
+						{
 							name:   "street_name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:   "city",
 							dbType: input.String,
 						},
@@ -68,11 +67,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"nullable field": testCase{
+		"nullable field": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -81,9 +79,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:     "bio",
 							dbType:   input.String,
 							nullable: true,
@@ -92,11 +90,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"renamed storageKey": testCase{
+		"renamed storageKey": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -105,9 +102,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:       "bio",
 							dbType:     input.String,
 							storageKey: "about_me",
@@ -116,11 +113,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"renamed graphqlName": testCase{
+		"renamed graphqlName": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -129,9 +125,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:        "bio",
 							dbType:      input.String,
 							graphqlName: "aboutMe",
@@ -140,11 +136,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"unique": testCase{
+		"unique": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -153,9 +148,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:   "email",
 							dbType: input.String,
 							unique: true,
@@ -164,11 +159,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"hideFromGraphQL": testCase{
+		"hideFromGraphQL": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -177,9 +171,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:            "password",
 							dbType:          input.String,
 							hideFromGraphQL: true,
@@ -188,11 +182,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"private field": testCase{
+		"private field": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -201,9 +194,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:    "password",
 							dbType:  input.String,
 							private: true,
@@ -212,11 +205,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"index": testCase{
+		"index": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, StringType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -225,9 +217,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:   "last_name",
 							dbType: input.String,
 							index:  true,
@@ -236,11 +228,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"server default": testCase{
+		"server default": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field} from "{schema}";
-				import {TimeType} from "{field}";
+				import {Schema, Field, TimeType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
@@ -249,9 +240,9 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:          "updated_at",
 							dbType:        input.Time,
 							serverDefault: "now()",
@@ -260,11 +251,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"with base schema": testCase{
+		"with base schema": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field, BaseEntSchema} from "{schema}";
-				import {StringType} from "{field}";
+				import {Schema, Field, BaseEntSchema, StringType} from "{schema}";
 
 				export default class User extends BaseEntSchema implements Schema {
 					fields: Field[] = [
@@ -273,23 +263,23 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:                    "ID",
 							dbType:                  input.UUID,
 							primaryKey:              true,
 							disableUserEditable:     true,
 							hasDefaultValueOnCreate: true,
 						},
-						field{
+						{
 							name:                    "createdAt",
 							dbType:                  input.Time,
 							hideFromGraphQL:         true,
 							disableUserEditable:     true,
 							hasDefaultValueOnCreate: true,
 						},
-						field{
+						{
 							name:                    "updatedAt",
 							dbType:                  input.Time,
 							hideFromGraphQL:         true,
@@ -297,7 +287,7 @@ func TestParseFields(t *testing.T) {
 							hasDefaultValueOnCreate: true,
 							hasDefaultValueOnEdit:   true,
 						},
-						field{
+						{
 							name:   "firstName",
 							dbType: input.String,
 						},
@@ -305,11 +295,10 @@ func TestParseFields(t *testing.T) {
 				},
 			},
 		},
-		"multiple files/complicated": testCase{
+		"multiple files/complicated": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import Schema, {Field, BaseEntSchema} from "{schema}"
-				import {UUIDType, StringType} from "{field}";
+				import {Schema, Field, BaseEntSchema, UUIDType, StringType } from "{schema}"
 
 				export default class User extends BaseEntSchema implements Schema {
 					fields: Field[] = [
@@ -320,8 +309,7 @@ func TestParseFields(t *testing.T) {
 					]
 				}`),
 				"event.ts": getCodeWithSchema(`
-				import Schema, {BaseEntSchema, Field} from "{schema}"
-				import {TimeType, StringType, UUIDType} from "{field}";
+				import {Schema, BaseEntSchema, Field, TimeType, StringType, UUIDType} from "{schema}"
 
 				export default class Event extends BaseEntSchema implements Schema {
 					fields: Field[] = [
@@ -334,23 +322,23 @@ func TestParseFields(t *testing.T) {
 				}`),
 			},
 			expectedOutput: map[string]node{
-				"User": node{
+				"User": {
 					fields: []field{
-						field{
+						{
 							name:                    "ID",
 							dbType:                  input.UUID,
 							primaryKey:              true,
 							disableUserEditable:     true,
 							hasDefaultValueOnCreate: true,
 						},
-						field{
+						{
 							name:                    "createdAt",
 							dbType:                  input.Time,
 							hideFromGraphQL:         true,
 							disableUserEditable:     true,
 							hasDefaultValueOnCreate: true,
 						},
-						field{
+						{
 							name:                    "updatedAt",
 							dbType:                  input.Time,
 							hideFromGraphQL:         true,
@@ -358,20 +346,20 @@ func TestParseFields(t *testing.T) {
 							hasDefaultValueOnCreate: true,
 							hasDefaultValueOnEdit:   true,
 						},
-						field{
+						{
 							name:   "first_name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:   "last_name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:   "email",
 							dbType: input.String,
 							unique: true,
 						},
-						field{
+						{
 							name:            "password",
 							dbType:          input.String,
 							private:         true,
@@ -379,23 +367,23 @@ func TestParseFields(t *testing.T) {
 						},
 					},
 				},
-				"Event": node{
+				"Event": {
 					fields: []field{
-						field{
+						{
 							name:                    "ID",
 							dbType:                  input.UUID,
 							primaryKey:              true,
 							disableUserEditable:     true,
 							hasDefaultValueOnCreate: true,
 						},
-						field{
+						{
 							name:                    "createdAt",
 							dbType:                  input.Time,
 							hideFromGraphQL:         true,
 							disableUserEditable:     true,
 							hasDefaultValueOnCreate: true,
 						},
-						field{
+						{
 							name:                    "updatedAt",
 							dbType:                  input.Time,
 							hideFromGraphQL:         true,
@@ -403,25 +391,25 @@ func TestParseFields(t *testing.T) {
 							hasDefaultValueOnCreate: true,
 							hasDefaultValueOnEdit:   true,
 						},
-						field{
+						{
 							name:   "name",
 							dbType: input.String,
 						},
-						field{
+						{
 							name:       "creator_id",
 							dbType:     input.UUID,
 							foreignKey: &[2]string{"User", "ID"},
 						},
-						field{
+						{
 							name:   "start_time",
 							dbType: input.Time,
 						},
-						field{
+						{
 							name:     "end_time",
 							dbType:   input.Time,
 							nullable: true,
 						},
-						field{
+						{
 							name:   "location",
 							dbType: input.String,
 						},

internal/schema/input/parse_ts_test.go
@@ -185,7 +185,7 @@ func assertStrEqual(t *testing.T, key, expectedValue string, value *string) {
 }
 
 func getCodeWithSchema(code string) string {
-	schemaPath := input.GetAbsoluteSchemaPath()
+	schemaPath := input.GetAbsoluteSchemaPathForTest()
 	fieldPath := strings.Replace(schemaPath, "schema", "field", 1)
 
 	r := strings.NewReplacer("{schema}", schemaPath, "{field}", fieldPath)