1f4397deb6901f520cf9c5169ce836cd3ba84db1
lolopinto <ola@okelola.com>
2020-10-21 12:08:48 -0700

    create_event_action should add creator as host also (#141)

--- ts/examples/simple/src/ent/event/actions/create_event_action.ts
+++ ts/examples/simple/src/ent/event/actions/create_event_action.ts
@@ -2,13 +2,24 @@ import {
   CreateEventActionBase,
   EventCreateInput,
 } from "src/ent/event/actions/generated/create_event_action_base";
-import { Validator } from "@lolopinto/ent/action";
+import { Trigger, Validator } from "@lolopinto/ent/action";
 import { SharedValidators } from "./event_validators";
 import Event from "src/ent/event";
+import { EventBuilder } from "./event_builder";
 
 export { EventCreateInput };
 
 // we're only writing this once except with --force and packageName provided
 export default class CreateEventAction extends CreateEventActionBase {
   validators: Validator<Event>[] = [...SharedValidators];
+
+  triggers: Trigger<Event>[] = [
+    {
+      changeset(builder: EventBuilder) {
+        let input = builder.getInput();
+        let creator = input.creatorID!;
+        builder.addHostID(creator);
+      },
+    },
+  ];
 }
