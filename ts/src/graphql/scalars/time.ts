8f1f3b8c43a5be6177bd0634d75ab8287dfd7a3f
Ola Okelola <ola@okelola.com>
2021-02-06 22:24:19 -0800

    update GraphQLTime to be better + support more things
    add dependeny on luxon. probably need to change a few things to use it

--- ts/src/graphql/scalars/time.ts
+++ ts/src/graphql/scalars/time.ts
@@ -1,23 +1,57 @@
-import { GraphQLScalarType } from "graphql";
+import { GraphQLScalarType, GraphQLError } from "graphql";
 import { Kind, ValueNode } from "graphql/language";
+import { DateTime } from "luxon";
 
 // Time refers to a Timestamp or Date scalar
+
+function parseValue(input: any): Date {
+  let dt: DateTime;
+  if (typeof input === "number") {
+    dt = DateTime.fromMillis(input);
+  } else if (typeof input === "string") {
+    dt = DateTime.fromISO(input);
+    if (!dt.isValid) {
+      let ms = Date.parse(input);
+      if (ms === NaN) {
+        throw new GraphQLError(`invalid input for type Time ${input}`);
+      }
+      dt = DateTime.fromMillis(ms);
+    }
+  } else if (input instanceof Date) {
+    return input;
+  } else if (input instanceof DateTime) {
+    dt = input;
+  } else {
+    throw new GraphQLError(`invalid input for type Time ${input}`);
+  }
+  if (!dt.isValid) {
+    throw new GraphQLError(`invalid input for type Time ${input}`);
+  }
+  return dt.toJSDate();
+}
+
+// Date or Timestamp
+// Need feedback. should this be renamed to Timestamp?
+// This is called Time but only supports Date and Timestamp values
+// and `Time` isn't supported. that's a string
 export const GraphQLTime = new GraphQLScalarType({
   name: "Time",
   description: "Time scalar type",
   serialize: (outputValue: any) => {
-    //console.log("serialize", outputValue, typeof outputValue);
-    return new Date(outputValue).toISOString();
+    if (outputValue instanceof Date) {
+      return outputValue.toISOString();
+    } else if (outputValue instanceof DateTime) {
+      return outputValue.toUTC().toISO();
+    }
+    return parseValue(outputValue).toISOString();
   },
-  parseValue: (input: any) => {
-    //    console.log("parseValue", input);
-    return input.getTime();
-  },
+  parseValue: parseValue,
   parseLiteral: (ast: ValueNode) => {
-    //  console.log("literal", ast);
+    //    console.log("literal", ast);
     if (ast.kind === Kind.INT) {
       return new Date(+ast.value);
     }
-    return null;
+    // TODO?
+    throw new GraphQLError(`Time cannot represent literal value ${ast}`);
   },
 });
