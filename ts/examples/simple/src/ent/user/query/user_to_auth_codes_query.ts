cd1ffd26acee1722c0b36805ce56bbc6d996ee22
Ola Okelola <ola@okelola.com>
2021-02-12 15:29:06 -0800

    generate non-base classes for edge files

---
+++ ts/examples/simple/src/ent/user/query/user_to_auth_codes_query.ts
@@ -0,0 +1,2 @@
+import { UserToAuthCodesQueryBase } from "src/ent/internal";
+export class UserToAuthCodesQuery extends UserToAuthCodesQueryBase {}
---
+++ ts/examples/simple/src/ent/user/query/user_to_contacts_query.ts
@@ -0,0 +1,2 @@
+import { UserToContactsQueryBase } from "src/ent/internal";
+export class UserToContactsQuery extends UserToContactsQueryBase {}
