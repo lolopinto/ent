a1fa183167bbe59567269e48d230e00628387549
Ola Okelola <ola@okelola.com>
2021-02-17 10:24:38 -0800

    fix some backend ish.
    remove dangling file
    fix tsconfig.json output location.
    change PassportStrategyHandler to get @lolopinto/ent-passport

--- examples/ent-rsvp/backend/src/graphql/index.ts
+++ examples/ent-rsvp/backend/src/graphql/index.ts
@@ -2,11 +2,8 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema";
 import { IncomingMessage, ServerResponse } from "http";
-import {
-  buildContext,
-  registerAuthHandler,
-  PassportStrategyHandler,
-} from "@lolopinto/ent/auth";
+import { buildContext, registerAuthHandler } from "@lolopinto/ent/auth";
+import { PassportStrategyHandler } from "@lolopinto/ent-passport";
 import passport from "passport";
 import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
 import { IDViewer } from "@lolopinto/ent";
