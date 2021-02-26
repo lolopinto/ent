lolopinto/ent

commit 3cc65ada8dba94fc63744d2264bf87f34c10cb4f

better UTC support for timestamp (#219)

ts/src/core/ent.ts
@@ -253,15 +253,13 @@ export function createDataLoader(options: SelectDataOptions) {
     // TODO is there a better way of doing this?
     // context not needed because we're creating a loader which has its own cache which is being used here
     const nodes = await loadRows(rowOptions);
-    let result: Data[] = [];
-    ids.forEach((id) => {
+    let result: (Data | Error)[] = ids.map((id) => {
       for (const node of nodes) {
         if (node[col] === id) {
-          result.push(node);
-          return;
+          return node;
         }
       }
-      return null;
+      return new Error(`couldn't find data for row ${id}`);
     });
 
     return result;
@@ -552,11 +550,11 @@ export class EdgeOperation implements DataOperation {
       data: edge.data || null,
     };
     if (edge.time) {
-      fields["time"] = edge.time;
+      fields["time"] = edge.time.toISOString();
     } else {
       // todo make this a schema field like what we do in generated base files...
       // maybe when actions exist?
-      fields["time"] = new Date();
+      fields["time"] = new Date().toISOString();
     }
     if (edge.data) {
       fields["data"] = edge.data;

internal/action/action_test.go
@@ -662,12 +662,12 @@ func TestActionOnlyFields(t *testing.T) {
 		map[string]string{
 			"event.ts": testhelper.GetCodeWithSchema(
 				`
-				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimestampType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
 						StringType({name: "name"}),
-						TimeType({name: "start_time"}),
+						TimestampType({name: "start_time"}),
 					];
 
 					actions: Action[] = [

internal/schema/input/input.go
@@ -45,7 +45,7 @@ const (
 	Int               = "Int"
 	Float             = "Float"
 	String            = "String"
-	Time              = "Time"
+	Timestamp         = "Timestamp"
 	JSON              = "JSON"
 	Enum              = "Enum"
 	StringEnum        = "StringEnum"
@@ -126,7 +126,7 @@ func (f *Field) GetEntType() enttype.EntType {
 			return &enttype.NullableStringType{}
 		}
 		return &enttype.StringType{}
-	case Time:
+	case Timestamp:
 		if f.Nullable {
 			return &enttype.NullableTimeType{}
 		}

internal/schema/input/parse_ts_action_test.go
@@ -13,12 +13,12 @@ func TestParseActions(t *testing.T) {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimestampType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
 						StringType({name: "name"}),
-						TimeType({name: "start_time"}),
+						TimestampType({name: "start_time"}),
 					];
 
 					actions: Action[] = [
@@ -37,7 +37,7 @@ func TestParseActions(t *testing.T) {
 						},
 						{
 							name:   "start_time",
-							dbType: input.Time,
+							dbType: input.Timestamp,
 						},
 					},
 					actions: []action{
@@ -52,12 +52,12 @@ func TestParseActions(t *testing.T) {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimestampType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
 						StringType({name: "name"}),
-						TimeType({name: "start_time"}),
+						TimestampType({name: "start_time"}),
 					];
 
 					actions: Action[] = [
@@ -76,7 +76,7 @@ func TestParseActions(t *testing.T) {
 						},
 						{
 							name:   "start_time",
-							dbType: input.Time,
+							dbType: input.Timestamp,
 						},
 					},
 					actions: []action{
@@ -91,13 +91,13 @@ func TestParseActions(t *testing.T) {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimestampType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
 						StringType({name: "name"}),
-						TimeType({name: "start_time"}),
-						TimeType({name: "end_time", nullable: true}),
+						TimestampType({name: "start_time"}),
+						TimestampType({name: "end_time", nullable: true}),
 					];
 
 					actions: Action[] = [
@@ -122,11 +122,11 @@ func TestParseActions(t *testing.T) {
 						},
 						{
 							name:   "start_time",
-							dbType: input.Time,
+							dbType: input.Timestamp,
 						},
 						{
 							name:     "end_time",
-							dbType:   input.Time,
+							dbType:   input.Timestamp,
 							nullable: true,
 						},
 					},
@@ -148,12 +148,12 @@ func TestParseActions(t *testing.T) {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimestampType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
 						StringType({name: "name"}),
-						TimeType({name: "start_time"}),
+						TimestampType({name: "start_time"}),
 					];
 
 					actions: Action[] = [
@@ -172,7 +172,7 @@ func TestParseActions(t *testing.T) {
 						},
 						{
 							name:   "start_time",
-							dbType: input.Time,
+							dbType: input.Timestamp,
 						},
 					},
 					actions: []action{
@@ -187,12 +187,12 @@ func TestParseActions(t *testing.T) {
 			code: map[string]string{
 				"event.ts": getCodeWithSchema(
 					`
-				import {Schema, Action, Field, ActionOperation, StringType, TimeType} from "{schema}";
+				import {Schema, Action, Field, ActionOperation, StringType, TimestampType} from "{schema}";
 
 				export default class Event implements Schema {
 					fields: Field[] = [
 						StringType({name: "name"}),
-						TimeType({name: "start_time"}),
+						TimestampType({name: "start_time"}),
 					];
 
 					actions: Action[] = [
@@ -220,7 +220,7 @@ func TestParseActions(t *testing.T) {
 						},
 						{
 							name:   "start_time",
-							dbType: input.Time,
+							dbType: input.Timestamp,
 						},
 					},
 					actions: []action{

internal/schema/input/parse_ts_field_test.go
@@ -231,11 +231,11 @@ func TestParseFields(t *testing.T) {
 		"server default": {
 			code: map[string]string{
 				"user.ts": getCodeWithSchema(`
-				import {Schema, Field, TimeType} from "{schema}";
+				import {Schema, Field, TimestampType} from "{schema}";
 
 				export default class User implements Schema {
 					fields: Field[] = [
-						TimeType({name: "updated_at", serverDefault: 'now()'}),
+						TimestampType({name: "updated_at", serverDefault: 'now()'}),
 					]
 				}`),
 			},
@@ -244,7 +244,7 @@ func TestParseFields(t *testing.T) {
 					fields: []field{
 						{
 							name:          "updated_at",
-							dbType:        input.Time,
+							dbType:        input.Timestamp,
 							serverDefault: "now()",
 						},
 					},
@@ -287,14 +287,14 @@ func TestParseFields(t *testing.T) {
 					]
 				}`),
 				"event.ts": getCodeWithSchema(`
-				import {Schema, BaseEntSchema, Field, TimeType, StringType, UUIDType} from "{schema}"
+				import {Schema, BaseEntSchema, Field, TimestampType, StringType, UUIDType} from "{schema}"
 
 				export default class Event extends BaseEntSchema implements Schema {
 					fields: Field[] = [
 						StringType({name: "name"}),
 						UUIDType({name: "creator_id", foreignKey: ["User", "ID"]}),
-						TimeType({name: "start_time"}),
-						TimeType({name: "end_time", nullable: true}),
+						TimestampType({name: "start_time"}),
+						TimestampType({name: "end_time", nullable: true}),
 						StringType({name: "location"}),
 					]
 				}`),
@@ -336,11 +336,11 @@ func TestParseFields(t *testing.T) {
 						},
 						field{
 							name:   "start_time",
-							dbType: input.Time,
+							dbType: input.Timestamp,
 						},
 						field{
 							name:     "end_time",
-							dbType:   input.Time,
+							dbType:   input.Timestamp,
 							nullable: true,
 						},
 						field{

internal/schema/input/parse_ts_test.go
@@ -319,14 +319,14 @@ func nodeFields() []field {
 		},
 		{
 			name:                    "createdAt",
-			dbType:                  input.Time,
+			dbType:                  input.Timestamp,
 			hideFromGraphQL:         true,
 			disableUserEditable:     true,
 			hasDefaultValueOnCreate: true,
 		},
 		{
 			name:                    "updatedAt",
-			dbType:                  input.Time,
+			dbType:                  input.Timestamp,
 			hideFromGraphQL:         true,
 			disableUserEditable:     true,
 			hasDefaultValueOnCreate: true,

ts/examples/simple/src/ent/generated/event_base.ts
@@ -28,19 +28,19 @@ import schema from "src/schema/event";
 const tableName = "events";
 
 export enum EventRsvpStatus {
-  Declined = "declined",
   Maybe = "maybe",
   Invited = "invited",
   Attending = "attending",
+  Declined = "declined",
   CanRsvp = "canRsvp",
 }
 
 export function getEventRsvpStatusValues() {
   return [
-    EventRsvpStatus.Declined,
     EventRsvpStatus.Maybe,
     EventRsvpStatus.Invited,
     EventRsvpStatus.Attending,
+    EventRsvpStatus.Declined,
     EventRsvpStatus.CanRsvp,
   ];
 }

ts/examples/simple/src/graphql/resolvers/generated/event_rsvp_status_type.ts
@@ -5,9 +5,6 @@ import { GraphQLEnumType } from "graphql";
 export const EventRsvpStatusType = new GraphQLEnumType({
   name: "EventRsvpStatus",
   values: {
-    DECLINED: {
-      value: "DECLINED",
-    },
     MAYBE: {
       value: "MAYBE",
     },
@@ -16,6 +13,9 @@ export const EventRsvpStatusType = new GraphQLEnumType({
     },
     ATTENDING: {
       value: "ATTENDING",
+    },
+    DECLINED: {
+      value: "DECLINED",
     },
     CAN_RSVP: {
       value: "CAN_RSVP",

commit c2b3edb7e26530946aaccf8609a0aaa1a3a8ecf9

fix loading data when row doesn't exist
broken by https://github.com/lolopinto/ent/pull/219

ts/src/core/context.ts
@@ -27,7 +27,7 @@ export interface RequestContext extends Context {
 }
 
 export class ContextCache {
-  loaders: Map<string, DataLoader<ID, Data>> = new Map();
+  loaders: Map<string, DataLoader<ID, Data | null>> = new Map();
 
   // only create as needed for the "requests" which need it
   getEntLoader(loaderOptions: SelectDataOptions) {

ts/src/core/ent.ts
@@ -179,7 +179,17 @@ export async function loadEnts<T extends Ent>(
   // TODO do we want this loader check all over the place?
   if (l) {
     const rows = await l.loadMany(ids);
-    m = await applyPrivacyPolicyForRows(viewer, rows, options);
+    let rows2: Data[] = [];
+    for (const row of rows) {
+      if (!row) {
+        continue;
+      }
+      if (row instanceof Error) {
+        throw row;
+      }
+      rows2.push(row);
+    }
+    m = await applyPrivacyPolicyForRows(viewer, rows2, options);
   } else {
     const col = options.pkey || "id";
     m = await loadEntsFromClause(viewer, clause.In(col, ...ids), options);
@@ -253,13 +263,13 @@ export function createDataLoader(options: SelectDataOptions) {
     // TODO is there a better way of doing this?
     // context not needed because we're creating a loader which has its own cache which is being used here
     const nodes = await loadRows(rowOptions);
-    let result: (Data | Error)[] = ids.map((id) => {
+    let result: (Data | null)[] = ids.map((id) => {
       for (const node of nodes) {
         if (node[col] === id) {
           return node;
         }
       }
-      return new Error(`couldn't find data for row ${id}`);
+      return null;
     });
 
     return result;
@@ -1026,7 +1036,17 @@ export async function loadEdgeDatas(
   }
 
   const rows = await assocEdgeLoader.loadMany(edgeTypes);
-  return new Map(rows.map((row) => [row["edge_type"], new AssocEdgeData(row)]));
+  const m = new Map<string, AssocEdgeData>();
+  rows.forEach((row) => {
+    if (!row) {
+      return;
+    }
+    if (row instanceof Error) {
+      throw row;
+    }
+    m.set(row["edge_type"], new AssocEdgeData(row));
+  });
+  return m;
 }
 
 const edgeFields = [

ts/src/core/privacy.ts
@@ -145,7 +145,7 @@ export const AllowIfHasIdentity = {
 
 export const AllowIfViewerRule = {
   async apply(v: Viewer, ent?: Ent): Promise<PrivacyResult> {
-    if (v.viewerID === ent?.id) {
+    if (v.viewerID && v.viewerID === ent?.id) {
       return Allow();
     }
     return Skip();

ts/src/testutils/builder.ts
@@ -1,9 +1,5 @@
 import { Ent, ID, Viewer, Data, EntConstructor } from "../core/ent";
-import {
-  PrivacyPolicy,
-  AlwaysAllowRule,
-  AlwaysAllowPrivacyPolicy,
-} from "../core/privacy";
+import { AlwaysAllowPrivacyPolicy } from "../core/privacy";
 import { Orchestrator } from "../action/orchestrator";
 import {
   Action,
@@ -25,9 +21,8 @@ export class User implements Ent {
   id: ID;
   accountID: string;
   nodeType = "User";
-  privacyPolicy: PrivacyPolicy = {
-    rules: [AlwaysAllowRule],
-  };
+  privacyPolicy = AlwaysAllowPrivacyPolicy;
+
   constructor(public viewer: Viewer, id: ID, public data: Data) {
     this.id = id;
   }
@@ -37,9 +32,8 @@ export class Event implements Ent {
   id: ID;
   accountID: string;
   nodeType = "Event";
-  privacyPolicy: PrivacyPolicy = {
-    rules: [AlwaysAllowRule],
-  };
+  privacyPolicy = AlwaysAllowPrivacyPolicy;
+
   constructor(public viewer: Viewer, id: ID, public data: Data) {
     this.id = id;
   }
@@ -49,9 +43,8 @@ export class Contact implements Ent {
   id: ID;
   accountID: string;
   nodeType = "Contact";
-  privacyPolicy: PrivacyPolicy = {
-    rules: [AlwaysAllowRule],
-  };
+  privacyPolicy = AlwaysAllowPrivacyPolicy;
+
   constructor(public viewer: Viewer, id: ID, public data: Data) {
     this.id = id;
   }
@@ -61,9 +54,8 @@ export class Group implements Ent {
   id: ID;
   accountID: string;
   nodeType = "Group";
-  privacyPolicy: PrivacyPolicy = {
-    rules: [AlwaysAllowRule],
-  };
+  privacyPolicy = AlwaysAllowPrivacyPolicy;
+
   constructor(public viewer: Viewer, id: ID, public data: Data) {
     this.id = id;
   }
@@ -73,9 +65,8 @@ export class Message implements Ent {
   id: ID;
   accountID: string;
   nodeType = "Message";
-  privacyPolicy: PrivacyPolicy = {
-    rules: [AlwaysAllowRule],
-  };
+  privacyPolicy = AlwaysAllowPrivacyPolicy;
+
   constructor(public viewer: Viewer, id: ID, public data: Data) {
     this.id = id;
   }
@@ -85,9 +76,8 @@ export class Address implements Ent {
   id: ID;
   accountID: string;
   nodeType = "Address";
-  privacyPolicy: PrivacyPolicy = {
-    rules: [AlwaysAllowRule],
-  };
+  privacyPolicy = AlwaysAllowPrivacyPolicy;
+
   constructor(public viewer: Viewer, id: ID, public data: Data) {
     this.id = id;
   }

ts/src/core/ent.test.ts
@@ -1,5 +1,10 @@
 import { WriteOperation } from "../action";
-import { User, BuilderSchema, SimpleBuilder } from "../testutils/builder";
+import {
+  User,
+  BuilderSchema,
+  SimpleBuilder,
+  SimpleAction,
+} from "../testutils/builder";
 import { IDViewer, LoggedOutViewer } from "./viewer";
 import { Pool } from "pg";
 import { QueryRecorder } from "../testutils/db_mock";
@@ -16,8 +21,11 @@ import {
   Viewer,
   loadDerivedEnt,
   loadDerivedEntX,
+  loadEnt,
+  loadEntX,
 } from "./ent";
 import { PrivacyPolicy, AlwaysDenyRule, AllowIfViewerRule } from "./privacy";
+import { Context, ContextCache } from "./context";
 
 jest.mock("pg");
 QueryRecorder.mockPool(Pool);
@@ -126,7 +134,7 @@ describe("loadDerivedEnt", () => {
   });
 });
 
-describe("loadEntX", () => {
+describe("loadDerivedEntX", () => {
   test("loggedout", async () => {
     try {
       await DerivedUser.loadX(loggedOutViewer, { id: "1" });
@@ -240,4 +248,165 @@ test("custom edge", async () => {
   expect(edges2.length).toBe(1);
   const edge2 = edges2[0];
   expect(edge2).toBeInstanceOf(AssocEdge);
+});
+
+class contextImpl implements Context {
+  cache?: ContextCache = new ContextCache();
+  viewer = new LoggedOutViewer();
+
+  getViewer(): Viewer {
+    return this.viewer;
+  }
+}
+
+describe("loadEnt(X)", () => {
+  const noCtxV = new LoggedOutViewer();
+  const options = {
+    ent: User,
+    fields: ["id", "foo"],
+    tableName: "users",
+  };
+  const ctx = new contextImpl();
+
+  test("loadEnt. no data. no context", async () => {
+    const ent = await loadEnt(noCtxV, "1", options);
+    expect(ent).toBeNull();
+  });
+
+  test("loadEnt. no data. with context", async () => {
+    const ent = await loadEnt(ctx.getViewer(), "1", options);
+    expect(ent).toBeNull();
+  });
+
+  test("loadEntX. no data. no context", async () => {
+    try {
+      await loadEntX(noCtxV, "1", options);
+      fail("should have thrown");
+    } catch (e) {
+      expect(e.message).toMatch(/couldn't find row for query id/);
+    }
+  });
+
+  test("loadEntX. no data. with context", async () => {
+    try {
+      await loadEntX(ctx.getViewer(), "1", options);
+      fail("should have thrown");
+    } catch (e) {
+      expect(e.message).toMatch(/couldn't find row for query id/);
+    }
+  });
+
+  test("loadEnt. data. no context", async () => {
+    const user = await createUser();
+    const ent = await loadEnt(noCtxV, user.id, options);
+    expect(ent).not.toBeNull();
+    expect(ent).toBeInstanceOf(User);
+  });
+
+  test("loadEnt. data. with context", async () => {
+    const user = await createUser();
+    const ent = await loadEnt(ctx.getViewer(), user.id, options);
+    expect(ent).not.toBeNull();
+    expect(ent).toBeInstanceOf(User);
+  });
+
+  test("loadEntX. data. no context", async () => {
+    const user = await createUser();
+    const ent = await loadEntX(noCtxV, user.id, options);
+    expect(ent).toBeInstanceOf(User);
+  });
+
+  test("loadEntX. data. with context", async () => {
+    const user = await createUser();
+    const ent = await loadEntX(ctx.getViewer(), user.id, options);
+    expect(ent).toBeInstanceOf(User);
+  });
+
+  class User2 implements Ent {
+    id: ID;
+    accountID: string;
+    nodeType = "User2";
+    privacyPolicy = {
+      rules: [AllowIfViewerRule, AlwaysDenyRule],
+    };
+
+    constructor(public viewer: Viewer, id: ID, public data: Data) {
+      this.id = id;
+    }
+  }
+
+  class User2Schema implements BuilderSchema<User2> {
+    ent = User2;
+    fields: Field[] = [
+      UUIDType({
+        name: "id",
+      }),
+      StringType({
+        name: "foo",
+      }),
+    ];
+  }
+
+  function getBuilder() {
+    const action = new SimpleAction(
+      viewer,
+      new User2Schema(),
+      new Map([
+        ["id", "{id}"],
+        ["foo", "bar"],
+      ]),
+    );
+    action.viewerForEntLoad = (data: Data) => {
+      return new IDViewer(data.id);
+    };
+    return action.builder;
+  }
+
+  test("loadEntX. not visible privacy. with context", async () => {
+    const user = await getBuilder().saveX();
+    try {
+      await loadEntX(ctx.getViewer(), user.id, {
+        ent: User2,
+        fields: ["id", "foo"],
+        tableName: "user2s",
+      });
+      fail("should have thrown");
+    } catch (e) {
+      expect(e.message).toMatch(
+        /ent (.+) of type User2 is not visible for privacy reasons/,
+      );
+    }
+  });
+
+  test("loadEntX. not visible privacy. no context", async () => {
+    const user = await getBuilder().saveX();
+    try {
+      await loadEntX(noCtxV, user.id, {
+        ent: User2,
+        fields: ["id", "foo"],
+        tableName: "user2s",
+      });
+      fail("should have thrown");
+    } catch (e) {
+      expect(e.message).toMatch(
+        /ent (.+) of type User2 is not visible for privacy reasons/,
+      );
+    }
+  });
+
+  test("retrieving garbage", async () => {
+    const user = await getBuilder().saveX();
+    try {
+      await loadEntX(noCtxV, user.id, {
+        ent: User2,
+        // this would throw in SQL land since columns don't exist but we're more flexible with parse_sql so need to handle this
+        fields: ["firstName", "lastName"],
+        tableName: "user2s",
+      });
+    } catch (e) {
+      expect(e.message).toMatch(
+        /ent undefined of type User2 is not visible for privacy reasons/,
+      );
+    }
+  });
 });