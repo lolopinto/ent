lolopinto/ent

commit 2486e19ac502b2ee44c5b80258d74bb11bda5a48

ent core API cleanup/remove dead code/add comments/put in right places

ent/sql.go
@@ -19,12 +19,11 @@ const (
 
 // first simple version of sql builder
 type sqlBuilder struct {
-	entity       DBObject
-	fields       DBFields
-	colsString   string // not long term value of course
-	tableName    string
-	clause       sql.QueryClause
-	argsOverride []interface{}
+	entity     DBObject
+	fields     DBFields
+	colsString string // not long term value of course
+	tableName  string
+	clause     sql.QueryClause
 	// keeping these so as to not kill existing use cases
 	// TODO kill inField, inArgs
 	inField      string
@@ -100,26 +99,25 @@ func (s *sqlBuilder) getColsString() string {
 	return strings.Join(columns, ", ")
 }
 
-// This should be a Build()-> (string, []interface{}, error)
-func (s *sqlBuilder) getQuery() string {
+func (s *sqlBuilder) Build() (string, []interface{}, error) {
 	if s.rawQuery != "" {
-		return s.rawQuery
+		return s.rawQuery, s.rawValues, nil
 	}
 
 	switch s.queryType {
 	case selectQuery:
-		return s.getSelectQuery()
+		return s.buildSelectQuery()
 	case insertQuery:
-		return s.getInsertQuery()
+		return s.buildInsertQuery()
 	case updateQuery:
-		return s.getUpdateQuery()
+		return s.buildUpdateQuery()
 	case deleteQuery:
-		return s.getDeleteQuery()
+		return s.buildDeleteQuery()
 	}
 	panic("unsupported query")
 }
 
-func (s *sqlBuilder) getInsertQuery() string {
+func (s *sqlBuilder) buildInsertQuery() (string, []interface{}, error) {
 	cols := make([]string, len(s.writeFields))
 	vals := make([]interface{}, len(s.writeFields))
 	valsString := make([]string, len(s.writeFields))
@@ -132,19 +130,17 @@ func (s *sqlBuilder) getInsertQuery() string {
 		idx++
 	}
 
-	// to be returned by getValues()
-	s.argsOverride = vals
-
-	return fmt.Sprintf(
+	query := fmt.Sprintf(
 		"INSERT INTO %s (%s) VALUES(%s) %s",
 		s.tableName,
 		strings.Join(cols, ", "),
 		strings.Join(valsString, ", "),
 		s.insertSuffix,
 	)
+	return query, vals, nil
 }
 
-func (s *sqlBuilder) getUpdateQuery() string {
+func (s *sqlBuilder) buildUpdateQuery() (string, []interface{}, error) {
 	vals := make([]interface{}, len(s.writeFields))
 	valsString := make([]string, len(s.writeFields))
 
@@ -155,34 +151,32 @@ func (s *sqlBuilder) getUpdateQuery() string {
 		idx++
 	}
 
-	// to be returned by getValues()
-	s.argsOverride = vals
-
-	return fmt.Sprintf(
+	query := fmt.Sprintf(
 		"UPDATE %s SET %s WHERE %s = '%s' RETURNING *",
 		s.tableName,
 		strings.Join(valsString, ", "),
 		getPrimaryKeyForObj(s.existingEnt),
 		s.existingEnt.GetID(),
 	)
+	return query, vals, nil
 }
 
-func (s *sqlBuilder) getDeleteQuery() string {
-	// to be returned by getValues()
-	s.argsOverride = s.clause.GetValues()
-
-	return fmt.Sprintf(
+func (s *sqlBuilder) buildDeleteQuery() (string, []interface{}, error) {
+	query := fmt.Sprintf(
 		"DELETE from %s WHERE %s",
 		s.tableName,
 		s.clause.GetClause(),
 	)
+	return query, s.clause.GetValues(), nil
 }
 
-func (s *sqlBuilder) getSelectQuery() string {
+func (s *sqlBuilder) buildSelectQuery() (string, []interface{}, error) {
 	var whereClause string
 
+	var args []interface{}
 	if s.clause != nil {
 		whereClause = s.clause.GetClause()
+		args = s.clause.GetValues()
 	}
 
 	var formatSb strings.Builder
@@ -214,30 +208,13 @@ func (s *sqlBuilder) getSelectQuery() string {
 		inClause, ok := s.clause.(sql.InClause)
 		if ok && inClause.RebindInClause() {
 			var err error
-			query, s.argsOverride, err = sqlx.In(query, s.clause.GetValues())
+			query, args, err = sqlx.In(query, s.clause.GetValues())
 			if err != nil {
-				// TODO make this return an error correctly
-				panic(err)
+				return "", nil, err
 			}
 		}
 	}
-	return query
-}
-
-func (s *sqlBuilder) getValues() []interface{} {
-	if len(s.argsOverride) != 0 {
-		return s.argsOverride
-	}
-	// TODO validate that rawQuery and rawValues are passed together
-	if len(s.rawValues) != 0 {
-		return s.rawValues
-	}
-
-	if s.clause != nil {
-		return s.clause.GetValues()
-	}
-
-	return nil
+	return query, args, nil
 }
 
 func getPrimaryKeyForObj(entity DBObject) string {

ent/models.go
@@ -1,446 +1,0 @@
-package ent
-
-import (
-	dbsql "database/sql"
-
-	"fmt"
-	"time"
-
-	"github.com/jmoiron/sqlx"
-
-	"github.com/lolopinto/ent/data"
-	"github.com/lolopinto/ent/ent/sql"
-
-	"github.com/pkg/errors"
-)
-
-// TODO move this and other raw data access pattern methods to a lower level API below ent
-func LoadNodeRawData(id string, entLoader Loader) (map[string]interface{}, error) {
-	l := &loadNodeLoader{
-		id:        id,
-		entLoader: entLoader,
-		rawData:   true,
-	}
-	err := loadData(l)
-	return l.dataRow, err
-}
-
-// LoadNodesRawData loads raw data for multiple objects
-func LoadNodesRawData(ids []string, entLoader Loader) ([]map[string]interface{}, error) {
-	l := &loadNodesLoader{
-		ids:       ids,
-		entLoader: entLoader,
-		rawData:   true,
-	}
-	err := loadData(l)
-	return l.dataRows, err
-}
-
-// TODO comments everything
-func LoadNodesRawDataViaQueryClause(entLoader Loader, clause sql.QueryClause) ([]map[string]interface{}, error) {
-	l := &loadNodesLoader{
-		entLoader: entLoader,
-		clause:    clause,
-		rawData:   true,
-	}
-	err := loadData(l)
-	return l.dataRows, err
-}
-
-func LoadNodeRawDataViaQueryClause(entLoader Loader, clause sql.QueryClause) (map[string]interface{}, error) {
-	l := &loadNodeLoader{
-		entLoader: entLoader,
-		clause:    clause,
-		rawData:   true,
-	}
-	err := loadData(l)
-	return l.dataRow, err
-}
-
-func loadNodesViaClause(entLoader Loader, clause sql.QueryClause) multiEntResult {
-	l := &loadNodesLoader{
-		entLoader: entLoader,
-		clause:    clause,
-	}
-	err := loadData(l)
-	return getMultiEntResult(entLoader, l, err)
-}
-
-func SaveChangeset(changeset Changeset) error {
-	return executeOperations(changeset.GetExecutor())
-}
-
-func getStmtFromTx(tx *sqlx.Tx, db *sqlx.DB, query string) (string, *sqlx.Stmt, error) {
-	var stmt *sqlx.Stmt
-	var err error
-
-	// handle if in transcation or not.
-	if tx == nil {
-		// automatically rebinding now but we need to handle this better later
-		// TODO this is the only place i'm rebinding
-		// change everything to stop using $ and now use "?"
-		query = db.Rebind(query)
-		stmt, err = db.Preparex(query)
-	} else {
-		query = tx.Rebind(query)
-		stmt, err = tx.Preparex(query)
-	}
-	return query, stmt, err
-}
-
-/*
- * performs a write (INSERT, UPDATE, DELETE statement) given the SQL statement
- * and values to be updated in the database
- */
-func performWrite(query string, values []interface{}, tx *sqlx.Tx, entity Entity) error {
-	db := data.DBConn()
-	if db == nil {
-		err := errors.New("error getting a valid db connection")
-		fmt.Println(err)
-		return err
-	}
-
-	query, stmt, err := getStmtFromTx(tx, db, query)
-
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	var res dbsql.Result
-
-	checkRows := false
-	if entity == nil {
-		checkRows = true
-		res, err = stmt.Exec(values...)
-	} else {
-		row := stmt.QueryRowx(values...)
-		err = queryRow(row, entity)
-	}
-	//fmt.Println(query)
-
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println(query, values)
-		return err
-	}
-	defer stmt.Close()
-
-	// TODO may need to eventually make this optional but for now
-	// let's assume all writes should affect at least one row
-	if checkRows {
-		// TODO this doesn't work anymore  when removing an edge that may not exist
-		// so if we still care about this move this to a new layer
-		rowCount, err := res.RowsAffected()
-		if err != nil || rowCount == 0 {
-			//fmt.Println(err, rowCount)
-			return err
-		}
-	}
-	return nil
-}
-
-// EdgeOptions is a struct that can be used to configure an edge.
-// Time refers to the time associated with the edge. If not specified, defaults to current time
-// Data refers to whatever information that needs to be stored/associated with the edge
-// It's up to 255 characters
-type EdgeOptions struct {
-	Time time.Time
-	Data string
-}
-
-// LoadEdgeConfig configures the way to load edges
-// This will eventually be used in EntQuery but allows us to start testing and building some things...
-type LoadEdgeConfig struct {
-	limit *int
-}
-
-func (cfg *LoadEdgeConfig) getKey() string {
-	if cfg.limit == nil {
-		return ""
-	}
-	return fmt.Sprintf("limit:%d", cfg.limit)
-}
-
-func Limit(limit int) func(*LoadEdgeConfig) {
-	return func(cfg *LoadEdgeConfig) {
-		cfg.limit = &limit
-	}
-}
-
-func LoadEdgesByType(id string, edgeType EdgeType, options ...func(*LoadEdgeConfig)) ([]*AssocEdge, error) {
-	l := &loadEdgesByType{
-		id:       id,
-		edgeType: edgeType,
-	}
-	return l.LoadData()
-}
-
-func LoadUniqueEdgeByType(id string, edgeType EdgeType) (*AssocEdge, error) {
-	edges, err := LoadEdgesByType(id, edgeType, Limit(1))
-	if err != nil {
-		return nil, err
-	}
-	return edges[0], err
-}
-
-// GenLoadEdgesByType handles loading of edges concurrently.
-// Because we get strong typing across all edges and for a consistent API with loading Nodes,
-// we use the EdgesResult struct here
-func GenLoadEdgesByType(id string, edgeType EdgeType, options ...func(*LoadEdgeConfig)) <-chan *AssocEdgesResult {
-	res := make(chan *AssocEdgesResult)
-	go func() {
-		edges, err := LoadEdgesByType(id, edgeType, options...)
-		res <- &AssocEdgesResult{
-			Edges: edges,
-			Err:   err,
-		}
-	}()
-	return res
-}
-
-func GenLoadUniqueEdgeByType(id string, edgeType EdgeType) <-chan *AssocEdgeResult {
-	res := make(chan *AssocEdgeResult)
-	go func() {
-		edge, err := LoadUniqueEdgeByType(id, edgeType)
-		res <- &AssocEdgeResult{
-			Edge: edge,
-			Err:  err,
-		}
-	}()
-	return res
-}
-
-// GenLoadEdgeByType is the concurrent version of LoadEdgeByType
-func GenLoadEdgeByType(id1, id2 string, edgeType EdgeType) <-chan *AssocEdgeResult {
-	res := make(chan *AssocEdgeResult)
-	go func() {
-		edge, err := LoadEdgeByType(id1, id2, edgeType)
-		res <- &AssocEdgeResult{
-			Edge: edge,
-			Err:  err,
-		}
-
-	}()
-	return res
-}
-
-// LoadEdgeByType checks if an edge exists between 2 ids
-func LoadEdgeByType(id string, id2 string, edgeType EdgeType) (*AssocEdge, error) {
-	// check if we can use the standard id1->edgeType cache
-	edges, err := LoadEdgesByType(id, edgeType)
-	if err != nil {
-		return nil, err
-	}
-	for _, edge := range edges {
-		if edge.ID2 == id2 {
-			return edge, nil
-		}
-	}
-	// no edge
-	return &AssocEdge{}, nil
-
-	// this checks if the edge exists based on the cache key and then does nothing about it
-	// I think this approach is the best longterm approach but we don't have the way to delete
-	// all the pairwise edges when there's a miss
-	// above is only a temporary solution.
-	// key := getKeyForEdge(id, edgeType)
-	// items, err := getItemsInCache(key)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// if items != nil {
-	// 	for _, item := range items {
-	// 		//			spew.Dump(item)
-	// 		if item["id2"] != nil {
-	// 			fmt.Printf("cache hit id1 %s, edge_type %s, id2 %s \n", id, edgeType, id2)
-	// 			var edge Edge
-	// 			err = edge.FillFromMap(item)
-	// 			if err != nil {
-	// 				return nil, err
-	// 			} else {
-	// 				return &edge, nil
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// fmt.Printf("cache miss id1 %s, edge_type %s, id2 %s \n", id, edgeType, id2)
-
-	// // todo...
-	// // sql no rows behavior
-	// if id == "" || id2 == "" {
-	// 	return &Edge{}, nil
-	// }
-	// db := data.DBConn()
-	// if db == nil {
-	// 	err := errors.New("error getting a valid db connection")
-	// 	fmt.Println(err)
-	// 	return nil, err
-	// }
-
-	// edgeData, err := getEdgeInfo(edgeType, nil)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// query := fmt.Sprintf(
-	// 	"SELECT * FROM %s WHERE id1 = $1 AND edge_type = $2 AND id2 = $3",
-	// 	edgeData.EdgeTable,
-	// )
-	// fmt.Println(query)
-
-	// stmt, err := db.Preparex(query)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return nil, err
-	// }
-	// defer stmt.Close()
-
-	// var edge Edge
-
-	// err = stmt.QueryRowx(id, edgeType, id2).StructScan(&edge)
-
-	// // nil state. return zero value of Edge for now. maybe come up with better
-	// // way of doing this in the future
-	// if err == dbsql.ErrNoRows {
-	// 	//fmt.Println("no rows", err)
-	// 	// don't mark this as an error. just no data
-	// 	return &Edge{}, nil
-	// }
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return nil, err
-	// }
-
-	// // TODO handle no edge. what's the correct error type here?
-	// return &edge, nil
-}
-
-func LoadRawNodesByType(id string, edgeType EdgeType, entLoader Loader) ([]map[string]interface{}, error) {
-	l := &loadNodesLoader{
-		entLoader: entLoader,
-		rawData:   true,
-	}
-	err := chainLoaders(
-		[]loader{
-			&loadEdgesByType{
-				id:         id,
-				edgeType:   edgeType,
-				outputID2s: true,
-			},
-			l,
-		},
-	)
-	return l.dataRows, err
-}
-
-func getMultiEntResult(entLoader Loader, l *loadNodesLoader, err error) multiEntResult {
-	if err != nil {
-		return multiEntResult{
-			err: err,
-		}
-	}
-	return multiEntResult{
-		ents:   l.dbobjects,
-		loader: entLoader,
-	}
-}
-
-func loadNodesByType(id string, edgeType EdgeType, entLoader Loader) multiEntResult {
-	l := &loadNodesLoader{
-		entLoader: entLoader,
-	}
-	err := chainLoaders(
-		[]loader{
-			&loadEdgesByType{
-				id:         id,
-				edgeType:   edgeType,
-				outputID2s: true,
-			},
-			l,
-		},
-	)
-	return getMultiEntResult(entLoader, l, err)
-}
-
-func loadNodes(ids []string, entLoader Loader) multiEntResult {
-	l := &loadNodesLoader{
-		ids:       ids,
-		entLoader: entLoader,
-	}
-	err := loadData(l)
-	return getMultiEntResult(entLoader, l, err)
-}
-
-// TODO figure out correct long-term API here
-// this is the single get of GenLoadAssocEdges so shouldn't be too hard
-func GetEdgeInfo(edgeType EdgeType, tx *sqlx.Tx) (*AssocEdgeData, error) {
-	l := &loadNodeLoader{
-		id:        string(edgeType),
-		entLoader: &AssocEdgeLoader{},
-	}
-	err := loadData(l, cfgtx(tx))
-	if err != nil {
-		return nil, err
-	}
-	return l.GetEntity().(*AssocEdgeData), nil
-}
-
-func GetEdgeInfos(edgeTypes []string) (map[EdgeType]*AssocEdgeData, error) {
-	entLoader := &AssocEdgeLoader{}
-	l := &loadNodesLoader{
-		entLoader: entLoader,
-		ids:       edgeTypes,
-	}
-	err := loadData(l)
-	return entLoader.GetMap(), err
-}
-
-// GenLoadAssocEdges loads all assoc edges from the db
-// TODO correct cache for this. we should load this once per request or have this
-// be in a central cache easily available
-func GenLoadAssocEdges() <-chan AssocEdgeDatasResult {
-	res := make(chan AssocEdgeDatasResult)
-	go func() {
-		entLoader := &AssocEdgeLoader{}
-		err := chainLoaders(
-			[]loader{
-				&loadAssocEdgeConfigExists{},
-				&loadNodesLoader{
-					rawQuery:  "SELECT * FROM assoc_edge_config",
-					entLoader: entLoader,
-				},
-			},
-		)
-		if err != nil {
-			res <- AssocEdgeDatasResult{
-				Err: err,
-			}
-		} else {
-			res <- AssocEdgeDatasResult{
-				Edges: entLoader.results,
-			}
-		}
-	}()
-	return res
-}
-
-func GenLoadRawQuery(query string, loader Loader) <-chan error {
-	res := make(chan error)
-	go func() {
-		res <- loadData(&loadNodesLoader{
-			rawQuery:  query,
-			entLoader: loader,
-		})
-	}()
-	return res
-}
-
-func LoadRawQuery(query string, loader Loader) ([]map[string]interface{}, error) {
-	l := &loadNodesLoader{
-		rawQuery:  query,
-		entLoader: loader,
-		rawData:   true,
-	}
-	err := loadData(l)
-	return l.dataRows, err
-}

ent/models_test.go
@@ -374,21 +374,21 @@ func (suite *modelsTestSuite) TestInvalidLoadEdgeByType() {
 
 	edge, err := ent.LoadEdgeByType(event.ID, user.ID, models.UserToEventsEdge)
 	assert.Nil(suite.T(), err)
-	assert.Zero(suite.T(), *edge)
+	assert.Nil(suite.T(), edge)
 }
 
 func (suite *modelsTestSuite) TestLoadEdgeByTypeEmptyID1() {
 	user := testingutils.CreateTestUser(suite.T())
 	edge, err := ent.LoadEdgeByType("", user.ID, models.UserToEventsEdge)
 	assert.Nil(suite.T(), err)
-	assert.Zero(suite.T(), *edge)
+	assert.Nil(suite.T(), edge)
 }
 
 func (suite *modelsTestSuite) TestLoadEdgeByTypeEmptyID2() {
 	user := testingutils.CreateTestUser(suite.T())
 	edge, err := ent.LoadEdgeByType(user.ID, "", models.UserToEventsEdge)
 	assert.Nil(suite.T(), err)
-	assert.Zero(suite.T(), *edge)
+	assert.Nil(suite.T(), edge)
 }
 
 func testLoadEdgeByType(suite *modelsTestSuite, f func(id, id2 string) (*ent.AssocEdge, error)) {
@@ -432,7 +432,7 @@ func testUniqueLoadNodeByType(suite *modelsTestSuite, f func(id string) (*models
 func verifyEdgeByType(suite *modelsTestSuite, f func() (*ent.AssocEdge, error), id1, id2 string) {
 	edge, err := f()
 	assert.Nil(suite.T(), err)
-	assert.NotZero(suite.T(), edge)
+	assert.NotNil(suite.T(), edge)
 	assert.Equal(suite.T(), edge.ID1, id1)
 	assert.Equal(suite.T(), edge.ID2, id2)
 }

ent/operations.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
-	"github.com/lolopinto/ent/data"
 	"github.com/lolopinto/ent/ent/sql"
 	"github.com/pkg/errors"
 	"github.com/rocketlaunchr/remember-go"
@@ -104,8 +103,7 @@ func (op *EditNodeOperation) PerformWrite(tx *sqlx.Tx) error {
 		return errors.New("Unsupported operation")
 	}
 
-	query, values := builder.getQuery(), builder.getValues()
-	return performWrite(query, values, tx, op.Entity)
+	return performWrite(builder, tx, op.Entity)
 }
 
 // GetCacheKeys returns keys that need to be deleted for this operation
@@ -294,7 +292,7 @@ func (op *EdgeOperation) PerformWrite(tx *sqlx.Tx) error {
 		return fmt.Errorf("unsupported edge operation %v passed to edgeOperation.PerformWrite", op)
 	}
 
-	return performWrite(builder.getQuery(), builder.getValues(), tx, nil)
+	return performWrite(builder, tx, nil)
 }
 
 // GetCacheKeys returns keys that need to be deleted for this operation
@@ -400,7 +398,7 @@ type DeleteNodeOperation struct {
 func (op *DeleteNodeOperation) PerformWrite(tx *sqlx.Tx) error {
 	builder := getDeleteQuery(op.EntConfig.GetTableName(), sql.Eq("id", op.ExistingEnt.GetID()))
 
-	return performWrite(builder.getQuery(), builder.getValues(), tx, nil)
+	return performWrite(builder, tx, nil)
 }
 
 // GetCacheKeys returns keys that need to be deleted for this operation
@@ -408,54 +406,6 @@ func (op *DeleteNodeOperation) GetCacheKeys() []string {
 	return []string{
 		getKeyForNode(op.ExistingEnt.GetID(), op.EntConfig.GetTableName()),
 	}
-}
-
-// the meat of how things work
-func executeOperations(exec Executor) error {
-	db := data.DBConn()
-	tx, err := db.Beginx()
-	if err != nil {
-		fmt.Println("error creating transaction", err)
-		return err
-	}
-
-	for {
-		op, err := exec.Operation()
-		if err == ErrAllOperations {
-			break
-		} else if err != nil {
-			return handErrInTransaction(tx, err)
-		}
-
-		resolvableOp, ok := op.(DataOperationWithResolver)
-		if ok {
-			if err = resolvableOp.Resolve(exec); err != nil {
-				return handErrInTransaction(tx, err)
-			}
-		}
-
-		// perform the write as needed
-		if err = op.PerformWrite(tx); err != nil {
-			return handErrInTransaction(tx, err)
-		}
-
-		// get any keys for this op and delete
-		cacheableOp, ok := op.(DataOperationWithKeys)
-		if cacheEnabled && ok {
-			for _, key := range cacheableOp.GetCacheKeys() {
-				deleteKey(key)
-			}
-		}
-	}
-
-	tx.Commit()
-	return nil
-}
-
-func handErrInTransaction(tx *sqlx.Tx, err error) error {
-	fmt.Println("error during transaction", err)
-	tx.Rollback()
-	return err
 }
 
 func getKeyForNode(id, tableName string) string {

ent/primitives.go
@@ -1,0 +1,254 @@
+package ent
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+
+	"github.com/lolopinto/ent/ent/sql"
+)
+
+// LoadNodeRawData is the public API to load the raw data for an ent without privacy checks
+func LoadNodeRawData(id string, entLoader Loader) (map[string]interface{}, error) {
+	l := &loadNodeLoader{
+		id:        id,
+		entLoader: entLoader,
+		rawData:   true,
+	}
+	err := loadData(l)
+	return l.dataRow, err
+}
+
+// LoadNodesRawData loads raw data for multiple objects given their ids
+func LoadNodesRawData(ids []string, entLoader Loader) ([]map[string]interface{}, error) {
+	l := &loadNodesLoader{
+		ids:       ids,
+		entLoader: entLoader,
+		rawData:   true,
+	}
+	err := loadData(l)
+	return l.dataRows, err
+}
+
+// LoadNodesRawDataViaQueryClause takes a query clause e.g. sql.Eq("id", "{id of foreign key here}")
+// and returns the raw data for all nodes that map to that
+func LoadNodesRawDataViaQueryClause(entLoader Loader, clause sql.QueryClause) ([]map[string]interface{}, error) {
+	l := &loadNodesLoader{
+		entLoader: entLoader,
+		clause:    clause,
+		rawData:   true,
+	}
+	err := loadData(l)
+	return l.dataRows, err
+}
+
+// LoadNodeRawDataViaQueryClause takes a query clause e.g. sql.Eq("email_address", "test@email.com")
+// and returns the raw data for a (the) node that maps to that
+func LoadNodeRawDataViaQueryClause(entLoader Loader, clause sql.QueryClause) (map[string]interface{}, error) {
+	l := &loadNodeLoader{
+		entLoader: entLoader,
+		clause:    clause,
+		rawData:   true,
+	}
+	err := loadData(l)
+	return l.dataRow, err
+}
+
+// LoadRawNodesByType loads the nodes at the end of an edge
+func LoadRawNodesByType(id string, edgeType EdgeType, entLoader Loader) ([]map[string]interface{}, error) {
+	// options... once we do EntQuery
+	l := &loadNodesLoader{
+		entLoader: entLoader,
+		rawData:   true,
+	}
+	err := chainLoaders(
+		[]loader{
+			&loadEdgesByType{
+				id:         id,
+				edgeType:   edgeType,
+				outputID2s: true,
+			},
+			l,
+		},
+	)
+	return l.dataRows, err
+}
+
+// LoadEdgesByType loads the edges for a given type
+func LoadEdgesByType(id string, edgeType EdgeType, options ...func(*LoadEdgeConfig)) ([]*AssocEdge, error) {
+	l := &loadEdgesByType{
+		id:       id,
+		edgeType: edgeType,
+		options:  options,
+	}
+	return l.LoadData()
+}
+
+// GenLoadEdgesByType handles loading of edges concurrently.
+// Because we get strong typing across all edges and for a consistent API with loading Nodes,
+// we use the EdgesResult struct here
+func GenLoadEdgesByType(id string, edgeType EdgeType, options ...func(*LoadEdgeConfig)) <-chan *AssocEdgesResult {
+	res := make(chan *AssocEdgesResult)
+	go func() {
+		edges, err := LoadEdgesByType(id, edgeType, options...)
+		res <- &AssocEdgesResult{
+			Edges: edges,
+			Err:   err,
+		}
+	}()
+	return res
+}
+
+// LoadUniqueEdgeByType loads the unique edge for a given type.
+// Applies a limit 1 to the query
+func LoadUniqueEdgeByType(id string, edgeType EdgeType) (*AssocEdge, error) {
+	edges, err := LoadEdgesByType(id, edgeType, Limit(1))
+	if err != nil {
+		return nil, err
+	}
+	return edges[0], err
+}
+
+// GenLoadUniqueEdgeByType is the concurrent version of LoadUniqueEdgeByType
+func GenLoadUniqueEdgeByType(id string, edgeType EdgeType) <-chan *AssocEdgeResult {
+	res := make(chan *AssocEdgeResult)
+	go func() {
+		edge, err := LoadUniqueEdgeByType(id, edgeType)
+		res <- &AssocEdgeResult{
+			Edge: edge,
+			Err:  err,
+		}
+	}()
+	return res
+}
+
+// GenLoadEdgeByType is the concurrent version of LoadEdgeByType
+func GenLoadEdgeByType(id1, id2 string, edgeType EdgeType) <-chan *AssocEdgeResult {
+	res := make(chan *AssocEdgeResult)
+	go func() {
+		edge, err := LoadEdgeByType(id1, id2, edgeType)
+		res <- &AssocEdgeResult{
+			Edge: edge,
+			Err:  err,
+		}
+
+	}()
+	return res
+}
+
+// LoadEdgeByType checks if an edge exists between 2 ids
+func LoadEdgeByType(id string, id2 string, edgeType EdgeType) (*AssocEdge, error) {
+	// TODO 2/25/2020 the logic we eventually want here is if count is less than say 10,000 or whatever we decide the cache limit
+	// is, load that, and do a check for id2 in memory
+	// otherwise, if no cache or if count is a lot bigger than the limit,
+	// need to do a check in the database directly
+	// probably not worth having cache for each edge by default.
+	// need to provide a way for places to override it as needed
+	edges, err := LoadEdgesByType(id, edgeType)
+	if err != nil {
+		return nil, err
+	}
+	for _, edge := range edges {
+		if edge.ID2 == id2 {
+			return edge, nil
+		}
+	}
+	// no edge
+	return nil, nil
+}
+
+// EdgeOptions is a struct that can be used to configure an edge.
+// Time refers to the time associated with the edge. If not specified, defaults to current time
+// Data refers to whatever information that needs to be stored/associated with the edge
+// It's up to 255 characters (hmm not true right now)
+type EdgeOptions struct {
+	Time time.Time
+	Data string
+}
+
+// LoadEdgeConfig configures the way to load edges
+// This will eventually be used in EntQuery but allows us to start testing and building some things...
+type LoadEdgeConfig struct {
+	limit *int
+}
+
+func (cfg *LoadEdgeConfig) getKey() string {
+	if cfg.limit == nil {
+		return ""
+	}
+	return fmt.Sprintf("limit:%d", cfg.limit)
+}
+
+// Limit is an option passed to edge queries to limit the number of edges returned
+func Limit(limit int) func(*LoadEdgeConfig) {
+	return func(cfg *LoadEdgeConfig) {
+		cfg.limit = &limit
+	}
+}
+
+// GetEdgeInfo gets the edge information for a given edgeType
+// TODO figure out correct long-term API here
+// this is the single get of GenLoadAssocEdges so shouldn't be too hard
+func GetEdgeInfo(edgeType EdgeType, tx *sqlx.Tx) (*AssocEdgeData, error) {
+	l := &loadNodeLoader{
+		id:        string(edgeType),
+		entLoader: &AssocEdgeLoader{},
+	}
+	err := loadData(l, cfgtx(tx))
+	if err != nil {
+		return nil, err
+	}
+	return l.GetEntity().(*AssocEdgeData), nil
+}
+
+// GetEdgeInfos gets the edge information for a list of edges
+func GetEdgeInfos(edgeTypes []string) (map[EdgeType]*AssocEdgeData, error) {
+	entLoader := &AssocEdgeLoader{}
+	l := &loadNodesLoader{
+		entLoader: entLoader,
+		ids:       edgeTypes,
+	}
+	err := loadData(l)
+	return entLoader.GetMap(), err
+}
+
+// GenLoadAssocEdges loads all assoc edges from the db
+// TODO correct cache for this. we should load this once per request or have this
+// be in a central cache easily available
+func GenLoadAssocEdges() <-chan AssocEdgeDatasResult {
+	res := make(chan AssocEdgeDatasResult)
+	go func() {
+		entLoader := &AssocEdgeLoader{}
+		err := chainLoaders(
+			[]loader{
+				&loadAssocEdgeConfigExists{},
+				&loadNodesLoader{
+					rawQuery:  "SELECT * FROM assoc_edge_config",
+					entLoader: entLoader,
+				},
+			},
+		)
+		if err != nil {
+			res <- AssocEdgeDatasResult{
+				Err: err,
+			}
+		} else {
+			res <- AssocEdgeDatasResult{
+				Edges: entLoader.results,
+			}
+		}
+	}()
+	return res
+}
+
+// LoadRawQuery takes a raw query string and runs it and gets the results in the raw format
+func LoadRawQuery(query string, loader Loader) ([]map[string]interface{}, error) {
+	l := &loadNodesLoader{
+		rawQuery:  query,
+		entLoader: loader,
+		rawData:   true,
+	}
+	err := loadData(l)
+	return l.dataRows, err
+}

commit e3f4568d075e568ae38691f463039c761e27f65b

fix ent/privacy_loading_test.go
sqlBuilder wasn't working for in queries

broken by https://github.com/lolopinto/ent/commit/2486e19ac502b2ee44c5b80258d74bb11bda5a48#diff-15801dd31d56c5d13c76e24c50e2678d6deb1333afe8b4bb2721dc12116502ac

apparently, forgot to kill and forgot a place to update

ent/loader.go
@@ -554,7 +554,7 @@ func (l *loadNodesLoader) GetSQLBuilder() (*sqlBuilder, error) {
 		for idx, id := range l.ids {
 			inArgs[idx] = id
 		}
-		builder.in(l.pkey, inArgs)
+		builder.clause = sql.In(l.pkey, inArgs...)
 	} else if l.clause != nil {
 		builder.clause = l.clause
 	} else if l.rawQuery != "" {

ent/sql.go
@@ -19,15 +19,11 @@ const (
 
 // first simple version of sql builder
 type sqlBuilder struct {
-	entity     DBObject
-	fields     DBFields
-	colsString string // not long term value of course
-	tableName  string
-	clause     sql.QueryClause
-	// keeping these so as to not kill existing use cases
-	// TODO kill inField, inArgs
-	inField      string
-	inArgs       []interface{}
+	entity       DBObject
+	fields       DBFields
+	colsString   string // not long term value of course
+	tableName    string
+	clause       sql.QueryClause
 	order        string
 	rawQuery     string
 	rawValues    []interface{}
@@ -63,12 +59,6 @@ func getDeleteQuery(tableName string, clause sql.QueryClause) *sqlBuilder {
 		tableName: tableName,
 		clause:    clause,
 	}
-}
-
-func (s *sqlBuilder) in(field string, args []interface{}) *sqlBuilder {
-	s.inField = field
-	s.inArgs = args
-	return s
 }
 
 func (s *sqlBuilder) orderBy(orderBy string) *sqlBuilder {