c03b3626039a7bf722ed31e8f61ac7224ecea2f2
lolopinto <ola@okelola.com>
2021-03-11 15:28:04 -0800

    Create main.go
    upgrade db at database.
    
    run `DB_CONNECTION_STRING=postgres://postgres:@localhost/postgres?sslmode=disable go run .`

---
+++ test_setup/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/lolopinto/ent/internal/codegen"
+	"github.com/lolopinto/ent/internal/db"
+)
+
+func main() {
+	env := os.Getenv("DB_CONNECTION_STRING")
+	if env == "" {
+		log.Fatal("DB_CONNECTION_STRING env variable is required")
+	}
+
+	codepath := codegen.NewCodePath("../internal/test_schema/models/configs", "")
+	db.UpgradeDB(codepath)
+}
