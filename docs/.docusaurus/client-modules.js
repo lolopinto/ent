0e3a286d54a55737d2487197e522e758066675a7
Ola Okelola <ola@okelola.com>
2021-05-26 11:40:29 -0700

    add .docusaurus to gitignore

--- docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/ola/code/ent/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ola/code/ent/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ola/code/ent/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ola/code/ent/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/ola/code/ent/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/ola/code/ent/docs/src/css/custom.css"),
-];
