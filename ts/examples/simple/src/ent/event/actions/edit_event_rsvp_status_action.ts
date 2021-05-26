ee011dcc392cb9f62686fbba4b87ccf9fecbac14
Ola Okelola <ola@okelola.com>
2021-05-26 14:25:24 -0700

    remove dangling files from move + merge gone wrong

--- ts/examples/simple/src/ent/event/actions/edit_event_rsvp_status_action.ts
+++
@@ -1,9 +0,0 @@
-import {
-  EditEventRsvpStatusActionBase,
-  EditEventRsvpStatusInput,
-} from "src/ent/event/actions/generated/edit_event_rsvp_status_action_base";
-
-export { EditEventRsvpStatusInput };
-
-// we're only writing this once except with --force and packageName provided
-export default class EditEventRsvpStatusAction extends EditEventRsvpStatusActionBase {}
--- ts/examples/simple/src/ent/event/actions/event_remove_host_action.ts
+++
@@ -1,4 +0,0 @@
-import { EventRemoveHostActionBase } from "src/ent/event/actions/generated/event_remove_host_action_base";
-
-// we're only writing this once except with --force and packageName provided
-export default class EventRemoveHostAction extends EventRemoveHostActionBase {}
