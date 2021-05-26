db156779bb92f7a839e3c267d4c22c5b4720142b
Ola Okelola <ola@okelola.com>
2021-05-25 22:43:44 -0700

    simple logout

--- examples/ent-rsvp/ent-rsvp-web/src/components/layout.tsx
+++ examples/ent-rsvp/ent-rsvp-web/src/components/layout.tsx
@@ -3,6 +3,8 @@ import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useRouter } from "next/router";
 import { useSession } from "../../src/session";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap";
 
 interface args {
   allowLoggedout?: boolean;
@@ -14,7 +16,7 @@ export default function Layout(props: args) {
   // there needs to be a delayed way of dealing with this in react...
   const [loading, setLoading] = useState(true);
 
-  const [session] = useSession();
+  const [session, _, clearSession] = useSession();
   const router = useRouter();
   const goLogin = () => {
     if (router.pathname !== "login") {
@@ -45,5 +47,21 @@ export default function Layout(props: args) {
     goLogin();
   }, [session, props.allowLoggedout, props.allowGuest]);
 
-  return <Container className="p-3">{!loading && props.children}</Container>;
+  const logout = (e) => {
+    clearSession();
+    goLogin();
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  return <Container className="p-3">
+    <>
+      {session?.viewer &&
+        <Navbar bg="light">
+          <Navbar.Brand href='/home'>Ent RSVP</Navbar.Brand>
+          <Nav className="mr-auto"><Nav.Link href="#" onClick={logout}>Logout</Nav.Link></Nav>
+        </Navbar>}
+      {!loading && props.children}
+    </>
+  </Container>;
 }
