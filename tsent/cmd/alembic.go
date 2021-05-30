d419a58af668d6edd7590f7f3e6bf1bde110114e
Ola Okelola <ola@okelola.com>
2021-05-30 15:50:30 -0700

    add alembic command to pass everything through

---
+++ tsent/cmd/alembic.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/lolopinto/ent/internal/codegen"
+	"github.com/lolopinto/ent/internal/db"
+	"github.com/spf13/cobra"
+)
+
+var validCmds= map[string]int{
+	"upgrade":1,
+	"downgrade":1,
+	"history":0,
+	"current":0,
+	"heads":0,
+	"branches":0,
+	"show":1,
+	"stamp":1,
+	"edit":1,
+}
+
+var alembicCmd = &cobra.Command{
+	Use:   "alembic",
+	Short: "alembic command",
+	Long:  `This runs the passed in alembic command`,
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error{
+		// another hardcoded place
+		codePathInfo := codegen.NewCodePath("src/schema", "")
+		command := args[0];
+		count, ok := validCmds[command]  
+		if !ok {
+			return fmt.Errorf("invalid alemvic command %s passed in", command)
+		}
+
+		if count + 1 != len(args) {
+			return fmt.Errorf("invalid number of args passed. expected %d", count+1)
+		}
+
+		db.RunAlembicCommand(codePathInfo, args[0], args[1:]...)
+		return nil
+	},
+}
