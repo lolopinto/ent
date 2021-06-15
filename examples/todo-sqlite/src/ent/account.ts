bb5fabe84782cde007d911eb61785a8050f6e576
Ola Okelola <ola@okelola.com>
2021-06-15 13:45:43 -0700

    mutations

--- examples/todo-sqlite/src/ent/account.ts
+++ examples/todo-sqlite/src/ent/account.ts
@@ -1,3 +1,6 @@
+import { AlwaysAllowPrivacyPolicy } from "@lolopinto/ent";
 import { AccountBase } from "src/ent/internal";
 
-export class Account extends AccountBase {}
+export class Account extends AccountBase {
+  privacyPolicy = AlwaysAllowPrivacyPolicy;
+}
--- examples/todo-sqlite/src/schema/account.ts
+++ examples/todo-sqlite/src/schema/account.ts
@@ -7,9 +7,14 @@ import {
 } from "@lolopinto/ent";
 import { PhoneNumberType } from "@lolopinto/ent-phonenumber";
 
-export default class User extends BaseEntSchema {
+export default class Account extends BaseEntSchema {
   fields: Field[] = [
     StringType({ name: "Name" }),
     PhoneNumberType({ name: "PhoneNumber", unique: true }),
+  ];
+  actions: Action[] = [
+    {
+      operation: ActionOperation.Mutations,
+    },
   ];
 }
