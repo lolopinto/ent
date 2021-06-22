lolopinto/ent

commit ef27dd0d118124d4d53e0e2ff8d74ca8ee491d73

more changes to support SQLite + example (#370)

ent/loader.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	"github.com/lolopinto/ent/ent/cast"
+	"github.com/lolopinto/ent/ent/config"
 	"github.com/lolopinto/ent/ent/sql"
 	"github.com/pkg/errors"
 )
@@ -90,12 +91,6 @@ type customMultiLoader interface {
 
 type configurableLoader interface {
 	Configure() configureQueryResult
-}
-
-// terrible name
-type abortEarlyLoader interface {
-	loader
-	AbortEarly() bool
 }
 
 type loaderConfig struct {
@@ -458,7 +453,8 @@ func (l *loadEdgesByType) LoadData() ([]*AssocEdge, error) {
 }
 
 type nodeExists struct {
-	Exists bool `db:"exists"`
+	Exists bool   `db:"exists"`
+	Name   string `db:"name"`
 }
 
 func (n *nodeExists) GetID() string {
@@ -470,30 +466,44 @@ func (n *nodeExists) DBFields() DBFields {
 }
 
 type loadAssocEdgeConfigExists struct {
-	n nodeExists
+	n []*nodeExists
 }
 
 func (l *loadAssocEdgeConfigExists) GetSQLBuilder() (*sqlBuilder, error) {
+	query := "SELECT to_regclass($1) IS NOT NULL as exists"
+	if config.IsSQLiteDialect() {
+		query = "SELECT name FROM sqlite_master WHERE type='table' AND name=$1"
+	}
+
 	return &sqlBuilder{
-		rawQuery: "SELECT to_regclass($1) IS NOT NULL as exists",
+		rawQuery: query,
 		rawValues: []interface{}{
 			"assoc_edge_config",
 		},
 	}, nil
 }
 
-func (l *loadAssocEdgeConfigExists) GetEntity() DBObject {
-	return &l.n
+func (l *loadAssocEdgeConfigExists) GetNewInstance() DBObject {
+	n := &nodeExists{}
+	l.n = append(l.n, n)
+	return n
 }
 
 func (l *loadAssocEdgeConfigExists) GetOutput() interface{} {
 	// has an output that's not necessary passable. just indicates whether we should continue
 	// so GetOutput has 2 different meanings. This should mean continue chaining vs input/output
 	// maybe decouple this later?
-	if l.n.Exists {
-		return l.n.Exists
+	// 0 no row in sqlite
+	if len(l.n) == 0 {
+		return nil
 	}
-	return nil
+	n := l.n[0]
+	// postgres, table exists
+	if n.Exists {
+		return n.Exists
+	}
+	// sqlite table exists
+	return n.Name != ""
 }
 
 type loadNodesLoader struct {

commit bbb6e5c27202623120c77ec6cb5342f4c44ec900

fix issue with checking for edges when table doesn't exist
broken by https://github.com/lolopinto/ent/pull/370/files#diff-c5b372ec274f3d44d99e8316cbd7ce357506cf96f9fb433cf132bdcdf2711870

ent/loader.go
@@ -503,7 +503,10 @@ func (l *loadAssocEdgeConfigExists) GetOutput() interface{} {
 		return n.Exists
 	}
 	// sqlite table exists
-	return n.Name != ""
+	if n.Name != "" {
+		return true
+	}
+	return nil
 }
 
 type loadNodesLoader struct {

test_setup/main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/lolopinto/ent/internal/db"
 )
 
+// run locally to fix db
+// in test_setup folder
+// `DB_CONNECTION_STRING=postgres://ola@localhost/ent_test go run .`
 func main() {
 	env := os.Getenv("DB_CONNECTION_STRING")
 	if env == "" {

ent/loader_test.go
@@ -1,0 +1,88 @@
+package ent_test
+
+import (
+	"testing"
+
+	"github.com/lolopinto/ent/ent"
+	"github.com/lolopinto/ent/internal/testingutils"
+	"github.com/lolopinto/ent/internal/testingutils/test_db"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type noTableSuite struct {
+	testingutils.Suite
+	tdb *test_db.TestDB
+}
+
+func (suite *noTableSuite) SetupSuite() {
+	suite.tdb = &test_db.TestDB{}
+
+	err := suite.tdb.BeforeAll()
+	require.Nil(suite.T(), err)
+
+	suite.Suite.ForceClean = true
+	suite.Suite.SetupSuite()
+}
+
+func (suite *noTableSuite) TearDownSuite() {
+	err := suite.tdb.AfterAll()
+	require.Nil(suite.T(), err)
+}
+
+func (suite *noTableSuite) TestLoadAssocEdges() {
+	res := <-ent.GenLoadAssocEdges()
+	assert.Len(suite.T(), res.Edges, 0)
+	assert.Nil(suite.T(), res.Err)
+}
+
+func TestNoTableSuite(t *testing.T) {
+	suite.Run(t, new(noTableSuite))
+}
+
+type tableExistsSuite struct {
+	testingutils.Suite
+	tdb *test_db.TestDB
+}
+
+func (suite *tableExistsSuite) SetupSuite() {
+	suite.tdb = &test_db.TestDB{
+		Tables: []test_db.Table{
+			{
+				Name: "assoc_edge_config",
+				Columns: []test_db.Column{
+					test_db.UUID("edge_type", test_db.PrimaryKey()),
+					test_db.Text("edge_name"),
+					test_db.Bool("symmetric_edge"),
+					test_db.UUID("inverse_edge_type", test_db.Nullable()),
+					test_db.Text("edge_table"),
+					test_db.Timestamp("created_at"),
+					test_db.Timestamp("updated_at"),
+				},
+			},
+		},
+	}
+
+	err := suite.tdb.BeforeAll()
+	require.Nil(suite.T(), err)
+
+	suite.Tables = []string{"assoc_edge_config"}
+	suite.Suite.ForceClean = true
+	suite.Suite.SetupSuite()
+}
+
+func (suite *tableExistsSuite) TearDownSuite() {
+	err := suite.tdb.AfterAll()
+	require.Nil(suite.T(), err)
+}
+
+func (suite *tableExistsSuite) TestLoadAssocEdges() {
+	res := <-ent.GenLoadAssocEdges()
+	assert.Len(suite.T(), res.Edges, 0)
+	assert.Nil(suite.T(), res.Err)
+}
+
+func TestTableExistsSuite(t *testing.T) {
+	suite.Run(t, new(tableExistsSuite))
+}