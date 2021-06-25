03e1dd7bce056e53664fc5c97946439a8395bab7
Ola Okelola <ola@okelola.com>
2021-06-24 17:37:33 -0700

    open todos legacy

--- examples/todo-sqlite/src/ent/account.ts
+++ examples/todo-sqlite/src/ent/account.ts
@@ -1,6 +1,17 @@
-import { AlwaysAllowPrivacyPolicy } from "@snowtop/snowtop-ts";
+import { AlwaysAllowPrivacyPolicy, query } from "@snowtop/snowtop-ts";
+import { gqlField } from "@snowtop/snowtop-ts/graphql";
 import { AccountBase } from "src/ent/internal";
+import { Todo } from "./todo";
 
 export class Account extends AccountBase {
   privacyPolicy = AlwaysAllowPrivacyPolicy;
+
+  // TODO this should eventually be a connection but we're starting here.
+  @gqlField({ name: "openTodosLegacy", type: "[Todo]" })
+  async openTodos() {
+    return await Todo.loadCustom(
+      this.viewer,
+      query.And(query.Eq("creator_id", this.id), query.Eq("completed", false)),
+    );
+  }
 }
