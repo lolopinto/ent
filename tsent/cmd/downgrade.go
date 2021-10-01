59df9a11999614f495192821e12efb72bda06134
Ola Okelola <ola@okelola.com>
2021-10-01 11:24:02 -0700

    integrate into codegen

--- tsent/cmd/downgrade.go
+++ tsent/cmd/downgrade.go
@@ -6,11 +6,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type downgradeArgs struct {
+	keepSchemaFiles bool
+}
+
+var downgradeInfo downgradeArgs
+
 var downgradeCmd = &cobra.Command{
 	Use:   "downgrade",
 	Short: "downgrade db",
 	Long:  `This downgrades the database to the specified version`,
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// another hardcoded place
 		cfg, err := codegen.NewConfig("src/schema", "")
@@ -18,6 +24,6 @@ var downgradeCmd = &cobra.Command{
 			return err
 		}
 
-		return db.DowngradeDB(cfg, args[0])
+		return db.DowngradeDB(cfg, args[0], downgradeInfo.keepSchemaFiles)
 	},
 }
