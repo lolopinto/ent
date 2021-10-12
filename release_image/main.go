0201e117a0d9774eccdacf20cec7a73e9dc3c780
lolopinto <10857143+lolopinto@users.noreply.github.com>
2021-10-11 19:41:16 -0700

    new image (#566)

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.0.28"
+const TAG = "0.0.29"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 16
@@ -32,7 +32,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.11"
-const TSENT_VERSION = "v0.0.28"
+const TSENT_VERSION = "v0.0.29"
 
 var SUFFIXES = []string{
 	"dev",
