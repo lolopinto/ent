ed93d9eb4d3fb8110161135f75738e760f059ba9
Ola Okelola <ola@okelola.com>
2021-11-01 12:49:22 -0700

    remove deleted file + revert change

--- examples/ent-rsvp/ent-rsvp-web/src/__generated__/guestGroupDeleteMutation.graphql.ts
+++ examples/ent-rsvp/ent-rsvp-web/src/__generated__/guestGroupDeleteMutation.graphql.ts
@@ -4,20 +4,22 @@
 
 import { ConcreteRequest } from "relay-runtime";
 export type GuestGroupDeleteInput = {
-  guestGroupID: string;
+    guestGroupID: string;
 };
 export type guestGroupDeleteMutationVariables = {
-  input: GuestGroupDeleteInput;
+    input: GuestGroupDeleteInput;
 };
 export type guestGroupDeleteMutationResponse = {
-  readonly guestGroupDelete: {
-    readonly deletedGuestGroupID: string | null;
-  };
+    readonly guestGroupDelete: {
+        readonly deletedGuestGroupID: string | null;
+    };
 };
 export type guestGroupDeleteMutation = {
-  readonly response: guestGroupDeleteMutationResponse;
-  readonly variables: guestGroupDeleteMutationVariables;
+    readonly response: guestGroupDeleteMutationResponse;
+    readonly variables: guestGroupDeleteMutationVariables;
 };
+
+
 
 /*
 mutation guestGroupDeleteMutation(
@@ -29,66 +31,66 @@ mutation guestGroupDeleteMutation(
 }
 */
 
-const node: ConcreteRequest = (function () {
-  var v0 = [
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "input"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": [
       {
-        defaultValue: null,
-        kind: "LocalArgument",
-        name: "input",
-      },
+        "kind": "Variable",
+        "name": "input",
+        "variableName": "input"
+      }
     ],
-    v1 = [
+    "concreteType": "GuestGroupDeletePayload",
+    "kind": "LinkedField",
+    "name": "guestGroupDelete",
+    "plural": false,
+    "selections": [
       {
-        alias: null,
-        args: [
-          {
-            kind: "Variable",
-            name: "input",
-            variableName: "input",
-          },
-        ],
-        concreteType: "GuestGroupDeletePayload",
-        kind: "LinkedField",
-        name: "guestGroupDelete",
-        plural: false,
-        selections: [
-          {
-            alias: null,
-            args: null,
-            kind: "ScalarField",
-            name: "deletedGuestGroupID",
-            storageKey: null,
-          },
-        ],
-        storageKey: null,
-      },
-    ];
-  return {
-    fragment: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: "Fragment",
-      metadata: null,
-      name: "guestGroupDeleteMutation",
-      selections: v1 /*: any*/,
-      type: "Mutation",
-      abstractKey: null,
-    },
-    kind: "Request",
-    operation: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: "Operation",
-      name: "guestGroupDeleteMutation",
-      selections: v1 /*: any*/,
-    },
-    params: {
-      cacheID: "56c15378aa1c8fc0531e0ed19e24c8da",
-      id: null,
-      metadata: {},
-      name: "guestGroupDeleteMutation",
-      operationKind: "mutation",
-      text: "mutation guestGroupDeleteMutation(\n  $input: GuestGroupDeleteInput!\n) {\n  guestGroupDelete(input: $input) {\n    deletedGuestGroupID\n  }\n}\n",
-    },
-  };
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "deletedGuestGroupID",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "guestGroupDeleteMutation",
+    "selections": (v1/*: any*/),
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "guestGroupDeleteMutation",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "56c15378aa1c8fc0531e0ed19e24c8da",
+    "id": null,
+    "metadata": {},
+    "name": "guestGroupDeleteMutation",
+    "operationKind": "mutation",
+    "text": "mutation guestGroupDeleteMutation(\n  $input: GuestGroupDeleteInput!\n) {\n  guestGroupDelete(input: $input) {\n    deletedGuestGroupID\n  }\n}\n"
+  }
+};
 })();
-(node as any).hash = "275de8104c4ee88a257d30a6b1818183";
+(node as any).hash = '275de8104c4ee88a257d30a6b1818183';
 export default node;
