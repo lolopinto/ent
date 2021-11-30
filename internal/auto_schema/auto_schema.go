4bb41580decd2483bed24d6cefba20b3198df232
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2021-11-29 20:49:27 -0500

    e2e workflow (#646)

--- internal/auto_schema/auto_schema.go
+++ internal/auto_schema/auto_schema.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 
 	"github.com/lolopinto/ent/ent/data"
+	"github.com/lolopinto/ent/internal/util"
 )
 
 func RunPythonCommand(pathToConfigs string, extraArgs ...string) error {
@@ -14,14 +15,33 @@ func RunPythonCommand(pathToConfigs string, extraArgs ...string) error {
 }
 
 func RunPythonCommandWriter(pathToConfigs string, w io.Writer, extraArgs ...string) error {
+	local := os.Getenv("LOCAL_AUTO_SCHEMA") == "true"
+
+	executable := "auto_schema"
 	args := []string{
 		fmt.Sprintf("-s=%s", pathToConfigs),
 		fmt.Sprintf("-e=%s", data.GetSQLAlchemyDatabaseURIgo()),
 	}
+	if local {
+		executable = "pipenv"
+		args = append(
+			[]string{
+				"run",
+				"python3",
+				"auto_schema/cli/__init__.py",
+			},
+			args...,
+		)
+	}
+
 	if len(extraArgs) > 0 {
 		args = append(args, extraArgs...)
 	}
-	cmd := exec.Command("auto_schema", args...)
+	cmd := exec.Command(executable, args...)
+	if local {
+		cmd.Env = append(cmd.Env, "LOCAL_AUTO_SCHEMA=true")
+		cmd.Dir = util.GetAbsolutePath("../../python/auto_schema")
+	}
 	cmd.Stdout = w
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
