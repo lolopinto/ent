lolopinto/ent

commit 0a2003ea2eae08f156320105c438aa7734df5003

auto_schema: CLI command to get changes (#408)

python/auto_schema/auto_schema/diff.py
@@ -1,0 +1,178 @@
+from __future__ import annotations
+from sqlalchemy.sql.sqltypes import String
+from .clause_text import get_clause_text
+from .change_type import ChangeType
+from .ops import MigrateOpInterface
+
+import alembic.operations.ops as alembicops
+
+from typing import List, Sequence, TypedDict
+
+
+class Change(TypedDict):
+    change: ChangeType
+    desc: String
+    col: String
+
+
+class Diff(object):
+
+    def __init__(self: Diff, diff: Sequence[alembicops.MigrateOperation], group_by_table=True):
+        self._diff = diff
+        self._changes = {}
+        self._changes_list = []
+        self._group_by_table = group_by_table
+
+        self._exec_ops(self._diff)
+
+    def list_changes(self: Diff) -> List[Change]:
+        return self._changes_list
+
+    def changes(self: Diff):
+        return self._changes
+
+    def _exec_ops(self: Diff, ops: Sequence[alembicops.MigrateOperation]):
+        class_name_map = {
+            'CreateTableOp': self._create_table,
+            'DropTableOp': self._drop_table,
+            'ModifyTableOps': self._modify_table,
+            'AddColumnOp': self._add_column,
+            'DropColumnOp': self._drop_column,
+            'CreateIndexOp': self._create_index,
+            'DropIndexOp': self._drop_index,
+            'CreateForeignKeyOp': self._create_foreign_key,
+            'AlterColumnOp': self._alter_column,
+            'CreateUniqueConstraintOp': self._create_unique_constraint,
+            # this is still sadly used
+            'DropConstraintOp': self._drop_constraint,
+            'CreateCheckConstraintOp': self._create_check_constraint,
+        }
+
+        for op in ops:
+            if isinstance(op, MigrateOpInterface):
+                self._custom_migrate_op(op)
+            else:
+                class_name_map[type(op).__name__](op)
+
+    def _custom_migrate_op(self: Diff, op: MigrateOpInterface):
+        self._append_change(op.get_table_name(), {
+            "change": op.get_change_type(),
+            "desc": op.get_revision_message(),
+        })
+
+    def _create_table(self: Diff, op: alembicops.CreateTableOp):
+        self._set_change(op.table_name,                 {
+            "change": ChangeType.ADD_TABLE,
+            "desc": 'add %s table' % op.table_name,
+        })
+
+    def _drop_table(self: Diff, op: alembicops.DropTableOp):
+        self._set_change(op.table_name,                 {
+            "change": ChangeType.DROP_TABLE,
+            "desc": 'drop %s table' % op.table_name,
+        })
+
+    def _modify_table(self: Diff, op: alembicops.ModifyTableOps):
+        self._exec_ops(op.ops)
+
+    def _add_column(self: Diff, op: alembicops.AddColumnOp):
+        self._append_change(op.table_name, {
+            "change": ChangeType.ADD_COLUMN,
+            "desc": "add column %s to table %s" % (op.column.name, op.table_name),
+            "col": op.column.name,
+        })
+
+    def _drop_column(self: Diff, op: alembicops.DropColumnOp):
+        self._append_change(op.table_name, {
+            "change": ChangeType.DROP_COLUMN,
+            "desc": "drop column %s from table %s" % (op.column_name, op.table_name),
+            "col": op.column_name,
+        })
+
+    def _create_index(self: Diff, op: alembicops.CreateIndexOp):
+        self._append_change(op.table_name, {
+            "change": ChangeType.CREATE_INDEX,
+            "desc": 'add index %s to %s' % (op.index_name, op.table_name),
+        })
+
+    def _drop_index(self: Diff, op: alembicops.DropIndexOp):
+        self._append_change(op.table_name, {
+            "change": ChangeType.DROP_INDEX,
+            "desc": 'drop index %s from %s' % (op.index_name, op.table_name),
+        })
+
+    def _create_foreign_key(self: Diff, op: alembicops.CreateForeignKeyOp):
+        self._append_change(op.source_table, {
+            "change": ChangeType.CREATE_FOREIGN_KEY,
+            "desc": 'create fk constraint %s on %s' % (op.constraint_name, op.source_table),
+        })
+
+    def _create_unique_constraint(self: Diff, op: alembicops.CreateUniqueConstraintOp):
+        self._append_change(op.table_name, {
+            "change": ChangeType.CREATE_UNIQUE_CONSTRAINT,
+            "desc": 'add unique constraint %s' % op.constraint_name,
+        })
+
+    def _drop_constraint(self: Diff, op: alembicops.DropConstraintOp):
+        self._append_change(op.table_name, {
+            "change": ChangeType.DROP_CHECK_CONSTRAINT,
+            "desc": 'drop constraint %s from %s' % (op.constraint_name, op.table_name)
+        })
+
+    def _create_check_constraint(self: Diff, op: alembicops.CreateCheckConstraintOp):
+        self._append_change(op.table_name, {
+            "change": ChangeType.CREATE_CHECK_CONSTRAINT,
+            "desc": 'add constraint %s to %s' % (op.constraint_name, op.table_name)
+        })
+
+    def _alter_column(self: Diff, op: alembicops.AlterColumnOp):
+        def get_desc(op: alembicops.AlterColumnOp):
+            if op.modify_type is not None:
+                return 'modify column %s type from %s to %s' % (op.column_name, op.existing_type, op.modify_type)
+            elif op.modify_nullable is not None:
+                return 'modify nullable value of column %s from %s to %s' % (op.column_name, op.existing_nullable, op.modify_nullable)
+            elif op.modify_server_default is not None:
+                return 'modify server_default value of column %s from %s to %s' % (
+                    op.column_name,
+                    get_clause_text(op.existing_server_default),
+                    get_clause_text(op.modify_server_default))
+            elif op.modify_comment:
+                return "modify comment of column %s"
+            elif op.modify_name:
+                return "modify name of column %s"
+            elif op.modify_server_default is None and op.existing_server_default is not None:
+                return 'modify server_default value of column %s from %s to None' % (
+                    op.column_name,
+                    get_clause_text(op.existing_server_default)
+                )
+            else:
+                raise ValueError("unsupported alter_column op")
+
+        self._append_change(op.table_name, {
+            "change": ChangeType.ALTER_COLUMN,
+            "desc": get_desc(op),
+        })
+
+    def _append_change(self: Diff, table_name: String, change: Change):
+        if not self._group_by_table:
+            self._changes_list.append(change)
+            return
+
+        changes = []
+        if table_name in self._changes:
+            changes = self._changes[table_name]
+        changes.append(change)
+        self._changes[table_name] = changes
+
+    # throws if there's changes already set for a table
+    def _set_change(self: Diff, table_name: String, change: Change):
+        if not self._group_by_table:
+            self._changes_list.append(change)
+            return
+
+        if table_name not in self._changes:
+            self._changes[table_name] = [
+                change
+            ]
+        else:
+            raise "there shouldn't already be an entry for table %s" % table_name

python/auto_schema/tests/runner_test.py
@@ -1,4 +1,6 @@
 import pprint
+from auto_schema.diff import Diff
+from auto_schema.change_type import ChangeType
 import pytest
 import os
 
@@ -9,6 +11,7 @@ from sqlalchemy.dialects import postgresql
 from . import conftest
 from auto_schema import runner
 from auto_schema import ops
+from auto_schema.clause_text import get_clause_text
 
 
 def get_new_metadata_for_runner(r):
@@ -206,9 +209,9 @@ def validate_column(schema_column, db_column, metadata, dialect):
 
 
 def validate_column_server_default(schema_column, db_column):
-    schema_clause_text = runner.Runner.get_clause_text(
+    schema_clause_text = get_clause_text(
         schema_column.server_default)
-    db_clause_text = runner.Runner.get_clause_text(db_column.server_default)
+    db_clause_text = get_clause_text(db_column.server_default)
 
     if isinstance(schema_column.type, sa.Boolean):
         schema_clause_text = runner.Runner.convert_postgres_boolean(
@@ -1222,13 +1225,48 @@ class TestPostgresRunner(BaseTestRunner):
 
         diff = r2.compute_changes()
 
+        exp_message = 'drop column meaning_of_life from table accounts'
+        assert r2.revision_message() == exp_message
         assert len(diff) == 1
 
-        assert r2.revision_message() == 'drop column meaning_of_life'
+        d = Diff(diff)
+        changes = d.changes()
+        assert len(changes) == 1
+        l = changes.get('accounts')
+        exp_change = {
+            'change': ChangeType.DROP_COLUMN,
+            'desc': exp_message,
+            'col': 'meaning_of_life',
+        }
+        assert len(l) == 1
+        assert l[0] == exp_change
+        d2 = Diff(diff, group_by_table=False)
+        assert d2.list_changes() == [exp_change]
 
         r2.run()
         assert_num_files(r2, 2)
         validate_metadata_after_change(r2, new_metadata)
+
+    @pytest.mark.usefixtures("metadata_with_table")
+    def test_add_multiple_cols(self, new_test_runner, metadata_with_table):
+        r = new_test_runner(metadata_with_table)
+        run_and_validate_with_standard_metadata_tables(r, metadata_with_table)
+
+        new_metadata = conftest.metadata_with_cols_added_to_table(
+            metadata_with_table)
+        new_metadata.bind = r.get_connection()
+        r2 = new_test_runner(new_metadata, r)
+
+        diff = r2.compute_changes()
+
+        d = Diff(diff)
+        changes = d.changes()
+        assert len(changes) == 1
+        l = changes.get('accounts')
+        assert len(l) == 2
+        for change in l:
+            assert change['change'] == ChangeType.ADD_COLUMN
+            assert change['col'] in ['new_column', 'rainbow']
 
     @pytest.mark.usefixtures("metadata_with_table")
     def test_new_enum_column_added_then_removed(self, new_test_runner, metadata_with_table):
@@ -1243,6 +1281,24 @@ class TestPostgresRunner(BaseTestRunner):
         diff = r2.compute_changes()
 
         assert len(diff) == 2
+
+        d = Diff(diff)
+        changes = d.changes()
+        assert len(changes) == 2
+        l = changes.get('accounts')
+        assert len(l) == 1
+        assert l[0] == {
+            'change': ChangeType.ADD_COLUMN,
+            'desc': 'add column rainbow to table accounts',
+            'col': 'rainbow',
+        }
+
+        l2 = changes.get('enum_schema')
+        assert len(l2) == 1
+        assert l2[0] == {
+            'change': ChangeType.ADD_ENUM,
+            'desc': 'add enum rainbow_type',
+        }
 
         add_enum = [op for op in diff if isinstance(op, ops.AddEnumOp)]
         modify_table_ops = [op for op in diff if isinstance(

python/auto_schema/auto_schema/change_type.py
@@ -1,0 +1,25 @@
+from enum import Enum
+
+
+# subclass of str for enum encoding
+class ChangeType(str, Enum):
+    ADD_TABLE = "add_table"
+    DROP_TABLE = "drop_table"
+    ADD_COLUMN = "add_column"
+    DROP_COLUMN = "drop_column"
+    CREATE_INDEX = "create_index"
+    DROP_INDEX = "drop_index"
+    CREATE_FOREIGN_KEY = "create_foreign_key"
+    ALTER_COLUMN = "alter_column"
+    CREATE_UNIQUE_CONSTRAINT = "create_unique_constraint"
+    ADD_EDGES = "add_edges"
+    REMOVE_EDGES = "remove_edges"
+    MODIFY_EDGE = "modify_edge"
+    ADD_ROWS = "add_rows"
+    REMOVE_ROWS = "remove_rows"
+    MODIFY_ROWS = "modify_rows"
+    ALTER_ENUM = "alter_enum"
+    ADD_ENUM = "add_enum"
+    DROP_ENUM = "drop_enum"
+    CREATE_CHECK_CONSTRAINT = "create_check_constraint"
+    DROP_CHECK_CONSTRAINT = "drop_check_constraint"

python/auto_schema/auto_schema/clause_text.py
@@ -1,0 +1,24 @@
+from sqlalchemy.sql.schema import DefaultClause
+from sqlalchemy.sql.elements import TextClause
+
+
+def get_clause_text(server_default):
+    if server_default is None:
+        return server_default
+
+    def normalize(arg):
+        # return the underlying string instead of quoted
+        arg = str(arg).strip("'")
+
+        # strip the extra text padding added so we can compare effectively
+        if arg.endswith("'::text"):
+            return arg.strip("'::text")
+        return arg
+
+    if isinstance(server_default, TextClause):
+        return normalize(server_default.text)
+
+    if isinstance(server_default, DefaultClause):
+        return normalize(server_default.arg)
+
+    return normalize(server_default)

python/auto_schema/auto_schema/cli/__init__.py
@@ -1,19 +1,13 @@
 import os
 import sys
 import argparse
-from alembic.command import heads
 
 # if env variable is set, manipulate the path to put local
-# current directory over possibly installed auto_schema so that we 
+# current directory over possibly installed auto_schema so that we
 # see local changes
 if os.getenv('LOCAL_AUTO_SCHEMA') == 'true':
     sys.path.insert(0, os.getcwd())
 
-# if env variable is set, manipulate the path to put local
-# current directory over possibly installed auto_schema so that we 
-# see local changes
-if os.getenv('LOCAL_AUTO_SCHEMA') == 'true':
-    sys.path.insert(0, os.getcwd())
 
 # run from auto_schema root. conflicts with pip-installed auto_schema when that exists so can't have
 # that installed when runnning this...
@@ -39,6 +33,9 @@ parser.add_argument('--heads', help='alembic heads', action='store_true')
 parser.add_argument('--branches', help='alembic branches', action='store_true')
 parser.add_argument('--stamp', help='alembic stamp')
 parser.add_argument('--edit', help='alembic edit')
+parser.add_argument(
+    '--changes', help='get changes in schema', action='store_true')
+
 
 def main():
     args = parser.parse_args()
@@ -69,6 +66,8 @@ def main():
             r.stamp(args.stamp)
         elif args.edit is not None:
             r.edit(args.edit)
+        elif args.changes is not None:
+            r.changes()
         else:
             r.run()
 

python/auto_schema/auto_schema/ops.py
@@ -1,9 +1,27 @@
 import alembic.operations.ops as alembicops
 from alembic.operations import Operations, MigrateOperation
+import abc
+
+from sqlalchemy.sql.sqltypes import String
+from .change_type import ChangeType
+
+
+class MigrateOpInterface(MigrateOperation, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def get_revision_message(self) -> String:
+        pass
+
+    @abc.abstractmethod
+    def get_change_type(self) -> ChangeType:
+        pass
+
+    @abc.abstractmethod
+    def get_table_name(self) -> String:
+        pass
 
 
 @Operations.register_operation("add_edges")
-class AddEdgesOp(MigrateOperation):
+class AddEdgesOp(MigrateOpInterface):
 
     """Add one or more new edges."""
 
@@ -21,12 +39,18 @@ class AddEdgesOp(MigrateOperation):
     def reverse(self):
         return RemoveEdgesOp(self.edges, schema=self.schema)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return _get_revision_message_for_edges(self.edges, "add edge %s", "add edges %s")
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.ADD_EDGES
+
+    def get_table_name(self) -> String:
+        return "assoc_edge_config"
 
 
 @Operations.register_operation("remove_edges")
-class RemoveEdgesOp(MigrateOperation):
+class RemoveEdgesOp(MigrateOpInterface):
 
     """Removes one or more existing edges."""
 
@@ -44,8 +68,14 @@ class RemoveEdgesOp(MigrateOperation):
     def reverse(self):
         return AddEdgesOp(self.edges, schema=self.schema)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return _get_revision_message_for_edges(self.edges, "remove edge %s", "remove edges %s")
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.REMOVE_EDGES
+
+    def get_table_name(self) -> String:
+        return "assoc_edge_config"
 
 
 def _get_revision_message_for_edges(edges, single_edge_msg, multi_edge_msg):
@@ -64,7 +94,7 @@ def _get_revision_message_for_rows(rows, table_name, single_row_msg, multi_row_m
 
 
 @Operations.register_operation("modify_edge")
-class ModifyEdgeOp(MigrateOperation):
+class ModifyEdgeOp(MigrateOpInterface):
 
     """Modify an existing edge"""
 
@@ -84,13 +114,19 @@ class ModifyEdgeOp(MigrateOperation):
     def reverse(self):
         return ModifyEdgeOp(self.edge_type, self.old_edge, self.new_edge, schema=self.schema)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         # assume name is not changing. if this is changing, this needs to be smarter
         return "modify edge %s" % (self.old_edge['edge_name'])
 
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.MODIFY_EDGE
+
+    def get_table_name(self) -> String:
+        return "assoc_edge_config"
+
 
 @Operations.register_operation("add_rows")
-class AddRowsOp(MigrateOperation):
+class AddRowsOp(MigrateOpInterface):
 
     """Add one or more new rows to table."""
 
@@ -110,12 +146,18 @@ class AddRowsOp(MigrateOperation):
     def reverse(self):
         return RemoveRowsOp(self.table_name, self.pkeys, self.rows, schema=self.schema)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return _get_revision_message_for_rows(self.rows, self.table_name, "add row to %s", "add rows to %s")
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.ADD_ROWS
+
+    def get_table_name(self) -> String:
+        return self.table_name
 
 
 @Operations.register_operation("remove_rows")
-class RemoveRowsOp(MigrateOperation):
+class RemoveRowsOp(MigrateOpInterface):
 
     """Removes one or more existing rows."""
 
@@ -135,20 +177,18 @@ class RemoveRowsOp(MigrateOperation):
     def reverse(self):
         return AddRowsOp(self.table_name, self.pkeys, self.rows, schema=self.schema)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return _get_revision_message_for_rows(self.rows, self.table_name, "remove row from %s", "remove rows from %s")
 
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.REMOVE_ROWS
 
-def _get_revision_message(edges, single_edge_msg, multi_edge_msg):
-    if len(edges) == 1:
-        return single_edge_msg % (edges[0]['edge_name'])
-
-    edge_names = [edge['edge_name'] for edge in edges]
-    return multi_edge_msg % (", ".join(sorted(edge_names)))
+    def get_table_name(self) -> String:
+        return self.table_name
 
 
 @Operations.register_operation("modify_rows")
-class ModifyRowsOp(MigrateOperation):
+class ModifyRowsOp(MigrateOpInterface):
 
     """Modify an existing row"""
 
@@ -169,12 +209,18 @@ class ModifyRowsOp(MigrateOperation):
     def reverse(self):
         return ModifyRowsOp(self.table_name, self.pkeys, self.old_rows, self.rows, schema=self.schema)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return "modify rows in %s" % self.table_name
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.MODIFY_ROWS
+
+    def get_table_name(self) -> String:
+        return self.table_name
 
 
 @Operations.register_operation("alter_enum")
-class AlterEnumOp(MigrateOperation):
+class AlterEnumOp(MigrateOpInterface):
 
     """Alters enum."""
 
@@ -194,8 +240,14 @@ class AlterEnumOp(MigrateOperation):
     def reverse(self):
         return NoDowngradeOp()
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return 'alter enum %s, add value %s' % (self.enum_name, self.value)
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.ALTER_ENUM
+
+    def get_table_name(self) -> String:
+        return "enum_schema"
 
 
 @Operations.register_operation("no_downgrade")
@@ -209,7 +261,7 @@ class NoDowngradeOp(MigrateOperation):
 
 
 @Operations.register_operation("add_enum_type")
-class AddEnumOp(MigrateOperation):
+class AddEnumOp(MigrateOpInterface):
 
     """Adds enum type."""
 
@@ -228,12 +280,18 @@ class AddEnumOp(MigrateOperation):
     def reverse(self):
         return DropEnumOp(self.enum_name, self.values)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return 'add enum %s' % (self.enum_name)
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.ADD_ENUM
+
+    def get_table_name(self) -> String:
+        return "enum_schema"
 
 
 @Operations.register_operation("drop_enum_type")
-class DropEnumOp(MigrateOperation):
+class DropEnumOp(MigrateOpInterface):
 
     """Drop enum type."""
 
@@ -252,18 +310,34 @@ class DropEnumOp(MigrateOperation):
     def reverse(self):
         return AddEnumOp(self.enum_name, self.values)
 
-    def get_revision_message(self):
+    def get_revision_message(self) -> String:
         return 'drop enum %s' % (self.enum_name)
 
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.DROP_ENUM
+
+    def get_table_name(self) -> String:
+        return "enum_schema"
 
 # overriding this so that we can implement dispatch and render
 # alembic for some reason doesn't have it...
-class OurCreateCheckConstraintOp(alembicops.CreateCheckConstraintOp):
-    pass
 
+
+class OurCreateCheckConstraintOp(MigrateOpInterface, alembicops.CreateCheckConstraintOp):
+
+    def get_revision_message(self) -> String:
+        return 'add constraint %s to %s' % (self.constraint_name, self.table_name)
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.CREATE_CHECK_CONSTRAINT
+
+    def get_table_name(self) -> String:
+        return self.table_name
 
 # need to override this so that when we reverse, we render ours instead of theirs
-class OurDropConstraintOp(alembicops.DropConstraintOp):
+
+
+class OurDropConstraintOp(MigrateOpInterface, alembicops.DropConstraintOp):
 
     def reverse(self):
         if self._orig_constraint is None:
@@ -272,3 +346,12 @@ class OurDropConstraintOp(alembicops.DropConstraintOp):
                 "original constraint is not present"
             )
         return OurCreateCheckConstraintOp.from_constraint(self._orig_constraint)
+
+    def get_revision_message(self) -> String:
+        return 'drop constraint %s from %s' % (self.constraint_name, self.table_name)
+
+    def get_change_type(self) -> ChangeType:
+        return ChangeType.DROP_CHECK_CONSTRAINT
+
+    def get_table_name(self) -> String:
+        return self.table_name

commit 6c5925dcfc7cd79662e666b5af22439f576e17bf

fix deleting a table which has an index
seemingly broken since https://github.com/lolopinto/ent/pull/408

python/auto_schema/auto_schema/diff.py
@@ -61,13 +61,13 @@ class Diff(object):
         })
 
     def _create_table(self: Diff, op: alembicops.CreateTableOp):
-        self._set_change(op.table_name,                 {
+        self._append_change(op.table_name,                 {
             "change": ChangeType.ADD_TABLE,
             "desc": 'add %s table' % op.table_name,
         })
 
     def _drop_table(self: Diff, op: alembicops.DropTableOp):
-        self._set_change(op.table_name,                 {
+        self._append_change(op.table_name,                 {
             "change": ChangeType.DROP_TABLE,
             "desc": 'drop %s table' % op.table_name,
         })
@@ -163,19 +163,3 @@ class Diff(object):
             changes = self._changes[table_name]
         changes.append(change)
         self._changes[table_name] = changes
-
-    # throws if there's changes already set for a table
-    def _set_change(self: Diff, table_name: String, change: Change):
-        if not self._group_by_table:
-            self._changes_list.append(change)
-            return
-
-        if table_name not in self._changes:
-            self._changes[table_name] = [
-                change
-            ]
-        else:
-            # TODO: this error needs to be better
-            # https://github.com/lolopinto/ent/issues/499
-            raise Exception(
-                "there shouldn't already be an entry for table %s" % table_name)

ts/Dockerfile
@@ -60,6 +60,8 @@ WORKDIR /app
 RUN npm install --save-dev tsconfig-paths@3.11.0
 
 COPY --from=golang-image /go/bin/tsent /go/bin/tsent
+# NOTE: if ever debugging in docker image, code is at 
+# /opt/venv/lib/python3.8/site-packages/auto_schema/
 COPY --from=python-image /opt/venv /opt/venv
 # RUN alias auto_schema=auto_schema_test
 COPY --from=golang-image /go/pkg/mod/github.com/lolopinto/ent@$GOLANG_VERSION /go/pkg/mod/github.com/lolopinto/ent@$GOLANG_VERSION

python/auto_schema/tests/runner_test.py
@@ -656,6 +656,65 @@ class BaseTestRunner(object):
         testingutils.run_and_validate_with_standard_metadata_tables(
             r, metadata_with_bigint, new_table_names=['tbl'])
 
+    @pytest.mark.usefixtures("metadata_with_table")
+    def test_drop_table_with_index(self, new_test_runner, metadata_with_table):
+        r = new_test_runner(metadata_with_table)
+        testingutils.run_and_validate_with_standard_metadata_tables(
+            r, metadata_with_table)
+
+        r2 = testingutils.recreate_with_new_metadata(
+            r, new_test_runner, metadata_with_table, conftest.metadata_with_table_with_index)
+
+        message = r2.revision_message()
+        assert message == "add index accounts_first_name_idx to accounts"
+
+        r2.run()
+        testingutils.assert_num_files(r2, 2)
+        testingutils.assert_num_tables(r2, 2)
+
+        # no tables
+        new_metadata = sa.MetaData()
+        r2 = testingutils.new_runner_from_old(
+            r,
+            new_test_runner,
+            new_metadata
+        )
+
+        diff = r2.compute_changes()
+
+        assert len(diff) == 2
+
+        modify_table = [op for op in diff if isinstance(
+            op, alembicops.ModifyTableOps)]
+        drop_table = [op for op in diff if isinstance(
+            op, alembicops.DropTableOp)]
+        assert len(modify_table) == 1
+        assert len(drop_table) == 1
+
+        d = Diff(diff, group_by_table=False)
+        assert d.list_changes() == [{
+            'change': ChangeType.DROP_INDEX,
+            'desc': 'drop index accounts_first_name_idx from accounts',
+        }, {
+            'change': ChangeType.DROP_TABLE,
+            'desc': 'drop accounts table',
+        }]
+
+        d2 = Diff(diff, group_by_table=True)
+        assert d2.changes() == {
+            'accounts': [{
+                'change': ChangeType.DROP_INDEX,
+                'desc': 'drop index accounts_first_name_idx from accounts',
+            }, {
+                'change': ChangeType.DROP_TABLE,
+                'desc': 'drop accounts table',
+            }]
+        }
+
+        r2.run()
+        testingutils.assert_num_files(r2, 3)
+        testingutils.validate_metadata_after_change(r2, new_metadata)
+
 
 class TestPostgresRunner(BaseTestRunner):
 