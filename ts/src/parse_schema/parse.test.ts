f61c825889f14f3860e25ffc0b1d279c2f4cf86e
Ola Okelola <ola@okelola.com>
2022-02-08 23:00:05 -0800

    handle instantiating schema

---
+++ ts/src/parse_schema/parse.test.ts
@@ -0,0 +1,34 @@
+import { FieldMap, Schema, Field } from "src/schema";
+import { StringType } from "../schema/field";
+import { BaseEntSchema } from "../schema/base_schema";
+import { parseSchema } from "./parse";
+
+test("legacy class", async () => {
+  class Foo extends BaseEntSchema {
+    fields: FieldMap = {
+      name: StringType(),
+    };
+  }
+
+  parseSchema({ foo: Foo });
+});
+
+test("implicit schema", async () => {
+  const Foo: Schema = {
+    fields: {
+      name: StringType(),
+    },
+  };
+
+  parseSchema({ foo: Foo });
+});
+
+test("new API with constructor config", async () => {
+  const Foo = new BaseEntSchema({
+    fields: {
+      name: StringType(),
+    },
+  });
+
+  parseSchema({ foo: Foo });
+});
