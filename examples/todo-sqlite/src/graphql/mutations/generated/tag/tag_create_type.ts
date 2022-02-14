4e2acdcdc2c753ce624eca3e4b011c17b267a730
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2022-02-14 13:05:55 -0800

    Fix dangling import (#751)

--- examples/todo-sqlite/src/graphql/mutations/generated/tag/tag_create_type.ts
+++ examples/todo-sqlite/src/graphql/mutations/generated/tag/tag_create_type.ts
@@ -12,7 +12,6 @@ import {
   GraphQLString,
 } from "graphql";
 import { RequestContext } from "@snowtop/ent";
-import { mustDecodeIDFromGQLID } from "@snowtop/ent/graphql";
 import { Tag } from "src/ent/";
 import CreateTagAction, {
   TagCreateInput,
