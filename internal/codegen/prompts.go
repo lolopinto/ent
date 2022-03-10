1ed00277c2e73a607b5b738c9e1e81bd114af45d
Ola Okelola <ola@okelola.com>
2022-03-10 13:23:33 -0800

    use changes from compare schema to check for prompts instead of calling into db/auto_schema

--- internal/codegen/prompts.go
+++ internal/codegen/prompts.go
@@ -6,21 +6,20 @@ import (
 
 	"github.com/lolopinto/ent/internal/prompt"
 	"github.com/lolopinto/ent/internal/schema"
+	"github.com/lolopinto/ent/internal/schema/change"
 )
 
-// TODO: this should all be in schema but there's dependency issues
-// because codegen depends on schema and we need schema to depend on the path to schema which we need to fix
-func checkAndHandlePrompts(p *Processor) error {
+func getPromptsFromDB(p *Processor) ([]prompt.Prompt, error) {
 	// get db changes and store in Buffer (output of auto_schema --changes)
 	buf, err := dbChanges(p.Config)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	changes := make(map[string][]deprecatedChange)
 
 	if err := json.Unmarshal(buf.Bytes(), &changes); err != nil {
-		return err
+		return nil, err
 	}
 
 	if len(changes) == 0 {
@@ -28,7 +27,26 @@ func checkAndHandlePrompts(p *Processor) error {
 		p.noDBChanges = true
 	}
 
-	prompts, err := getPrompts(p.Schema, changes)
+	return getPromptsFromDBChanges(p.Schema, changes)
+}
+
+// TODO: this should all be in schema but there's dependency issues
+// because codegen depends on schema and we need schema to depend on the path to schema which we need to fix
+func checkAndHandlePrompts(p *Processor) error {
+	useChanges := p.Config.useChanges
+	var prompts []prompt.Prompt
+	var err error
+
+	if useChanges {
+		if len(p.ChangeMap) == 0 {
+			// we know there's no db changes so we should flag this so that we don't call into python in the future to try and make changes
+			p.noDBChanges = true
+		}
+		prompts, err = getPromptsFromChanges(p.Schema, p.ChangeMap)
+
+	} else {
+		prompts, err = getPromptsFromDB(p)
+	}
 	if err != nil {
 		return err
 	}
@@ -42,7 +60,8 @@ func checkAndHandlePrompts(p *Processor) error {
 	return nil
 }
 
-func getPrompts(s *schema.Schema, changes map[string][]deprecatedChange) ([]prompt.Prompt, error) {
+// TODO eventually deprecate this. for now, we keep this to have both paths just in case
+func getPromptsFromDBChanges(s *schema.Schema, changes map[string][]deprecatedChange) ([]prompt.Prompt, error) {
 	var prompts []prompt.Prompt
 	for tableName, changes := range changes {
 		for _, change := range changes {
@@ -57,6 +76,38 @@ func getPrompts(s *schema.Schema, changes map[string][]deprecatedChange) ([]prom
 			field := nodeData.FieldInfo.GetFieldByColName(change.Col)
 			if field == nil {
 				return nil, fmt.Errorf("couldn't find field in node data %s for column %s", nodeData.Node, change.Col)
+			}
+
+			// adding new field which isn't nullable, prompt needed
+			if !field.Nullable() {
+				prompts = append(prompts, &prompt.YesNoQuestion{
+					Question: fmt.Sprintf(
+						"You're adding a new field '%s' to an existing Node '%s' which isn't nullable. This could result in database errors. Are you sure you want to do that? Y/N: ",
+						field.FieldName,
+						nodeData.Node,
+					),
+					NoHandler: prompt.ExitHandler,
+					// YesHandler: prompt.LogHandler("yes answered \n"),
+				})
+			}
+		}
+	}
+
+	return prompts, nil
+}
+
+func getPromptsFromChanges(s *schema.Schema, changes change.ChangeMap) ([]prompt.Prompt, error) {
+	var prompts []prompt.Prompt
+	for _, info := range s.Nodes {
+		for _, c := range changes[info.NodeData.Node] {
+			if c.Change != change.AddField {
+				continue
+			}
+			nodeData := info.NodeData
+
+			field := nodeData.FieldInfo.GetFieldByName(c.Name)
+			if field == nil {
+				return nil, fmt.Errorf("couldn't find field in node data %s for field %s", nodeData.Node, c.Name)
 			}
 
 			// adding new field which isn't nullable, prompt needed
