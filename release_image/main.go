cb73012d4e518d8b31ba07bd184f7aa5908d1533
Ola Okelola <ola@okelola.com>
2022-03-14 10:46:24 -0700

    bump

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.0.37.alpha12"
+const TAG = "0.0.37.alpha13"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 17
@@ -32,7 +32,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.17"
-const TSENT_VERSION = "v0.0.38-alpha9"
+const TSENT_VERSION = "v0.0.38-alpha10"
 
 var SUFFIXES = []string{
 	"dev",
