45a8a0754ebc7bd26f54facffa37e7e77d1c617d
Ola Okelola <ola@okelola.com>
2022-03-30 14:44:12 -0700

    for https://github.com/lolopinto/ent/releases/tag/v0.0.39-alpha3

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.0.38.alpha2"
+const TAG = "0.0.38.alpha3"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 17
@@ -32,7 +32,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.17"
-const TSENT_VERSION = "v0.0.39-alpha2"
+const TSENT_VERSION = "v0.0.39-alpha3"
 
 var SUFFIXES = []string{
 	"dev",
