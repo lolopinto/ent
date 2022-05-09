91220988c506727a0c72e159f3e44eae301b3c44
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2022-05-09 13:47:45 -0700

    add some tests (#878)

--- ts/src/testutils/context/test_context.ts
+++ ts/src/testutils/context/test_context.ts
@@ -2,9 +2,22 @@ import { Context, Viewer } from "../../core/base";
 import { ContextCache } from "../../core/context";
 import { LoggedOutViewer } from "../../core/viewer";
 
+interface viewerWithContext extends Viewer {
+  setContext(ctx: Context): any;
+}
+
 export class TestContext implements Context {
+  constructor(viewer?: Viewer) {
+    if (viewer) {
+      this.viewer = viewer;
+      if ((viewer as viewerWithContext).setContext !== undefined) {
+        (viewer as viewerWithContext).setContext(this);
+      }
+    }
+  }
+
   cache: ContextCache = new ContextCache();
-  viewer = new LoggedOutViewer(this);
+  viewer: Viewer = new LoggedOutViewer(this);
 
   getViewer(): Viewer {
     return this.viewer;
