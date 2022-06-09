0c292fc8222d772a27f93757385cb6ca2c81bc24
Ola Okelola <ola@okelola.com>
2022-06-09 10:00:59 -0700

    nodejs changes

--- ts/src/scripts/migrate_v0.1.ts
+++ ts/src/scripts/migrate_v0.1.ts
@@ -4,39 +4,38 @@ import { TransformEnt } from "../tsc/transform_ent";
 import { moveGenerated } from "../tsc/move_generated";
 import { TransformAction } from "../tsc/transform_action";
 import minimist from "minimist";
-
-// call from docker
-// since clients [sh/w]ouldn't have ts-node...
+import { getCustomInfo } from "../tsc/ast";
 
 // locally
 // ts-node --swc --project ./tsconfig.json -r tsconfig-paths/register ../../../ts/src/scripts/migrate_v0.1.ts
-async function main() {
+function main() {
+  const customInfo = getCustomInfo();
   const options = minimist(process.argv.slice(2));
 
   // install 0.1.x dependencies
   // maybe provide options to make this easier if someone wants to do this in steps to see what's happening
   if (options.move_generated) {
-    moveGenerated();
+    moveGenerated(customInfo.relativeImports!!);
   }
   // codegen  write-all --disable-custom-graphql
   if (options.transform_schema) {
-    transform(new TransformSchema());
+    transform(
+      new TransformSchema(
+        customInfo.relativeImports!!,
+        options.old_base_class,
+        options.new_schema_class,
+        options.transform_path,
+      ),
+    );
   }
   // codegen write-all --disable-custom-graphql
   if (options.transform_ent) {
     transform(new TransformEnt());
   }
   if (options.transform_action) {
-    transform(new TransformAction());
+    transform(new TransformAction(customInfo));
   }
   // codegen write-all
 }
 
-main()
-  .then(() => {
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+main();
