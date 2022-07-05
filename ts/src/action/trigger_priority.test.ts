578afbb5fb6daea0db5103ba3289a65db37ea821
Ola Okelola <ola@okelola.com>
2022-07-05 08:07:37 -1000

    more test

--- ts/src/action/trigger_priority.test.ts
+++ ts/src/action/trigger_priority.test.ts
@@ -183,4 +183,72 @@ function commonTests() {
 
     await action.saveX();
   });
+
+  test("combine all the things", async () => {
+    const action = getInsertUserAction(
+      new Map([
+        ["FirstName", "Jon"],
+        ["LastName", "Snow"],
+      ]),
+    );
+    action.getTriggers = () => [
+      [
+        {
+          changeset: (builder: SimpleBuilder<User>) => {
+            const val = builder.getStoredData("key");
+            const val2 = builder.getStoredData("key2");
+            expect(val).toBeUndefined();
+            expect(val2).toBeUndefined();
+            builder.storeData("key", "priority-1");
+          },
+        },
+      ],
+      [
+        {
+          changeset: (builder: SimpleBuilder<User>) => {
+            const val = builder.getStoredData("key");
+            const val2 = builder.getStoredData("key2");
+            expect(val).toBe("priority-1");
+            expect(val2).toBeUndefined();
+            builder.storeData("key2", "priority-2");
+          },
+        },
+      ],
+      [
+        {
+          changeset: (builder: SimpleBuilder<User>) => {
+            const val = builder.getStoredData("key");
+            const val2 = builder.getStoredData("key2");
+            expect(val).toBe("priority-1");
+            expect(val2).toBe("priority-2");
+            builder.storeData("key3a", "priority-3a");
+          },
+        },
+        {
+          changeset: (builder: SimpleBuilder<User>) => {
+            const val = builder.getStoredData("key");
+            const val2 = builder.getStoredData("key2");
+            expect(val).toBe("priority-1");
+            expect(val2).toBe("priority-2");
+            builder.storeData("key3b", "priority-3b");
+          },
+        },
+      ],
+      {
+        changeset: (builder: SimpleBuilder<User>) => {
+          const val = builder.getStoredData("key");
+          const val2 = builder.getStoredData("key2");
+          const val3a = builder.getStoredData("key3a");
+          const val3b = builder.getStoredData("key3b");
+
+          expect(val).toBe("priority-1");
+          expect(val2).toBe("priority-2");
+          expect(val3a).toBe("priority-3a");
+          expect(val3b).toBe("priority-3b");
+        },
+      },
+    ];
+
+    await action.saveX();
+  });
 }
