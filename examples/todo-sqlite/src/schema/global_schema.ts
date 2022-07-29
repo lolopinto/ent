478544a8d00415350d4fa57e9159b253a0919c7e
Ola Okelola <ola@okelola.com>
2022-07-29 16:00:40 -0700

    .

--- examples/todo-sqlite/src/schema/global_schema.ts
+++ examples/todo-sqlite/src/schema/global_schema.ts
@@ -3,13 +3,5 @@ import { GlobalDeletedEdge } from "@snowtop/ent-soft-delete";
 
 const glo: GlobalSchema = {
   ...GlobalDeletedEdge,
-
-  extraEdgeFields: {
-    // same energy as soft_delete pattern but implemented manually
-    deleted_at: TimestampType({
-      nullable: true,
-      defaultValueOnCreate: () => null,
-    }),
-  },
 };
 export default glo;
