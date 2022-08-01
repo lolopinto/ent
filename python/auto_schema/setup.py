98435a6ea63afa91181f1f86ea66808f8f706016
Ola Okelola <ola@okelola.com>
2022-08-01 12:47:50 -0700

    test pypi

--- python/auto_schema/setup.py
+++ python/auto_schema/setup.py
@@ -6,8 +6,9 @@ with open("README.md", "r") as fh:
 # https://pypi.org/project/auto-schema/#history
 # https://test.pypi.org/project/auto-schema-test/#history
 setuptools.setup(
-    name="auto_schema",  # auto_schema_test to test
-    version="0.0.18",  # 0.0.22 was last test version
+    name="auto_schema_test",  # auto_schema_test to test
+    # 0.0.18 is last production
+    version="0.0.23",  # 0.0.22 was last test version
     author="Ola Okelola",
     author_email="email@email.com",
     description="auto schema for a db",
