dd6ea104f71f6c22966a579b813cae3cf51595f8
Ola Okelola <ola@okelola.com>
2022-08-15 19:18:27 -0400

    .

--- release_image/main.go
+++ release_image/main.go
@@ -33,7 +33,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.19"
-const TSENT_VERSION = "v0.1.0-alpha.31"
+const TSENT_VERSION = "v0.1.0-alpha.32"
 
 var SUFFIXES = []string{
 	"dev",
