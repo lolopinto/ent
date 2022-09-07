f1c7682d077187bdfb2fdde380e17366f275bc9f
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2022-09-07 15:54:59 -0700

    Docker cross compile test (#1106)

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.0.38"
+const TAG = "0.1.0-testtttt"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -32,8 +32,8 @@ var NODE_VERSIONS = []int{
 	18,
 }
 
-const AUTO_SCHEMA_VERSION = "0.0.18"
-const TSENT_VERSION = "v0.0.39"
+const AUTO_SCHEMA_VERSION = "0.0.20"
+const TSENT_VERSION = "v0.1.0-alpha.34"
 
 var SUFFIXES = []string{
 	"dev",
