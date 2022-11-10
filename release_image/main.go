2b3b2ed3927a8414dd3bdd50b05a030f512d3c82
Ola Okelola <ola@okelola.com>
2022-11-10 12:11:19 -0800

    new test release for using rome

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.1.0-alpha.30"
+const TAG = "0.1.0-alpha.31-test"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -33,11 +33,11 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.24"
-const TSENT_VERSION = "v0.1.0-alpha.42"
+const TSENT_VERSION = "v0.1.0-alpha.43-test"
 
 var SUFFIXES = []string{
 	"dev",
-	"slim",
+	// "slim",
 }
 
 // can change platforms here to test locally
