d0c244040c47e53af6b481eb1dd023348f6b989a
Ola Okelola <ola@okelola.com>
2022-12-02 07:53:21 -0800

    redux

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.1.0-alpha.32-alpha1"
+const TAG = "0.1.0-alpha.32-alpha2"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -33,7 +33,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.25"
-const TSENT_VERSION = "v0.1.0-alpha.44-test"
+const TSENT_VERSION = "v0.1.0-alpha.44-test2"
 
 var SUFFIXES = []string{
 	"dev",
