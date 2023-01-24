9ebbaf14649cccfddfac4e9ef498b8b85e759fe5
Ola Okelola <ola@okelola.com>
2023-01-23 21:39:07 -0800

    .

--- release_image/main.go
+++ release_image/main.go
@@ -33,7 +33,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.25"
-const TSENT_VERSION = "v0.1.0-alpha.46-test3"
+const TSENT_VERSION = "v0.1.0-alpha.46-test4"
 
 var SUFFIXES = []string{
 	"dev",
