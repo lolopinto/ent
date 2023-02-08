32395be6f3da6c1c1064f9c8e3aa5ab5512643cd
Ola Okelola <ola@okelola.com>
2023-02-08 15:32:13 -0800

    .

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.1.0-alpha.37"
+const TAG = "0.1.0-alpha.38"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -26,14 +26,14 @@ const REPO = "ghcr.io/lolopinto/ent"
 const UPDATE_LATEST = true
 
 var NODE_VERSIONS = []int{
-	14,
-	16,
+	// 14,
+	// 16,
 	17,
-	18,
+	// 18,
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.26"
-const TSENT_VERSION = "v0.1.0-alpha.49"
+const TSENT_VERSION = "v0.1.0-alpha.50"
 
 var SUFFIXES = []string{
 	"dev",
