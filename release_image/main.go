320a314e147838e4a539fa59fff37b1745e3d6ca
Ola Okelola <ola@okelola.com>
2023-03-14 14:54:15 -0700

    .

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag
-const TAG = "0.1.0-alpha.42-test2"
+const TAG = "0.1.0-alpha.42-test3"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -33,16 +33,16 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.26"
-const TSENT_VERSION = "v0.1.0-alpha.54-test2"
+const TSENT_VERSION = "v0.1.0-alpha.54-test3"
 
 var SUFFIXES = []string{
 	"dev",
-	"slim",
+	// "slim",
 }
 
 // can change platforms here to test locally
 var PLATFORMS = []string{
-	"linux/amd64",
+	// "linux/amd64",
 	"linux/arm64",
 }
 
