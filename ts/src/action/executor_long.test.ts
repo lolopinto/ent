35f383a97ddd074be61f5361313cd0e26b9f4b6f
Ola Okelola <ola@okelola.com>
2023-05-10 15:44:24 -0700

    .

--- ts/src/action/executor_long.test.ts
+++ ts/src/action/executor_long.test.ts
@@ -6,7 +6,7 @@ import { WriteOperation } from "../action/action";
 import { User, Group, SimpleAction, BuilderSchema } from "../testutils/builder";
 import { LoggedOutViewer } from "../core/viewer";
 
-import { setupPostgres, setupSqlite } from "../testutils/db/temp_db";
+import { setupPostgres } from "../testutils/db/temp_db";
 import {
   createGroup,
   createUser,
@@ -18,124 +18,115 @@ import {
 
 setupTest();
 
-jest.setTimeout(10 * 1000);
+jest.setTimeout(30 * 1000);
 
-describe("postgres", () => {
-  setupPostgres(getTables);
-  commonTests();
-});
+// only doing postgres because don't think we get much benefit from both
+setupPostgres(getTables);
 
-describe("sqlite", () => {
-  setupSqlite(`sqlite:///executor-test.db`, getTables);
-  commonTests();
-});
+test("nested + Promise.all with lots of actions", async () => {
+  const NUM_USERS = 500;
+  const host = await createUser();
+  const users: User[] = [];
+  for (let i = 0; i < NUM_USERS; i++) {
+    users.push(await createUser());
+  }
 
-function commonTests() {
-  test("nested + Promise.all with lots of actions", async () => {
-    const NUM_USERS = 500;
-    const host = await createUser();
-    const users: User[] = [];
-    for (let i = 0; i < NUM_USERS; i++) {
-      users.push(await createUser());
+  class SendWelcomeGroupMessageAction extends SimpleAction<Group> {
+    constructor(
+      public viewer: Viewer,
+      schema: BuilderSchema<Group>,
+      fields: Map<string, any>,
+      existingEnt: Group,
+      host: User,
+      users: User[],
+    ) {
+      super(viewer, schema, fields, WriteOperation.Edit, existingEnt);
+      this.builder.storeData("host", host);
+      this.builder.storeData("users", users);
     }
 
-    class SendWelcomeGroupMessageAction extends SimpleAction<Group> {
-      constructor(
-        public viewer: Viewer,
-        schema: BuilderSchema<Group>,
-        fields: Map<string, any>,
-        existingEnt: Group,
-        host: User,
-        users: User[],
-      ) {
-        super(viewer, schema, fields, WriteOperation.Edit, existingEnt);
-        this.builder.storeData("host", host);
-        this.builder.storeData("users", users);
-      }
+    getObservers = () => [
+      {
+        async observe(builder, input) {
+          const host = builder.getStoredData("host") as User;
+          const users = builder.getStoredData("users") as User[];
 
-      getObservers = () => [
-        {
-          async observe(builder, input) {
-            const host = builder.getStoredData("host") as User;
-            const users = builder.getStoredData("users") as User[];
-
-            await Promise.all(
-              users.map(async (user) => {
-                // create welcome message to each user from host
-                const action = new CreateMessageAction(
-                  builder.viewer,
-                  new Map<string, any>([
-                    ["sender", host.id],
-                    ["recipient", user.id],
-                    ["message", "Welcome to the group!"],
-                    ["transient", false],
-                    ["expiresAt", new Date()],
-                  ]),
-                  WriteOperation.Insert,
-                  null,
-                );
-                await action.saveX();
-              }),
-            );
-          },
+          await Promise.all(
+            users.map(async (user) => {
+              // create welcome message to each user from host
+              const action = new CreateMessageAction(
+                builder.viewer,
+                new Map<string, any>([
+                  ["sender", host.id],
+                  ["recipient", user.id],
+                  ["message", "Welcome to the group!"],
+                  ["transient", false],
+                  ["expiresAt", new Date()],
+                ]),
+                WriteOperation.Insert,
+                null,
+              );
+              await action.saveX();
+            }),
+          );
         },
-      ];
-    }
+      },
+    ];
+  }
 
-    class DeliverMessageAction extends MessageAction {
-      getObservers = () => [
-        {
-          async observe(builder, input) {
-            // deliver message to recipient
-            const message = await builder.editedEntX();
+  class DeliverMessageAction extends MessageAction {
+    getObservers = () => [
+      {
+        async observe(builder, input) {
+          // deliver message to recipient
+          const message = await builder.editedEntX();
 
-            const action = new MessageAction(
-              builder.viewer,
-              new Map([["delivered", true]]),
-              WriteOperation.Edit,
-              message,
-            );
-            await action.saveX();
-          },
+          const action = new MessageAction(
+            builder.viewer,
+            new Map([["delivered", true]]),
+            WriteOperation.Edit,
+            message,
+          );
+          await action.saveX();
         },
-      ];
-    }
+      },
+    ];
+  }
 
-    class CreateMessageAction extends MessageAction {
-      getObservers = () => [
-        {
-          async observe(builder, input) {
-            const message = await builder.editedEntX();
-            // deliver the notif
-            const action = new DeliverMessageAction(
-              builder.viewer,
-              new Map(),
-              WriteOperation.Edit,
-              message,
-            );
-            await action.saveX();
-          },
+  class CreateMessageAction extends MessageAction {
+    getObservers = () => [
+      {
+        async observe(builder, input) {
+          const message = await builder.editedEntX();
+          // deliver the notif
+          const action = new DeliverMessageAction(
+            builder.viewer,
+            new Map(),
+            WriteOperation.Edit,
+            message,
+          );
+          await action.saveX();
         },
-      ];
-    }
+      },
+    ];
+  }
 
-    const group = await createGroup();
-    const action = new SendWelcomeGroupMessageAction(
-      new LoggedOutViewer(),
-      GroupSchema,
-      new Map(),
-      group,
-      host,
-      users,
-    );
-    await action.saveX();
+  const group = await createGroup();
+  const action = new SendWelcomeGroupMessageAction(
+    new LoggedOutViewer(),
+    GroupSchema,
+    new Map(),
+    group,
+    host,
+    users,
+  );
+  await action.saveX();
 
-    const messages = await loadRows({
-      clause: clause.Eq("sender", host.id),
-      fields: ["*"],
-      tableName: "messages",
-    });
-    expect(messages.length).toBe(NUM_USERS);
-    expect(messages.every((message) => message.delivered)).toBeTruthy();
+  const messages = await loadRows({
+    clause: clause.Eq("sender", host.id),
+    fields: ["*"],
+    tableName: "messages",
   });
-}
+  expect(messages.length).toBe(NUM_USERS);
+  expect(messages.every((message) => message.delivered)).toBeTruthy();
+});
