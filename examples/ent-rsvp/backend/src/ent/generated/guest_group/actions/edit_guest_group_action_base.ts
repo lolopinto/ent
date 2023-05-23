b41228dcade57d0d2d8ed74d318309342f049f4f
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2023-05-23 08:06:24 -0700

    Update example to get codegen changes (#1439)

--- examples/ent-rsvp/backend/src/ent/generated/guest_group/actions/edit_guest_group_action_base.ts
+++ examples/ent-rsvp/backend/src/ent/generated/guest_group/actions/edit_guest_group_action_base.ts
@@ -9,6 +9,7 @@ import {
 import {
   Action,
   Changeset,
+  ChangesetOptions,
   Observer,
   Trigger,
   Validator,
@@ -107,6 +108,12 @@ export class EditGuestGroupActionBase
 
   async changeset(): Promise<Changeset> {
     return this.builder.build();
+  }
+
+  async changesetWithOptions_BETA(
+    options: ChangesetOptions,
+  ): Promise<Changeset> {
+    return this.builder.buildWithOptions_BETA(options);
   }
 
   async valid(): Promise<boolean> {
