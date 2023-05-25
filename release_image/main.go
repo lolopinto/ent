b6c0524bd282996f17e2836cbcb59810233e2e3b
Ola Okelola <ola@okelola.com>
2023-05-25 09:48:12 -0700

    .

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag to use
-const TAG = "0.1.0-alpha.47-test6"
+const TAG = "0.1.0-alpha.47-test7"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -33,7 +33,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.28"
-const TSENT_VERSION = "v0.1.0-alpha.59-test4"
+const TSENT_VERSION = "v0.1.0-alpha.59-test5"
 
 var SUFFIXES = []string{
 	"dev",
