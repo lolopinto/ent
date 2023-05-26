b6258cb614da4c8077539d5c099fa2cb9491e111
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2023-05-25 17:26:44 -0700

    new release with auto_schema + other changes (#1444)

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag to use
-const TAG = "0.1.0-alpha.46"
+const TAG = "0.1.0-alpha.47"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -32,8 +32,8 @@ var NODE_VERSIONS = []int{
 	18,
 }
 
-const AUTO_SCHEMA_VERSION = "0.0.26"
-const TSENT_VERSION = "v0.1.0-alpha.58"
+const AUTO_SCHEMA_VERSION = "0.0.28"
+const TSENT_VERSION = "v0.1.0-alpha.59"
 
 var SUFFIXES = []string{
 	"dev",
