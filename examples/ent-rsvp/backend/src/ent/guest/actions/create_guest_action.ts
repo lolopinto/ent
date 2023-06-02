73b3f6a6266b752e8219e6b91016d3f0747a359e
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2023-06-02 10:29:01 -0700

    cleanup in example (#1476)

--- examples/ent-rsvp/backend/src/ent/guest/actions/create_guest_action.ts
+++ examples/ent-rsvp/backend/src/ent/guest/actions/create_guest_action.ts
@@ -4,9 +4,6 @@ import {
   CreateGuestActionTriggers,
 } from "src/ent/generated/guest/actions/create_guest_action_base";
 import { AllowIfEventCreatorPrivacyPolicy } from "src/ent/event/privacy/event_creator";
-import { Trigger } from "@snowtop/ent/action";
-import { Ent, Viewer } from "@snowtop/ent";
-import { GuestBuilder } from "../../generated/guest/actions/guest_builder";
 import CreateAuthCodeAction from "src/ent/auth_code/actions/create_auth_code_action";
 
 export { GuestCreateInput };
