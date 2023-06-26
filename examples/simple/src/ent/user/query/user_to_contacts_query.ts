6d98855b7fda0fdd446e0b062816dc1ebb9c981c
Ola Okelola <ola@okelola.com>
2023-06-26 10:23:45 -0700

    revert

--- examples/simple/src/ent/user/query/user_to_contacts_query.ts
+++ examples/simple/src/ent/user/query/user_to_contacts_query.ts
@@ -2,6 +2,12 @@
  * Copyright whaa whaa
  */
 
-import { UserToContactsQueryBase } from "../../internal";
+import { ExampleViewer } from "src/viewer/viewer";
+import { User, UserToContactsQueryBase } from "../../internal";
+import { ID } from "@snowtop/ent";
 
-export class UserToContactsQuery extends UserToContactsQueryBase {}
+export class UserToContactsQuery extends UserToContactsQueryBase {
+  constructor(viewer: ExampleViewer, user: User | ID) {
+    super(viewer, user, "created_at");
+  }
+}
