9d2107cab1fa9e3deb2236608e69858833fbaa76
Ola Okelola <ola@okelola.com>
2023-07-07 11:58:03 -0700

    .

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 // next tag to use
-const TAG = "0.1.0-alpha.50-test2"
+const TAG = "0.1.0-alpha.50-test3"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -33,7 +33,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.28"
-const TSENT_VERSION = "v0.1.0-alpha.62-test2"
+const TSENT_VERSION = "v0.1.0-alpha.62-test3"
 
 var SUFFIXES = []string{
 	"dev",
@@ -42,8 +42,8 @@ var SUFFIXES = []string{
 
 // can change platforms here to test locally
 var PLATFORMS = []string{
-	"linux/amd64",
-	// "linux/arm64",
+	// "linux/amd64",
+	"linux/arm64",
 }
 
 func main() {
