eaf8c30b537bc5744c10d1bc74e5d9ace6bb6e19
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2023-07-12 14:38:15 -0700

    new (previous) release (#1521)

--- release_image/main.go
+++ release_image/main.go
@@ -17,7 +17,8 @@ import (
 )
 
 // next tag to use
-const TAG = "0.1.0-alpha.49"
+
+const TAG = "0.1.0-alpha.50"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -32,7 +33,7 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.28"
-const TSENT_VERSION = "v0.1.0-alpha.61"
+const TSENT_VERSION = "v0.1.0-alpha.62"
 
 var SUFFIXES = []string{
 	"dev",
