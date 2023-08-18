3b0eb491f4ce2358a58cb69b6d0838b508a6a8c1
Ola Okelola <ola@okelola.com>
2023-08-18 16:54:17 -0700

    new version. flipped

--- release_image/main.go
+++ release_image/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 // next tag to use
-const TAG = "v0.1.6-test1"
+const TAG = "v0.1.6-test2"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -32,7 +32,7 @@ var NODE_VERSIONS = []int{
 	// 20,
 }
 
-const AUTO_SCHEMA_VERSION = "0.0.25"
+const AUTO_SCHEMA_VERSION = "0.0.26"
 const TSENT_VERSION = "v0.1.6-test1"
 
 var SUFFIXES = []string{
