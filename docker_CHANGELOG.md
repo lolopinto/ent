f84797bf18c7c3a45da282fb65cf6dfa5772f909
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2023-10-09 16:19:30 -0700

    new release (#1679)

---
+++ docker_CHANGELOG.md
@@ -0,0 +1,16 @@
+# CHANGELOG
+
+All notable changes to the docker image will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+Changelog for the npm version are [here](/CHANGELOG.md).
+
+## [Unreleased]
+
+## [0.1.10] - 2023-10-08
+
+### Fixed
+
+- Fix types.ts referencing itself in imports (#1678)
--- release_image/main.go
+++ release_image/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 // next tag to use
-const TAG = "v0.1.9"
+const TAG = "v0.1.10"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -33,14 +33,14 @@ var NODE_VERSIONS = []int{
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.32"
-const TSENT_VERSION = "v0.1.8"
+const TSENT_VERSION = "v0.1.9"
 
 var SUFFIXES = []string{
 	"dev",
 	"slim",
 }
 
-// can change platforms here to test locally
+// you can change platforms here to test locally
 var PLATFORMS = []string{
 	"linux/amd64",
 	"linux/arm64",
