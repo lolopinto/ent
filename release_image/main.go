67d3ab6dd69902affa29efcba416e1cb340dd978
Ola Okelola <ola@okelola.com>
2023-12-04 23:02:35 -0800

    [test] new image

--- release_image/main.go
+++ release_image/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 // next tag to use
-const TAG = "v0.1.14"
+const TAG = "v0.1.15-test1"
 
 // current node gets latest tag...
 const CURRENT_NODE_VERSION = 18
@@ -29,7 +29,7 @@ const UPDATE_LATEST = true
 var NODE_VERSIONS = []int{
 	// 16,
 	18,
-	20,
+	// 20,
 }
 
 const AUTO_SCHEMA_VERSION = "0.0.32"
@@ -37,13 +37,13 @@ const TSENT_VERSION = "v0.1.13"
 
 var SUFFIXES = []string{
 	"dev",
-	"slim",
+	// "slim",
 }
 
 // you can change platforms here to test locally
 var PLATFORMS = []string{
 	"linux/amd64",
-	"linux/arm64",
+	// "linux/arm64",
 }
 
 func do(version int) error {
