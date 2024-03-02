lolopinto/ent

commit 7850360c31a58d5e732cf86d527e9c2767bda03f

Add support for @gqlField with gqlConnection and args (#1768)

examples/todo-sqlite/src/graphql/generated/schema.gql
@@ -64,6 +64,7 @@ type Account implements Node {
   can_viewer_see_info: AccountCanViewerSee!
   open_todos_plural: [Todo!]!
   open_todos(first: Int, after: String, last: Int, before: String): AccountToOpenTodosConnection!
+  custom_todos(completed: Boolean, completed_date: Date, first: Int, after: String, last: Int, before: String): AccountToCustomTodosConnection!
 }
 
 type CountryCapital {
@@ -97,7 +98,7 @@ type Todo implements Node {
   id: ID!
   text: String!
   completed: Boolean!
-  completed_date: Time
+  completed_date: Date
   bounty: Int
   tags(first: Int, after: String, last: Int, before: String): TodoToTagsConnection!
   todo_scope(first: Int, after: String, last: Int, before: String): TodoToTodoScopeConnection!
@@ -133,6 +134,18 @@ type AccountToCreatedWorkspacesEdge implements Edge {
 type AccountToCreatedWorkspacesConnection implements Connection {
   edges: [AccountToCreatedWorkspacesEdge!]!
   nodes: [Workspace!]!
+  pageInfo: PageInfo!
+  rawCount: Int!
+}
+
+type AccountToCustomTodosEdge implements Edge {
+  node: Todo!
+  cursor: String!
+}
+
+type AccountToCustomTodosConnection implements Connection {
+  edges: [AccountToCustomTodosEdge!]!
+  nodes: [Todo!]!
   pageInfo: PageInfo!
   rawCount: Int!
 }
@@ -228,6 +241,18 @@ type RootToClosedTodosLastDayEdge implements Edge {
 
 type RootToClosedTodosLastDayConnection implements Connection {
   edges: [RootToClosedTodosLastDayEdge!]!
+  nodes: [Todo!]!
+  pageInfo: PageInfo!
+  rawCount: Int!
+}
+
+type RootToCustomTodosEdge implements Edge {
+  node: Todo!
+  cursor: String!
+}
+
+type RootToCustomTodosConnection implements Connection {
+  edges: [RootToCustomTodosEdge!]!
   nodes: [Todo!]!
   pageInfo: PageInfo!
   rawCount: Int!
@@ -525,6 +550,7 @@ type TodoStatusAccountEditPayload {
 type Query {
   account(id: ID!): Account
   closed_todos_last_day(first: Int, after: String, last: Int, before: String): RootToClosedTodosLastDayConnection!
+  custom_todos(completed: Boolean, completed_date: Date, first: Int, after: String, last: Int, before: String): RootToCustomTodosConnection!
   open_todos(id: ID!, first: Int, after: String, last: Int, before: String): RootToOpenTodosConnection!
   open_todos_plural(id: ID!): [Todo!]!
   tag(id: ID!): Tag
@@ -553,7 +579,4 @@ type Mutation {
   renameTodo(input: RenameTodoInput!): RenameTodoPayload!
   todoStatusAccountEdit(input: TodoStatusAccountEditInput!): TodoStatusAccountEditPayload!
 }
-
-"""Time scalar type"""
-scalar Time
 

internal/graphql/generate_ts_code.go
@@ -47,8 +47,8 @@ func (p *TSStep) Name() string {
 }
 
 var knownTypes = map[string]*tsimport.ImportPath{
-	"String": tsimport.NewGQLImportPath("GraphQLString"),
-	// "Date":       tsimport.NewEntGraphQLImportPath("GraphQLTime"),
+	"String":     tsimport.NewGQLImportPath("GraphQLString"),
+	"Date":       tsimport.NewEntGraphQLImportPath("GraphQLTime"),
 	"Int":        tsimport.NewGQLImportPath("GraphQLInt"),
 	"Float":      tsimport.NewGQLImportPath("GraphQLFloat"),
 	"Boolean":    tsimport.NewGQLImportPath("GraphQLBoolean"),
@@ -2349,6 +2349,7 @@ func addConnection(processor *codegen.Processor, nodeData *schema.NodeData, edge
 	}
 
 	var buildQuery string
+	var args []CustomItem
 	if customField == nil {
 		// for custom fields, EntQuery is an implementation detail
 		// and may or may not be exposed so we don't depend on it here
@@ -2357,8 +2358,16 @@ func addConnection(processor *codegen.Processor, nodeData *schema.NodeData, edge
 			Import:     edge.TsEdgeQueryName(),
 		})
 		buildQuery = fmt.Sprintf("%s.query(v, obj)", edge.TsEdgeQueryName())
+		args = getConnectionArgs()
 	} else {
-		buildQuery = fmt.Sprintf("%s.%s()", "obj", customField.FunctionName)
+		args = customField.Args
+		nonConnectionArgs := customField.getNonConnectionArgs()
+		var params []string
+		for _, arg := range nonConnectionArgs {
+			params = append(params, fmt.Sprintf("args.%s", arg.Name))
+
+		}
+		buildQuery = fmt.Sprintf("%s.%s(%s)", "obj", customField.FunctionName, strings.Join(params, ", "))
 	}
 
 	gqlField := &fieldType{
@@ -2366,7 +2375,7 @@ func addConnection(processor *codegen.Processor, nodeData *schema.NodeData, edge
 		HasResolveFunction: true,
 		FieldImports:       getGQLFileImports(edge.GetTSGraphQLTypeImports(), false),
 		ExtraImports:       extraImports,
-		Args:               getFieldConfigArgsFrom(processor, getConnectionArgs(), nil, false),
+		Args:               getFieldConfigArgsFrom(processor, args, nil, false),
 		// TODO typing for args later?
 		FunctionContents: []string{
 			fmt.Sprintf(

examples/todo-sqlite/src/ent/account.ts
@@ -1,6 +1,8 @@
+import { Interval } from "luxon";
 import {
   AlwaysAllowPrivacyPolicy,
   CustomEdgeQueryBase,
+  CustomClauseQuery,
   ID,
   query,
   Viewer,
@@ -36,6 +38,39 @@ export class Account extends AccountBase {
   })
   openTodos() {
     return new AccountToOpenTodosQuery(this.viewer, this);
+  }
+
+  // queryable connection
+  @gqlField({
+    class: "Account",
+    name: "custom_todos",
+    type: gqlConnection("Todo"),
+    args: [
+      {
+        name: "completed",
+        type: "Boolean",
+        nullable: true,
+      },
+      {
+        name: "completed_date",
+        type: "Date",
+        nullable: true,
+      },
+    ],
+  })
+  customTodos(completed: boolean | null, completedDate: Date | null) {
+    const start = Interval.before(new Date(), { hours: 24 })
+      .start.toUTC()
+      .toISO();
+
+    return new CustomClauseQuery(this.viewer, {
+      loadEntOptions: Todo.loaderOptions(),
+      clause: query.And(
+        query.Eq("completed", completed ?? true),
+        query.GreaterEq("completed_date", completedDate ?? start),
+      ),
+      name: "todos",
+    });
   }
 
   getDeletedAt() {

examples/todo-sqlite/src/graphql/generated/resolvers/account/account_to_custom_todos_connection_type.ts
@@ -1,0 +1,15 @@
+// Generated by github.com/lolopinto/ent/ent, DO NOT EDIT.
+
+import { GraphQLObjectType } from "graphql";
+import { Data, Viewer } from "@snowtop/ent";
+import { GraphQLConnectionType } from "@snowtop/ent/graphql";
+import { TodoType } from "src/graphql/resolvers/internal";
+
+var connType: GraphQLConnectionType<GraphQLObjectType, Data, Viewer>;
+
+export const AccountToCustomTodosConnectionType = () => {
+  if (connType === undefined) {
+    connType = new GraphQLConnectionType("AccountToCustomTodos", TodoType);
+  }
+  return connType;
+};

examples/todo-sqlite/src/graphql/generated/resolvers/account_type.ts
@@ -14,6 +14,7 @@ import { RequestContext, Viewer } from "@snowtop/ent";
 import {
   GraphQLEdgeConnection,
   GraphQLNodeInterface,
+  GraphQLTime,
 } from "@snowtop/ent/graphql";
 import {
   Account,
@@ -32,6 +33,7 @@ import {
   AccountPrefsType,
   AccountToClosedTodosDupConnectionType,
   AccountToCreatedWorkspacesConnectionType,
+  AccountToCustomTodosConnectionType,
   AccountToOpenTodosConnectionType,
   AccountToOpenTodosDupConnectionType,
   AccountToScopedTodosConnectionType,
@@ -376,6 +378,44 @@ export const AccountType = new GraphQLObjectType({
           obj.viewer,
           obj,
           (v, obj: Account) => obj.openTodos(),
+          args,
+        );
+      },
+    },
+    custom_todos: {
+      type: new GraphQLNonNull(AccountToCustomTodosConnectionType()),
+      args: {
+        completed: {
+          description: "",
+          type: GraphQLBoolean,
+        },
+        completed_date: {
+          description: "",
+          type: GraphQLTime,
+        },
+        first: {
+          description: "",
+          type: GraphQLInt,
+        },
+        after: {
+          description: "",
+          type: GraphQLString,
+        },
+        last: {
+          description: "",
+          type: GraphQLInt,
+        },
+        before: {
+          description: "",
+          type: GraphQLString,
+        },
+      },
+      resolve: (obj: Account, args: any, context: RequestContext<Viewer>) => {
+        return new GraphQLEdgeConnection(
+          obj.viewer,
+          obj,
+          (v, obj: Account) =>
+            obj.customTodos(args.completed, args.completed_date),
           args,
         );
       },

examples/todo-sqlite/src/graphql/generated/resolvers/custom_todos_query_type.ts
@@ -1,0 +1,70 @@
+// Generated by github.com/lolopinto/ent/ent, DO NOT EDIT.
+
+import {
+  GraphQLBoolean,
+  GraphQLFieldConfig,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLResolveInfo,
+  GraphQLString,
+} from "graphql";
+import { RequestContext, Viewer } from "@snowtop/ent";
+import { GraphQLEdgeConnection, GraphQLTime } from "@snowtop/ent/graphql";
+import { RootToCustomTodosConnectionType } from "src/graphql/resolvers/internal";
+import { TodoResolver } from "../../resolvers/todos_resolver";
+
+interface CustomTodosArgs {
+  completed: boolean | null;
+  completed_date: Date | null;
+  first: number | null;
+  after: string | null;
+  last: number | null;
+  before: string | null;
+}
+
+export const CustomTodosQueryType: GraphQLFieldConfig<
+  undefined,
+  RequestContext<Viewer>,
+  CustomTodosArgs
+> = {
+  type: new GraphQLNonNull(RootToCustomTodosConnectionType()),
+  args: {
+    completed: {
+      description: "",
+      type: GraphQLBoolean,
+    },
+    completed_date: {
+      description: "",
+      type: GraphQLTime,
+    },
+    first: {
+      description: "",
+      type: GraphQLInt,
+    },
+    after: {
+      description: "",
+      type: GraphQLString,
+    },
+    last: {
+      description: "",
+      type: GraphQLInt,
+    },
+    before: {
+      description: "",
+      type: GraphQLString,
+    },
+  },
+  resolve: async (
+    _source,
+    args,
+    context: RequestContext<Viewer>,
+    _info: GraphQLResolveInfo,
+  ) => {
+    const r = new TodoResolver();
+    return new GraphQLEdgeConnection(
+      context.getViewer(),
+      (v) => r.customTodos(context, args.completed, args.completed_date),
+      args,
+    );
+  },
+};

examples/todo-sqlite/src/graphql/generated/resolvers/query_type.ts
@@ -4,6 +4,7 @@ import { GraphQLObjectType } from "graphql";
 import {
   AccountQueryType,
   ClosedTodosLastDayQueryType,
+  CustomTodosQueryType,
   OpenTodosPluralQueryType,
   OpenTodosQueryType,
   TagQueryType,
@@ -16,6 +17,7 @@ export const QueryType = new GraphQLObjectType({
   fields: () => ({
     account: AccountQueryType,
     closed_todos_last_day: ClosedTodosLastDayQueryType,
+    custom_todos: CustomTodosQueryType,
     open_todos: OpenTodosQueryType,
     open_todos_plural: OpenTodosPluralQueryType,
     tag: TagQueryType,

examples/todo-sqlite/src/graphql/generated/resolvers/root/root_to_custom_todos_connection_type.ts
@@ -1,0 +1,15 @@
+// Generated by github.com/lolopinto/ent/ent, DO NOT EDIT.
+
+import { GraphQLObjectType } from "graphql";
+import { Data, Viewer } from "@snowtop/ent";
+import { GraphQLConnectionType } from "@snowtop/ent/graphql";
+import { TodoType } from "src/graphql/resolvers/internal";
+
+var connType: GraphQLConnectionType<GraphQLObjectType, Data, Viewer>;
+
+export const RootToCustomTodosConnectionType = () => {
+  if (connType === undefined) {
+    connType = new GraphQLConnectionType("RootToCustomTodos", TodoType);
+  }
+  return connType;
+};

commit 57ce86778b53f499a2cee24ae2c89874375517e3

fix generated date types
broken by https://github.com/lolopinto/ent/pull/1768

what seemed to need it was fixed by https://github.com/lolopinto/ent/pull/1780

examples/ent-rsvp/backend/src/graphql/generated/schema.gql
@@ -60,8 +60,8 @@ type EventActivity implements Node {
   event: Event
   id: ID!
   name: String!
-  startTime: Date!
-  endTime: Date
+  startTime: Time!
+  endTime: Time
   location: String!
   description: String
   inviteAllGuests: Boolean!
@@ -288,8 +288,8 @@ type AuthUserPayload {
 input ActivityEventCreateInput {
   addressId: ID
   name: String!
-  startTime: Date!
-  endTime: Date
+  startTime: Time!
+  endTime: Time
   location: String!
   description: String
   inviteAllGuests: Boolean!
@@ -357,8 +357,8 @@ input EventActivityCreateInput {
   addressId: ID
   name: String!
   eventId: ID!
-  startTime: Date!
-  endTime: Date
+  startTime: Time!
+  endTime: Time
   location: String!
   description: String
   inviteAllGuests: Boolean
@@ -384,8 +384,8 @@ input EventActivityEditInput {
   addressId: ID
   name: String
   eventId: ID
-  startTime: Date
-  endTime: Date
+  startTime: Time
+  endTime: Time
   location: String
   description: String
   inviteAllGuests: Boolean
@@ -549,6 +549,9 @@ type Mutation {
   importGuests(eventId: ID!, file: Upload!): Event!
   userCreate(input: UserCreateInput!): UserCreatePayload!
 }
+
+"""Time scalar type"""
+scalar Time
 
 """The `Upload` scalar type represents a file upload."""
 scalar Upload

examples/simple/src/graphql/generated/schema.gql
@@ -232,14 +232,14 @@ type Attachment {
   fileId: ID!
   dupeFileId: ID
   note: String
-  date: Date!
+  date: Time!
   phoneNumber: String
   emailAddress: String
 }
 
 type CatType {
   name: String!
-  birthday: Date!
+  birthday: Time!
   breed: CatBreed!
   kitten: Boolean!
 }
@@ -263,7 +263,7 @@ type Comment implements Node {
 type ContactDate implements ContactItemType {
   contact: Contact
   label: ContactLabel!
-  date: Date!
+  date: Time!
   description: String!
 }
 
@@ -328,7 +328,7 @@ type Contact implements Node {
 
 type DogType {
   name: String!
-  birthday: Date!
+  birthday: Time!
   breed: DogBreed!
   breedGroup: DogBreedGroup!
   puppy: Boolean!
@@ -348,8 +348,8 @@ type Event implements Node {
   creator: User
   id: ID!
   name: String!
-  startTime: Date!
-  endTime: Date
+  startTime: Time!
+  endTime: Time
   eventLocation: String!
   coverPhoto: Byte
   coverPhoto2: Byte
@@ -381,7 +381,7 @@ type Holiday implements Node {
   dayOfWeek: DayOfWeek!
   dayOfWeekAlt: DayOfWeekAlt!
   label: String!
-  date: Date!
+  date: Time!
 }
 
 type HoursOfOperation implements Node {
@@ -396,7 +396,7 @@ union PetUnionType = CatType | DogType | RabbitType
 
 type RabbitType {
   name: String!
-  birthday: Date!
+  birthday: Time!
   breed: RabbitBreed!
 }
 
@@ -1012,7 +1012,7 @@ type UserToLikersConnection implements Connection {
 type UserToLikesEdge implements Edge {
   node: Node!
   cursor: String!
-  time: Date!
+  time: Time!
 }
 
 type UserToLikesConnection implements Connection {
@@ -1079,14 +1079,14 @@ input AttachmentInput {
   fileId: ID!
   dupeFileId: ID
   note: String
-  date: Date!
+  date: Time!
   phoneNumber: String
   emailAddress: String
 }
 
 input CatTypeInput {
   name: String!
-  birthday: Date!
+  birthday: Time!
   breed: CatBreed!
   kitten: Boolean!
 }
@@ -1286,7 +1286,7 @@ input CustomEditHolidayInput {
   dayOfWeek: DayOfWeek
   dayOfWeekAlt: DayOfWeekAlt
   label: String
-  date: Date
+  date: Time
 }
 
 type CustomEditHolidayPayload {
@@ -1305,7 +1305,7 @@ input DeleteUserInput2 {
 
 input DogTypeInput {
   name: String!
-  birthday: Date!
+  birthday: Time!
   breed: DogBreed!
   breedGroup: DogBreedGroup!
   puppy: Boolean!
@@ -1362,8 +1362,8 @@ input EventArgInput {
 input EventCreateInput {
   name: String!
   creatorId: ID!
-  startTime: Date!
-  endTime: Date
+  startTime: Time!
+  endTime: Time
   eventLocation: String!
   addressId: ID
   coverPhoto: Byte
@@ -1388,8 +1388,8 @@ input EventEditInput {
   """id of Event"""
   id: ID!
   name: String
-  startTime: Date
-  endTime: Date
+  startTime: Time
+  endTime: Time
   eventLocation: String
   addressId: ID
   coverPhoto: Byte
@@ -1474,7 +1474,7 @@ input HolidayCreateInput {
   dayOfWeek: DayOfWeek!
   dayOfWeekAlt: DayOfWeekAlt!
   label: String!
-  date: Date
+  date: Time
   log: JSON
 }
 
@@ -1512,7 +1512,7 @@ input PhoneNumberContactCreateInput {
 
 input RabbitTypeInput {
   name: String!
-  birthday: Date!
+  birthday: Time!
   breed: RabbitBreed!
 }
 
@@ -1689,7 +1689,7 @@ type Query {
   """custom query for hours_of_operation. list"""
   hours_of_operation_list_deprecated(id: ID, ids: [ID!], extra: Boolean): [HoursOfOperation!]!
   node(id: ID!): Node
-  timeDiff(time: Date!, log: JSON!): String!
+  timeDiff(time: Time!, log: JSON!): String!
   """custom query for user. connection"""
   user_connection(ids: [ID!], sortCol: UserSortColumn, query: UserArgInput, first: Int, after: String, last: Int, before: String): RootToUserConnection!
   """custom query for user. list"""
@@ -1751,6 +1751,9 @@ scalar Byte
 
 """The `JSON` scalar type represents JSON values as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf)."""
 scalar JSON
+
+"""Time scalar type"""
+scalar Time
 
 """The `Upload` scalar type represents a file upload."""
 scalar Upload

examples/todo-sqlite/src/graphql/generated/schema.gql
@@ -64,7 +64,7 @@ type Account implements Node {
   can_viewer_see_info: AccountCanViewerSee!
   open_todos_plural: [Todo!]!
   open_todos(first: Int, after: String, last: Int, before: String): AccountToOpenTodosConnection!
-  custom_todos(completed: Boolean, completed_date: Date, first: Int, after: String, last: Int, before: String): AccountToCustomTodosConnection!
+  custom_todos(completed: Boolean, completed_date: Time, first: Int, after: String, last: Int, before: String): AccountToCustomTodosConnection!
 }
 
 type CountryCapital {
@@ -98,7 +98,7 @@ type Todo implements Node {
   id: ID!
   text: String!
   completed: Boolean!
-  completed_date: Date
+  completed_date: Time
   bounty: Int
   tags(first: Int, after: String, last: Int, before: String): TodoToTagsConnection!
   todo_scope(first: Int, after: String, last: Int, before: String): TodoToTodoScopeConnection!
@@ -550,7 +550,7 @@ type TodoStatusAccountEditPayload {
 type Query {
   account(id: ID!): Account
   closed_todos_last_day(first: Int, after: String, last: Int, before: String): RootToClosedTodosLastDayConnection!
-  custom_todos(completed: Boolean, completed_date: Date, first: Int, after: String, last: Int, before: String): RootToCustomTodosConnection!
+  custom_todos(completed: Boolean, completed_date: Time, first: Int, after: String, last: Int, before: String): RootToCustomTodosConnection!
   open_todos(id: ID!, first: Int, after: String, last: Int, before: String): RootToOpenTodosConnection!
   open_todos_plural(id: ID!): [Todo!]!
   tag(id: ID!): Tag
@@ -579,4 +579,7 @@ type Mutation {
   renameTodo(input: RenameTodoInput!): RenameTodoPayload!
   todoStatusAccountEdit(input: TodoStatusAccountEditInput!): TodoStatusAccountEditPayload!
 }
+
+"""Time scalar type"""
+scalar Time
 

internal/graphql/generate_ts_code.go
@@ -46,9 +46,13 @@ func (p *TSStep) Name() string {
 	return "graphql"
 }
 
+// note Date here is intentionally commented out
+// adding it to knownTypes kills the scalar Time and changes all
+// GraphQL Time to Date
+// TODO figure out exactly what's happening here...
 var knownTypes = map[string]*tsimport.ImportPath{
-	"String":     tsimport.NewGQLImportPath("GraphQLString"),
-	"Date":       tsimport.NewEntGraphQLImportPath("GraphQLTime"),
+	"String": tsimport.NewGQLImportPath("GraphQLString"),
+	// "Date":       tsimport.NewEntGraphQLImportPath("GraphQLTime"),
 	"Int":        tsimport.NewGQLImportPath("GraphQLInt"),
 	"Float":      tsimport.NewGQLImportPath("GraphQLFloat"),
 	"Boolean":    tsimport.NewGQLImportPath("GraphQLBoolean"),