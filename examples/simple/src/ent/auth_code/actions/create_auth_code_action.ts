lolopinto/ent

commit dfe1edbd98a7751b2cc946556ee703f1617f6c7b

example showing triggers and observers working in transformWrite (#1786)

examples/simple/src/ent/auth_code/actions/create_auth_code_action.ts
@@ -1,16 +1,20 @@
 import {
   CreateAuthCodeActionBase,
   AuthCodeCreateInput,
+  CreateAuthCodeActionTriggers,
+  CreateAuthCodeActionObservers,
 } from "../../generated/auth_code/actions/create_auth_code_action_base";
 import { AuthCodeBuilder } from "../../generated/auth_code/actions/auth_code_builder";
 
 export { AuthCodeCreateInput };
 import { ExampleViewer } from "../../../viewer/viewer";
-import { AuthCode } from "../../";
+import { AuthCode, UserStatistics } from "../../";
 import { Validator } from "@snowtop/ent/action";
 import { Data, UpdateOperation, query } from "@snowtop/ent";
 import { TransformedUpdateOperation } from "@snowtop/ent";
 import { SQLStatementOperation } from "@snowtop/ent";
+import { FakeComms, Mode } from "@snowtop/ent/testutils/fake_comms";
+import EditUserStatisticsAction from "src/ent/user_statistics/actions/edit_user_statistics_action";
 
 // we're only writing this once except with --force and packageName provided
 export default class CreateAuthCodeAction extends CreateAuthCodeActionBase {
@@ -81,5 +85,51 @@ export default class CreateAuthCodeAction extends CreateAuthCodeActionBase {
       existingEnt: first,
       data,
     };
+  }
+
+  getTriggers(): CreateAuthCodeActionTriggers {
+    return [
+      {
+        async changeset(builder, input) {
+          const stats = await UserStatistics.loadFromUserIdX(
+            builder.viewer,
+            input.userId,
+          );
+          return EditUserStatisticsAction.create(builder.viewer, stats, {
+            authCodeEmailsSent: {
+              add: 1,
+            },
+          }).changeset();
+        },
+      },
+    ];
+  }
+
+  getObservers(): CreateAuthCodeActionObservers {
+    return [
+      {
+        observe(builder, input) {
+          if (input.emailAddress) {
+            // send email
+            FakeComms.send({
+              to: input.emailAddress,
+              mode: Mode.EMAIL,
+              from: "noreply@foo.com",
+              subject: "auth code",
+              body: `your auth code is ${input.code}`,
+            });
+          }
+          if (input.phoneNumber) {
+            // send sms
+            FakeComms.send({
+              to: input.phoneNumber,
+              mode: Mode.SMS,
+              from: "42423",
+              body: `your auth code is ${input.code}`,
+            });
+          }
+        },
+      },
+    ];
   }
 }

examples/simple/src/ent/generated/auth_code/actions/create_auth_code_action_base.ts
@@ -10,7 +10,6 @@ import {
 } from "@snowtop/ent";
 import {
   Action,
-  Builder,
   Changeset,
   ChangesetOptions,
   Observer,
@@ -18,13 +17,13 @@ import {
   Validator,
   WriteOperation,
 } from "@snowtop/ent/action";
-import { AuthCode, User } from "../../..";
+import { AuthCode } from "../../..";
 import { AuthCodeBuilder } from "./auth_code_builder";
 import { ExampleViewer as ExampleViewerAlias } from "../../../../viewer/viewer";
 
 export interface AuthCodeCreateInput {
   code: string;
-  userId: ID | Builder<User, ExampleViewerAlias>;
+  userId: ID;
   emailAddress?: string | null;
   phoneNumber?: string | null;
 }

examples/simple/src/schema/auth_code_schema.ts
@@ -13,7 +13,7 @@ const AuthCodeSchema = new EntSchema({
     code: StringType(),
     userID: UUIDType({
       immutable: true,
-      foreignKey: { schema: "User", column: "id" },
+      foreignKey: { schema: "User", column: "id", disableBuilderType: true },
     }),
     emailAddress: EmailType({ nullable: true }),
     phoneNumber: PhoneNumberType({ nullable: true }),

examples/simple/src/ent/generated/loadAny.ts
@@ -24,6 +24,7 @@ import {
   Holiday,
   HoursOfOperation,
   User,
+  UserStatistics,
 } from "../internal";
 
 export async function loadEntByType(
@@ -66,6 +67,8 @@ export function getLoaderOptions(type: NodeType): LoadEntOptions<Ent, any> {
       return HoursOfOperation.loaderOptions();
     case NodeType.User:
       return User.loaderOptions();
+    case NodeType.UserStatistics:
+      return UserStatistics.loaderOptions();
     default:
       throw new Error(`invalid nodeType ${type} passed to getLoaderOptions`);
   }

examples/simple/src/ent/generated/loaders.ts
@@ -907,6 +907,69 @@ userEmailAddressLoader.addToPrime(userPhoneNumberLoader);
 userPhoneNumberLoader.addToPrime(userLoader);
 userPhoneNumberLoader.addToPrime(userEmailAddressLoader);
 
+export interface UserStatisticsDBData {
+  id: ID;
+  created_at: Date;
+  updated_at: Date;
+  user_id: ID;
+  auth_code_emails_sent: number;
+}
+
+const userStatisticsTable = "user_statistics";
+const userStatisticsFields = [
+  "id",
+  "created_at",
+  "updated_at",
+  "user_id",
+  "auth_code_emails_sent",
+];
+
+export const userStatisticsLoader =
+  new ObjectLoaderFactory<UserStatisticsDBData>({
+    tableName: userStatisticsTable,
+    fields: userStatisticsFields,
+    key: "id",
+  });
+
+export const userStatisticsUserIdLoader =
+  new ObjectLoaderFactory<UserStatisticsDBData>({
+    tableName: userStatisticsTable,
+    fields: userStatisticsFields,
+    key: "user_id",
+  });
+
+export const userStatisticsLoaderInfo = {
+  tableName: userStatisticsTable,
+  fields: userStatisticsFields,
+  nodeType: NodeType.UserStatistics,
+  loaderFactory: userStatisticsLoader,
+  fieldInfo: {
+    id: {
+      dbCol: "id",
+      inputKey: "id",
+    },
+    createdAt: {
+      dbCol: "created_at",
+      inputKey: "createdAt",
+    },
+    updatedAt: {
+      dbCol: "updated_at",
+      inputKey: "updatedAt",
+    },
+    userID: {
+      dbCol: "user_id",
+      inputKey: "userId",
+    },
+    authCodeEmailsSent: {
+      dbCol: "auth_code_emails_sent",
+      inputKey: "authCodeEmailsSent",
+    },
+  },
+};
+
+userStatisticsLoader.addToPrime(userStatisticsUserIdLoader);
+userStatisticsUserIdLoader.addToPrime(userStatisticsLoader);
+
 export function getLoaderInfoFromSchema(schema: string) {
   switch (schema) {
     case "Address":
@@ -931,6 +994,8 @@ export function getLoaderInfoFromSchema(schema: string) {
       return hoursOfOperationLoaderInfo;
     case "User":
       return userLoaderInfo;
+    case "UserStatistics":
+      return userStatisticsLoaderInfo;
     default:
       throw new Error(
         `invalid schema ${schema} passed to getLoaderInfoFromSchema`,
@@ -962,6 +1027,8 @@ export function getLoaderInfoFromNodeType(nodeType: NodeType) {
       return hoursOfOperationLoaderInfo;
     case NodeType.User:
       return userLoaderInfo;
+    case NodeType.UserStatistics:
+      return userStatisticsLoaderInfo;
     default:
       throw new Error(
         `invalid nodeType ${nodeType} passed to getLoaderInfoFromNodeType`,

examples/simple/src/ent/generated/types.ts
@@ -31,6 +31,8 @@ export enum NodeType {
   HoursOfOperation = "hoursOfOperation",
   // User is the node type for the User object. Used to identify this node in edges and other places.
   User = "user",
+  // UserStatistics is the node type for the UserStatistics object. Used to identify this node in edges and other places.
+  UserStatistics = "userStatistics",
 }
 
 export enum EdgeType {

examples/simple/src/ent/generated/user_statistics/actions/create_user_statistics_action_base.ts
@@ -1,0 +1,153 @@
+/**
+ * Copyright whaa whaa
+ * Generated by github.com/lolopinto/ent/ent, DO NOT EDIT.
+ */
+
+import { ID, PrivacyPolicy } from "@snowtop/ent";
+import {
+  Action,
+  Changeset,
+  ChangesetOptions,
+  Observer,
+  Trigger,
+  Validator,
+  WriteOperation,
+} from "@snowtop/ent/action";
+import { UserStatistics } from "../../..";
+import { UserStatisticsBuilder } from "./user_statistics_builder";
+import schema from "../../../../schema/user_statistics_schema";
+import { ExampleViewer as ExampleViewerAlias } from "../../../../viewer/viewer";
+
+export interface UserStatisticsCreateInput {
+  userId: ID;
+  authCodeEmailsSent?: number;
+}
+
+export type CreateUserStatisticsActionTriggers = (
+  | Trigger<
+      UserStatistics,
+      UserStatisticsBuilder<UserStatisticsCreateInput, UserStatistics | null>,
+      ExampleViewerAlias,
+      UserStatisticsCreateInput,
+      UserStatistics | null
+    >
+  | Trigger<
+      UserStatistics,
+      UserStatisticsBuilder<UserStatisticsCreateInput, UserStatistics | null>,
+      ExampleViewerAlias,
+      UserStatisticsCreateInput,
+      UserStatistics | null
+    >[]
+)[];
+
+export type CreateUserStatisticsActionObservers = Observer<
+  UserStatistics,
+  UserStatisticsBuilder<UserStatisticsCreateInput, UserStatistics | null>,
+  ExampleViewerAlias,
+  UserStatisticsCreateInput,
+  UserStatistics | null
+>[];
+
+export type CreateUserStatisticsActionValidators = Validator<
+  UserStatistics,
+  UserStatisticsBuilder<UserStatisticsCreateInput, UserStatistics | null>,
+  ExampleViewerAlias,
+  UserStatisticsCreateInput,
+  UserStatistics | null
+>[];
+
+export class CreateUserStatisticsActionBase
+  implements
+    Action<
+      UserStatistics,
+      UserStatisticsBuilder<UserStatisticsCreateInput, UserStatistics | null>,
+      ExampleViewerAlias,
+      UserStatisticsCreateInput,
+      UserStatistics | null
+    >
+{
+  public readonly builder: UserStatisticsBuilder<
+    UserStatisticsCreateInput,
+    UserStatistics | null
+  >;
+  public readonly viewer: ExampleViewerAlias;
+  protected input: UserStatisticsCreateInput;
+
+  constructor(viewer: ExampleViewerAlias, input: UserStatisticsCreateInput) {
+    this.viewer = viewer;
+    this.input = input;
+    this.builder = new UserStatisticsBuilder(
+      this.viewer,
+      WriteOperation.Insert,
+      this,
+      null,
+    );
+  }
+
+  getPrivacyPolicy(): PrivacyPolicy<UserStatistics, ExampleViewerAlias> {
+    if (schema.defaultActionPrivacy === undefined) {
+      throw new Error(
+        `defaultActionPrivacy in schema UserStatistics is undefined. This is likely a bug in the codegen. Please file an issue.`,
+      );
+    }
+
+    return typeof schema.defaultActionPrivacy === "function"
+      ? schema.defaultActionPrivacy()
+      : schema.defaultActionPrivacy;
+  }
+
+  getTriggers(): CreateUserStatisticsActionTriggers {
+    return [];
+  }
+
+  getObservers(): CreateUserStatisticsActionObservers {
+    return [];
+  }
+
+  getValidators(): CreateUserStatisticsActionValidators {
+    return [];
+  }
+
+  getInput(): UserStatisticsCreateInput {
+    return this.input;
+  }
+
+  async changeset(): Promise<Changeset> {
+    return this.builder.build();
+  }
+
+  async changesetWithOptions_BETA(
+    options: ChangesetOptions,
+  ): Promise<Changeset> {
+    return this.builder.buildWithOptions_BETA(options);
+  }
+
+  async valid(): Promise<boolean> {
+    return this.builder.valid();
+  }
+
+  async validX(): Promise<void> {
+    await this.builder.validX();
+  }
+
+  async save(): Promise<UserStatistics | null> {
+    await this.builder.save();
+    return this.builder.editedEnt();
+  }
+
+  async saveX(): Promise<UserStatistics> {
+    await this.builder.saveX();
+    return this.builder.editedEntX();
+  }
+
+  static create<T extends CreateUserStatisticsActionBase>(
+    this: new (
+      viewer: ExampleViewerAlias,
+      input: UserStatisticsCreateInput,
+    ) => T,
+    viewer: ExampleViewerAlias,
+    input: UserStatisticsCreateInput,
+  ): T {
+    return new this(viewer, input);
+  }
+}

examples/simple/src/ent/generated/user_statistics/actions/delete_user_statistics_action_base.ts
@@ -1,0 +1,161 @@
+/**
+ * Copyright whaa whaa
+ * Generated by github.com/lolopinto/ent/ent, DO NOT EDIT.
+ */
+
+import { ID, PrivacyPolicy } from "@snowtop/ent";
+import {
+  Action,
+  Changeset,
+  ChangesetOptions,
+  Observer,
+  Trigger,
+  Validator,
+  WriteOperation,
+} from "@snowtop/ent/action";
+import { UserStatistics } from "../../..";
+import {
+  UserStatisticsBuilder,
+  UserStatisticsInput,
+} from "./user_statistics_builder";
+import schema from "../../../../schema/user_statistics_schema";
+import { ExampleViewer as ExampleViewerAlias } from "../../../../viewer/viewer";
+
+export type DeleteUserStatisticsActionTriggers = (
+  | Trigger<
+      UserStatistics,
+      UserStatisticsBuilder<UserStatisticsInput, UserStatistics>,
+      ExampleViewerAlias,
+      UserStatisticsInput,
+      UserStatistics
+    >
+  | Trigger<
+      UserStatistics,
+      UserStatisticsBuilder<UserStatisticsInput, UserStatistics>,
+      ExampleViewerAlias,
+      UserStatisticsInput,
+      UserStatistics
+    >[]
+)[];
+
+export type DeleteUserStatisticsActionObservers = Observer<
+  UserStatistics,
+  UserStatisticsBuilder<UserStatisticsInput, UserStatistics>,
+  ExampleViewerAlias,
+  UserStatisticsInput,
+  UserStatistics
+>[];
+
+export type DeleteUserStatisticsActionValidators = Validator<
+  UserStatistics,
+  UserStatisticsBuilder<UserStatisticsInput, UserStatistics>,
+  ExampleViewerAlias,
+  UserStatisticsInput,
+  UserStatistics
+>[];
+
+export class DeleteUserStatisticsActionBase
+  implements
+    Action<
+      UserStatistics,
+      UserStatisticsBuilder<UserStatisticsInput, UserStatistics>,
+      ExampleViewerAlias,
+      UserStatisticsInput,
+      UserStatistics
+    >
+{
+  public readonly builder: UserStatisticsBuilder<
+    UserStatisticsInput,
+    UserStatistics
+  >;
+  public readonly viewer: ExampleViewerAlias;
+  protected readonly userStatistics: UserStatistics;
+
+  constructor(viewer: ExampleViewerAlias, userStatistics: UserStatistics) {
+    this.viewer = viewer;
+    this.builder = new UserStatisticsBuilder(
+      this.viewer,
+      WriteOperation.Delete,
+      this,
+      userStatistics,
+    );
+    this.userStatistics = userStatistics;
+  }
+
+  getPrivacyPolicy(): PrivacyPolicy<UserStatistics, ExampleViewerAlias> {
+    if (schema.defaultActionPrivacy === undefined) {
+      throw new Error(
+        `defaultActionPrivacy in schema UserStatistics is undefined. This is likely a bug in the codegen. Please file an issue.`,
+      );
+    }
+
+    return typeof schema.defaultActionPrivacy === "function"
+      ? schema.defaultActionPrivacy()
+      : schema.defaultActionPrivacy;
+  }
+
+  getTriggers(): DeleteUserStatisticsActionTriggers {
+    return [];
+  }
+
+  getObservers(): DeleteUserStatisticsActionObservers {
+    return [];
+  }
+
+  getValidators(): DeleteUserStatisticsActionValidators {
+    return [];
+  }
+
+  getInput(): UserStatisticsInput {
+    return {};
+  }
+
+  async changeset(): Promise<Changeset> {
+    return this.builder.build();
+  }
+
+  async changesetWithOptions_BETA(
+    options: ChangesetOptions,
+  ): Promise<Changeset> {
+    return this.builder.buildWithOptions_BETA(options);
+  }
+
+  async valid(): Promise<boolean> {
+    return this.builder.valid();
+  }
+
+  async validX(): Promise<void> {
+    await this.builder.validX();
+  }
+
+  async save(): Promise<void> {
+    await this.builder.save();
+  }
+
+  async saveX(): Promise<void> {
+    await this.builder.saveX();
+  }
+
+  static create<T extends DeleteUserStatisticsActionBase>(
+    this: new (
+      viewer: ExampleViewerAlias,
+      userStatistics: UserStatistics,
+    ) => T,
+    viewer: ExampleViewerAlias,
+    userStatistics: UserStatistics,
+  ): T {
+    return new this(viewer, userStatistics);
+  }
+
+  static async saveXFromID<T extends DeleteUserStatisticsActionBase>(
+    this: new (
+      viewer: ExampleViewerAlias,
+      userStatistics: UserStatistics,
+    ) => T,
+    viewer: ExampleViewerAlias,
+    id: ID,
+  ): Promise<void> {
+    const userStatistics = await UserStatistics.loadX(viewer, id);
+    return new this(viewer, userStatistics).saveX();
+  }
+}

commit bffc6c7b835075a3bd924de5346d9cc3ffab7e01

fix test in example
broken by https://github.com/lolopinto/ent/pull/1786

examples/simple/src/ent/auth_code/actions/create_auth_code_action.ts
@@ -114,9 +114,9 @@ export default class CreateAuthCodeAction extends CreateAuthCodeActionBase {
             FakeComms.send({
               to: input.emailAddress,
               mode: Mode.EMAIL,
-              from: "noreply@foo.com",
-              subject: "auth code",
-              body: `your auth code is ${input.code}`,
+              from: input.from ?? "noreply@foo.com",
+              subject: input.subject ?? "auth code",
+              body: input.body ?? `your auth code is ${input.code}`,
             });
           }
           if (input.phoneNumber) {
@@ -124,8 +124,8 @@ export default class CreateAuthCodeAction extends CreateAuthCodeActionBase {
             FakeComms.send({
               to: input.phoneNumber,
               mode: Mode.SMS,
-              from: "42423",
-              body: `your auth code is ${input.code}`,
+              from: input.from ?? "42423",
+              body: input.body ?? `your auth code is ${input.code}`,
             });
           }
         },

examples/simple/src/ent/generated/auth_code/actions/create_auth_code_action_base.ts
@@ -26,6 +26,9 @@ export interface AuthCodeCreateInput {
   userId: ID;
   emailAddress?: string | null;
   phoneNumber?: string | null;
+  from?: string;
+  subject?: string;
+  body?: string;
 }
 
 export type CreateAuthCodeActionTriggers = (

examples/simple/src/ent/user/actions/edit_email_address_action.ts
@@ -4,13 +4,12 @@ import {
 } from "../../generated/user/actions/edit_email_address_action_base";
 import { UserBuilder } from "../../generated/user/actions/user_builder";
 import CreateAuthCodeAction from "../../auth_code/actions/create_auth_code_action";
-import { FakeComms, Mode } from "@snowtop/ent/testutils/fake_comms";
 import { User } from "../..";
 import { EditUserPrivacy } from "./edit_user_privacy";
 
 export { EditEmailAddressInput };
 import { ExampleViewer } from "../../../viewer/viewer";
-import { Validator, Trigger, Observer } from "@snowtop/ent/action";
+import { Validator, Trigger } from "@snowtop/ent/action";
 
 class NewAuthCode {
   private code: string = "";
@@ -27,25 +26,18 @@ class NewAuthCode {
   }
 
   changeset(builder: UserBuilder, input: EditEmailAddressInput) {
+    const link = `https://website/confirm?email=${
+      input.newEmail
+    }&code=${this.getCode()}`;
+
     return CreateAuthCodeAction.create(builder.viewer, {
       emailAddress: input.newEmail,
       userId: builder.viewer.viewerID!,
       code: this.getCode(),
-    }).changeset();
-  }
-
-  observe(_builder: UserBuilder, input: EditEmailAddressInput) {
-    let link = `https://website/confirm?email=${
-      input.newEmail
-    }&code=${this.getCode()}`;
-
-    FakeComms.send({
-      to: input.newEmail,
-      mode: Mode.EMAIL,
       subject: "confirm email",
       from: "noreply@example-app.com",
       body: link,
-    });
+    }).changeset();
   }
 }
 
@@ -73,16 +65,6 @@ export default class EditEmailAddressAction extends EditEmailAddressActionBase {
     ];
   }
   getTriggers(): Trigger<
-    User,
-    UserBuilder<EditEmailAddressInput, User>,
-    ExampleViewer,
-    EditEmailAddressInput,
-    User
-  >[] {
-    return [this.generateNewCode];
-  }
-
-  getObservers(): Observer<
     User,
     UserBuilder<EditEmailAddressInput, User>,
     ExampleViewer,

examples/simple/src/ent/user/actions/edit_phone_number_action.ts
@@ -1,4 +1,3 @@
-import { FakeComms, Mode } from "@snowtop/ent/testutils/fake_comms";
 import CreateAuthCodeAction from "../../auth_code/actions/create_auth_code_action";
 import {
   EditPhoneNumberActionBase,
@@ -10,7 +9,7 @@ import { EditUserPrivacy } from "./edit_user_privacy";
 
 export { EditPhoneNumberInput };
 import { ExampleViewer } from "../../../viewer/viewer";
-import { Validator, Trigger, Observer } from "@snowtop/ent/action";
+import { Validator, Trigger } from "@snowtop/ent/action";
 
 class NewAuthCode {
   private code: string = "";
@@ -27,22 +26,15 @@ class NewAuthCode {
   }
 
   changeset(builder: UserBuilder, input: EditPhoneNumberInput) {
+    const body = `your new code is ${this.getCode()}`;
+
     return CreateAuthCodeAction.create(builder.viewer, {
       phoneNumber: input.newPhoneNumber,
       userId: builder.viewer.viewerID!,
       code: this.getCode(),
-    }).changeset();
-  }
-
-  observe(_builder: UserBuilder, input: EditPhoneNumberInput) {
-    let body = `your new code is ${this.getCode()}`;
-
-    FakeComms.send({
-      to: input.newPhoneNumber,
-      mode: Mode.SMS,
-      from: "42423",
       body,
-    });
+      from: "42423",
+    }).changeset();
   }
 }
 
@@ -72,16 +64,6 @@ export default class EditPhoneNumberAction extends EditPhoneNumberActionBase {
     ];
   }
   getTriggers(): Trigger<
-    User,
-    UserBuilder<EditPhoneNumberInput, User>,
-    ExampleViewer,
-    EditPhoneNumberInput,
-    User
-  >[] {
-    return [this.generateNewCode];
-  }
-
-  getObservers(): Observer<
     User,
     UserBuilder<EditPhoneNumberInput, User>,
     ExampleViewer,

examples/simple/src/schema/auth_code_schema.ts
@@ -24,6 +24,23 @@ const AuthCodeSchema = new EntSchema({
   actions: [
     {
       operation: ActionOperation.Create,
+      actionOnlyFields: [
+        {
+          name: "from",
+          type: "String",
+          optional: true,
+        },
+        {
+          name: "subject",
+          type: "String",
+          optional: true,
+        },
+        {
+          name: "body",
+          type: "String",
+          optional: true,
+        },
+      ],
     },
     {
       operation: ActionOperation.Delete,