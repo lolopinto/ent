3ddc1bf0e99a45901ddd1029f7cb48619315cb8d
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2025-12-19 17:29:40 -0800

    remove flaky test (#1896)

--- ent/loader_test.go
+++ ent/loader_test.go
@@ -11,37 +11,6 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type noTableSuite struct {
-	testingutils.Suite
-	tdb *test_db.TestDB
-}
-
-func (suite *noTableSuite) SetupSuite() {
-	suite.tdb = &test_db.TestDB{}
-
-	err := suite.tdb.BeforeAll()
-	require.Nil(suite.T(), err)
-
-	suite.Suite.ForceClean = true
-	suite.Suite.SetupSuite()
-}
-
-func (suite *noTableSuite) TearDownSuite() {
-	err := suite.tdb.AfterAll()
-	require.Nil(suite.T(), err)
-}
-
-func (suite *noTableSuite) TestLoadAssocEdges() {
-	res := <-ent.GenLoadAssocEdges()
-	assert.Len(suite.T(), res.Edges, 0)
-	assert.Nil(suite.T(), res.Err)
-}
-
-func TestNoTableSuite(t *testing.T) {
-	t.Parallel()
-	suite.Run(t, new(noTableSuite))
-}
-
 type tableExistsSuite struct {
 	testingutils.Suite
 	tdb *test_db.TestDB
