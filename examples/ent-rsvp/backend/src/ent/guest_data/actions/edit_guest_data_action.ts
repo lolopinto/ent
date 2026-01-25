c5ad0fa7a2395fa7e7032f1ae79c210cab3efda3
Ola Okelola <10857143+lolopinto@users.noreply.github.com>
2026-01-25 14:31:31 -0800

    update example (#1949)

--- examples/ent-rsvp/backend/src/ent/guest_data/actions/edit_guest_data_action.ts
+++ examples/ent-rsvp/backend/src/ent/guest_data/actions/edit_guest_data_action.ts
@@ -1,10 +1,8 @@
 import { AlwaysAllowPrivacyPolicy } from "@snowtop/ent";
-import {
-  EditGuestDataActionBase,
-  GuestDataEditInput,
-} from "src/ent/generated/guest_data/actions/edit_guest_data_action_base";
+import { EditGuestDataActionBase } from "src/ent/generated/guest_data/actions/edit_guest_data_action_base";
+import type { GuestDataEditInput } from "src/ent/generated/guest_data/actions/edit_guest_data_action_base";
 
-export { GuestDataEditInput };
+export type { GuestDataEditInput };
 
 // we're only writing this once except with --force and packageName provided
 export default class EditGuestDataAction extends EditGuestDataActionBase {
